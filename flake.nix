948781e10d52ed658f301f5e7b92463d862658c8
Islam Ahmed <softeng.islam@gmail.com>
2024-10-30 03:22:48 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,31 +1,23 @@
 {
   description = "My system configuration";
-
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     polymc.url = "github:PolyMC/PolyMC";
   };
-
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
-
     let
       system = "x86_64-linux";
     in
     {
-
       # nixos - system hostname
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         specialArgs = {
@@ -40,7 +32,6 @@
           inputs.nixvim.nixosModules.nixvim
         ];
       };
-
       homeConfigurations.softeng = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.${system};
         modules = [ ./home-manager/home.nix ];
