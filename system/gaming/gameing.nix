96b1d3b6484091221f9de33c2e16051d3aaeb42a
Islam Ahmed <softeng.islam@gmail.com>
2024-12-10 12:23:19 +0200

    update

--- system/gaming/gameing.nix
+++ system/gaming/gameing.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }: {
+  imports = [ ./aagl.nix ./lutris.nix ./steam.nix ];
   environment.systemPackages = with pkgs; [
     nethack
     zeroad
     steam
     steam-run # Run commands in the same FHS environment that is used for Steam
-    # protonup
+    protonup
     retroarch
     retroarch-assets
     retroarch-joypad-autoconfig
