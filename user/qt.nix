199ed3c0054b507f0f406608e0c4f21ebf361eef
Islam Ahmed <softeng.islam@gmail.com>
2024-12-13 15:40:25 +0200

    New Update

--- user/qt.nix
+++ user/qt.nix
@@ -1,11 +1,27 @@
-{ pkgs, ... }: {
-  # platform theme "gtk" or "gnome"
-  # name of the qt theme
-  # package to use
+{ pkgs, ... }:
+# let
+#   defaultFont =
+#     "${config.gtk.font.name},${builtins.toString config.gtk.font.size}";
+# in
+{
   qt = {
     enable = true;
-    platformTheme = "gtk";
-    style.name = "adwaita-dark";
-    style.package = pkgs.adwaita-qt;
+    platformTheme.name = "qtct";
+
+    # platformTheme = "gtk";
+    # style.name = "adwaita-dark";
+    # style.package = pkgs.adwaita-qt;
+  };
+
+  home.packages = [
+    pkgs.qt6Packages.qtstyleplugin-kvantum
+    pkgs.qt6Packages.qt6ct
+    pkgs.libsForQt5.qtstyleplugin-kvantum
+    pkgs.libsForQt5.qt5ct
+  ];
+  xdg.configFile = {
+    # qtct config
+    "qt5ct/qt5ct.conf".text = "";
+    "qt6ct/qt6ct.conf".text = "";
   };
 }
