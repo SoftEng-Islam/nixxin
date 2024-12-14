e19d4dc5ce934d7eb29c1e2fca62c47d63a61db1
Islam Ahmed <softeng.islam@gmail.com>
2024-12-14 03:49:31 +0200

    New Update

--- themes/everforest.nix
+++ themes/everforest.nix
@@ -1,26 +1,23 @@
-{ dir }:
-{
-    themeName = "everforest-dark-hard";
-    wallpaper = ../non-nix/wallpapers/frieren-everforest.jpg;
-    override = null;
+{ dir }: {
+  themeName = "everforest-dark-hard";
+  wallpaper = ../non-nix/wallpapers/frieren-everforest.jpg;
+  override = null;
 
-    # Override stylix theme of btop.
-    btopTheme = null;
+  # Override stylix theme of btop.
+  btopTheme = null;
 
-    # Hyprland and ags;
-    opacity = 1.0;
-    rounding = 0;
-    shadow = true;
-    bordersPlusPlus = true;
-    ags = {
-        theme = {
-            border = {
-                width = 1;
-                opacity = 70;
-            };
-        };
-        bar = {
-            flatButtons = true;
-        };
+  # Hyprland and ags;
+  opacity = 1.0;
+  rounding = 0;
+  shadow = true;
+  bordersPlusPlus = true;
+  ags = {
+    theme = {
+      border = {
+        width = 1;
+        opacity = 70;
+      };
     };
+    bar = { flatButtons = true; };
+  };
 }
