501575d8ba3e8f2ae165a1dee2c10bb0edd1ed0b
Islam Ahmed <softeng.islam@gmail.com>
2024-12-18 03:37:44 +0200

    update

--- system/global/desktop-apps.nix
+++ system/global/desktop-apps.nix
@@ -32,7 +32,7 @@
 
     # community-apps
     discord # All-in-one cross-platform voice and text chat for gamers
-    zoom-us # zoom.us video conferencing application
+    # zoom-us # zoom.us video conferencing application
     # ferdium # All your services in one place built by the community
     # mumble # Low-latency, high quality voice chat software
     # signal-desktop # Private, simple, and secure messenger
