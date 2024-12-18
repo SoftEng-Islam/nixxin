8862157ae03713be02acccee0182698b4e9077d1
Islam Ahmed <softeng.islam@gmail.com>
2024-12-18 16:28:51 +0200

    update

--- system/global/packages.nix
+++ system/global/packages.nix
@@ -36,28 +36,28 @@
     meson # An open source, fast and friendly build system made in Python
     pyenv # Simple Python version management
     python3
-    pythonPackages.openai
-    # pythonPackages.pip # Errors
-    # pythonPackages.build
-    pythonPackages.cairosvg
-    pythonPackages.certifi
-    pythonPackages.charset-normalizer
-    pythonPackages.click
-    pythonPackages.idna
-    pythonPackages.loguru
-    pythonPackages.markupsafe
-    pythonPackages.opencv-python # Open Computer Vision Library with more than 500 algorithms
-    pythonPackages.pillow
-    pythonPackages.psutil
-    pythonPackages.pycairo
-    pythonPackages.pygobject3
-    pythonPackages.pywal
-    pythonPackages.pywayland
-    pythonPackages.requests
-    pythonPackages.setuptools-scm
-    pythonPackages.urllib3
-    pythonPackages.wheel
-    pythonPackages.beautifulsoup4
+    python3Packages.openai
+    python3Packages.pip
+    python3Packages.build
+    python3Packages.cairosvg
+    python3Packages.certifi
+    python3Packages.charset-normalizer
+    python3Packages.click
+    python3Packages.idna
+    python3Packages.loguru
+    python3Packages.markupsafe
+    python3Packages.opencv-python # Open Computer Vision Library with more than 500 algorithms
+    python3Packages.pillow
+    python3Packages.psutil
+    python3Packages.pycairo
+    python3Packages.pygobject3
+    python3Packages.pywal
+    python3Packages.pywayland
+    python3Packages.requests
+    python3Packages.setuptools-scm
+    python3Packages.urllib3
+    python3Packages.wheel
+    python3Packages.beautifulsoup4
 
     # Rust --------------------------------
     cargo # Downloads your Rust project's dependencies and builds your project
