d5cd281e23f540e252374676b8ea72acb4593e3c
Islam Ahmed <softeng.islam@gmail.com>
2024-12-18 06:29:53 +0200

    update

--- system/global/desktop-apps.nix
+++ system/global/desktop-apps.nix
@@ -43,7 +43,7 @@
     brave # Privacy-oriented browser for Desktop and Laptop computers
     firefox-beta # Web browser built from Firefox Beta Release source tree
     google-chrome # Freeware web browser developed by Google
-    # microsoft-edge # The web browser from Microsoft
+    microsoft-edge # The web browser from Microsoft
 
     # audacity # Sound editor with graphical UI
     gparted # Graphical disk partitioning tool
