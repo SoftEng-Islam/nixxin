62cdb2522416c2e44c588eb5885d67eb067dc3f2
Islam Ahmed <softeng.islam@gmail.com>
2024-12-19 10:52:20 +0200

    update

--- profiles/desktop/configuration.nix
+++ profiles/desktop/configuration.nix
@@ -28,9 +28,6 @@
     ../../system/desktop/nixos.nix
     ../../system/desktop/packages.nix
     ../../system/desktop/power-management.nix
-    ../../system/desktop/programs.nix
-    ../../system/desktop/security.nix
-    ../../system/desktop/services.nix
     ../../system/desktop/shell.nix
     ../../system/desktop/users.nix
     ../../system/desktop/wayland.nix
@@ -39,10 +36,63 @@
     ../../system/desktop/zram.nix
   ];
 
-  # Use zsh. TODO: option to flake.nix, several shells.
+  # Use zsh.
   environment.shells = with pkgs; [ zsh ];
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
+
+  services.accounts-daemon.enable = true;
+  services.dbus.implementation = "broker";
+  services.flatpak.enable = false;
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.mouse.accelSpeed = "-0.5";
+  # a DBus service that provides power management support to applications.
+  services.openssh.enable = true; # Enable the OpenSSH daemon.
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.printing.enable = false; # Enable CUPS to print documents.
+  services.sysprof.enable = false; # Whether to enable sysprof profiling daemon.
+  # a DBus service that allows applications to update firmware.
+  services.fwupd.enable = false;
+  services.geoclue2.enable = true;
+  services.colord.enable = true;
+  # automount
+  services.devmon.enable = true;
+  services.udisks2.enable = true;
+  services.gvfs.enable = true; # A lot of mpris packages require it.
+  # logind
+  services.logind.extraConfig = ''
+    HandlePowerKey=ignore
+    HandleLidSwitch=suspend
+    HandleLidSwitchExternalPower=ignore
+  '';
+  services.displayManager.enable = true;
+  services.displayManager.defaultSession = "gnome"; # Set `gnome` or `hyprland`
+
+  programs.corectrl.enable = true;
+  programs.corectrl.gpuOverclock.enable = true;
+  programs.corectrl.gpuOverclock.ppfeaturemask = "0xffffffff";
+  programs.dconf.enable = true; # dconf
+  programs.droidcam.enable = true; # camera
+  programs.mtr.enable = true;
+  programs.xwayland.enable = false;
+  programs.htop.enable = true;
+  programs.htop.settings = { tree_view = 1; };
+  programs.gnupg.agent.enable = true;
+  programs.gnupg.agent.enableSSHSupport = true;
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Security
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # show Password as stars in Terminals.
+  security.sudo.extraConfig = "Defaults        env_reset,pwfeedback";
+  security.allowSimultaneousMultithreading = true; # to allow SMT/hyperthreading
+  security.polkit.enable = true;
+  security.pam.services.astal-auth = { };
+
+  # system.autoUpgrade.enable = true;
+  # system.autoUpgrade.allowReboot = true;
+  # system.autoUpgrade.channel = "https://channels.nixos.org/nixos-24.05";
   # List of globally installed packages.
   environment.systemPackages = with pkgs; [
     home-manager
@@ -56,14 +106,6 @@
     git
     vim
   ];
-
-  # qt.platformTheme = "qt5ct";
-  # qt.style = "adwaita-dark";
-
-  # system.autoUpgrade.enable = true;
-  # system.autoUpgrade.allowReboot = true;
-  # system.autoUpgrade.channel = "https://channels.nixos.org/nixos-24.05";
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- system/desktop/services.nix
+++ system/desktop/services.nix
@@ -1,48 +1,4 @@
 {
-  services = {
-    # fstrim.enable = true; # Whether to enable periodic SSD TRIM of mounted partitions in background.
-    # tlp.enable = true;
-    accounts-daemon.enable = true;
-    dbus.implementation = "broker";
-    flatpak.enable = false;
-
-    # Enable touchpad support (enabled default in most desktopManager).
-    libinput.mouse.accelSpeed = "-0.5";
-
-    # a DBus service that provides power management support to applications.
-    upower.enable = true;
-
-    openssh.enable = true; # Enable the OpenSSH daemon.
-    power-profiles-daemon.enable = true;
-    printing.enable = false; # Enable CUPS to print documents.
-    sysprof.enable = false; # Whether to enable sysprof profiling daemon.
-
-    # a DBus service that allows applications to update firmware.
-    fwupd.enable = false;
-
-    geoclue2.enable = true;
-    colord.enable = true;
-
-    # automount
-    devmon.enable = true;
-    udisks2.enable = true;
-    # A lot of mpris packages require it.
-    gvfs.enable = true;
-
-    # logind
-    logind.extraConfig = ''
-      HandlePowerKey=ignore
-      HandleLidSwitch=suspend
-      HandleLidSwitchExternalPower=ignore
-    '';
-
-    displayManager = {
-      enable = true; # Whether to enable systemd’s display-manager service.
-      # sddm.enable = true; # Whether to enable sddm as the display manager.
-      defaultSession =
-        "gnome"; # Set `gnome` or `hyprland` as the default session
-    };
-  };
   #beesd = {
   #	enable = true;
   #	filesystems.btrfs = {
