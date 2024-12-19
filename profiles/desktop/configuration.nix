ad4c5ffff662433fff44614ae8e00551ce680311
Islam Ahmed <softeng.islam@gmail.com>
2024-12-19 18:29:36 +0200

    update

--- profiles/desktop/configuration.nix
+++ profiles/desktop/configuration.nix
@@ -169,6 +169,30 @@
   # system.autoUpgrade.channel = "https://channels.nixos.org/nixos-24.05";
   # List of globally installed packages.
   environment.systemPackages = with pkgs; [
+    # Themes & Graphical Interfaces
+    gtk3 # A multi-platform toolkit for creating graphical user interfaces
+    gtk4 # A multi-platform toolkit for creating graphical user interfaces
+    gtk_engines # Theme engines for GTK 2
+    libadwaita # Library to help with developing UI for mobile devices using GTK/GNOME
+
+    adw-gtk3 # Theme from libadwaita ported to GTK-3
+    gruvbox-dark-gtk # Gruvbox theme for GTK based desktop environments
+    gruvbox-gtk-theme # GTK theme based on the Gruvbox colour palette
+    colloid-gtk-theme # Modern and clean Gtk theme
+
+    qt6Packages.qtstyleplugin-kvantum
+    qt6Packages.qt6ct
+    libsForQt5.qtstyleplugin-kvantum
+    libsForQt5.qt5ct
+
+    # Icons
+    gruvbox-dark-icons-gtk
+    gruvbox-plus-icons
+    papirus-icon-theme # Pixel perfect icon theme for Linux
+
+    # QT Themes
+    adwaita-qt
+    adwaita-qt6
 
     # Plymouth Theme For Nixos:
     plymouth # Boot splash and boot logger
