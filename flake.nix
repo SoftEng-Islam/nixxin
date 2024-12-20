4b73e6def60aa598bda40074aae3e3f70165ad4a
Islam Ahmed <softeng.islam@gmail.com>
2024-12-20 06:44:36 +0200

    update

--- flake.nix
+++ flake.nix
@@ -1,46 +1,51 @@
 {
   description = "Softeng Nixxin Configuration";
+
   inputs = {
+    # channels
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # third party nixpkgs|overlays|modules
-    chaotic.url = "github:chaotic-cx/nyx";
-    chaotic.inputs.nixpkgs.follows = "nixpkgs";
-
-    matcha.url = "git+https://codeberg.org/QuincePie/matcha";
-    matcha.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-gaming.url =
-      "github:fufexan/nix-gaming?rev=1e435616e688c2b9125cd5282febcad3ab981d5e";
-    nix-gaming.inputs.nixpkgs.follows = "nixpkgs";
-
-    nixpkgs-wayland.url =
-      "github:nix-community/nixpkgs-wayland?rev=52b72b12c456a5c0c87c40941ef79335e8d61104"; # master (03 sep 2024)
-    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
-
-    android-nixpkgs.url =
-      "github:tadfisher/android-nixpkgs?rev=e2aec559a903ee1d94fd9935b4d558803adaf5a4";
-    android-nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
-
-    nur.url = "github:nix-community/NUR";
-    nixos-hardware.url =
-      "github:NixOS/nixos-hardware?rev=672ac2ac86f7dff2f6f3406405bddecf960e0db6"; # master (22 nov 2024)
-
-    ags.url = "github:Aylur/ags";
-    ags.inputs.nixpkgs.follows = "nixpkgs";
-
+    ucodenix.url = "github:e-tho/ucodenix";
     stylix.url = "github:danth/stylix";
     swww.url = "github:LGFae/swww";
+    nur.url = "github:nix-community/NUR";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
     # superfile.url = "github:yorukot/superfile";
 
-    nix-ld.url = "github:Mic92/nix-ld";
-    nix-ld.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-alien.url = "github:thiagokokada/nix-alien";
-    nix-alien.inputs.nixpkgs.follows = "nixpkgs";
-
+    # third party nixpkgs|overlays|modules
+    chaotic = {
+      url = "github:chaotic-cx/nyx";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    matcha = {
+      url = "git+https://codeberg.org/QuincePie/matcha";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs-wayland = {
+      url = "github:nix-community/nixpkgs-wayland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags = {
+      url = "github:Aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-ld = {
+      url = "github:Mic92/nix-ld";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-alien = {
+      url = "github:thiagokokada/nix-alien";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hyprland = {
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
@@ -54,74 +59,177 @@
       url = "github:hyprwm/hyprpicker";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     pyprland = {
       url = "github:hyprland-community/pyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     lobster = {
       url = "github:justchokingaround/lobster";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    scenefx.url =
-      "github:wlrfx/scenefx?rev=7a7c35750239bd117931698b121a072f30bc1cbe";
-    scenefx.inputs.nixpkgs.follows = "nixpkgs";
-    wayfreeze.url =
-      "github:Jappie3/wayfreeze?rev=37ef9e456c87601ed4e06f36f437f688083d61cf";
-    wayfreeze.inputs.nixpkgs.follows = "nixpkgs";
-    arkenfox.url = "github:dwarfmaster/arkenfox-nixos";
-    arkenfox.inputs.nixpkgs.follows = "nixpkgs";
-    zen-browser.url =
-      "github:MarceColl/zen-browser-flake?rev=96dffb45b36141261e97a8f83438e4c88911fefa";
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
-    ucodenix.url =
-      "github:e-tho/ucodenix?rev=7d7586d3fcd07e147c0dba9291b2473e060c4c98";
-
-    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay?rev=9822e0611d49ae70278ac20c9d7b68e4797b2fab";
+    scenefx = {
+      scenefx.url = "github:wlrfx/scenefx";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    wayfreeze = {
+      url = "github:Jappie3/wayfreeze";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    arkenfox = {
+      url = "github:dwarfmaster/arkenfox-nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # others
+    easy-effects-presets.url = "github:JackHack96/EasyEffects-Presets";
+    easy-effects-presets.flake = false;
+    retroarch-shaders.url = "github:libretro/glsl-shaders";
+    retroarch-shaders.flake = false;
+    reversal-extra.url =
+      "github:wochap/Reversal-Extra?rev=cab51b96a08289d4ad5eed8e3a58d06d55dcc961";
+    reversal-extra.flake = false;
+    wluma.url =
+      "github:maximbaz/wluma?rev=f7aa9967231ef5c821eeea7e43a9929ace0c4fa8";
+    wluma.flake = false;
+
+    # terminal tools
+    fuzzy-sys.url =
+      "github:NullSense/fuzzy-sys?rev=ddd8f87bee2260f1a27bd5f9b6362a4e904e1e8f";
+    fuzzy-sys.flake = false;
+    kitty-scrollback-nvim.url =
+      "github:mikesmithgh/kitty-scrollback.nvim?rev=3f430ff8829dc2b0f5291d87789320231fdb65a1"; # (20 aug 2024)
+    kitty-scrollback-nvim.flake = false;
+    kitty-smart-scroll.url = "github:yurikhan/kitty-smart-scroll";
+    kitty-smart-scroll.flake = false;
+    kitty-smart-tab.url = "github:yurikhan/kitty-smart-tab";
+    kitty-smart-tab.flake = false;
+    powerlevel10k.url =
+      "github:romkatv/powerlevel10k?rev=35833ea15f14b71dbcebc7e54c104d8d56ca5268";
+    powerlevel10k.flake = false;
+    smart-splits-nvim.url =
+      "github:mrjones2014/smart-splits.nvim?rev=aee30930689ae427729aad41568ccaae57c167fe"; # (30 aug 2024)
+    smart-splits-nvim.flake = false;
+    zsh-autocomplete.url =
+      "github:wochap/zsh-autocomplete?rev=d52da825c2b60b664f33e8d82fdfc1c3b647b753";
+    zsh-autocomplete.flake = false;
+    zsh-defer.url =
+      "github:romkatv/zsh-defer?rev=1c75faff4d8584afe090b06db11991c8c8d62055";
+    zsh-defer.flake = false;
+    zsh-history-substring-search.url =
+      "github:zsh-users/zsh-history-substring-search?rev=8dd05bfcc12b0cd1ee9ea64be725b3d9f713cf64";
+    zsh-history-substring-search.flake = false;
+    zsh-notify.url =
+      "github:marzocchi/zsh-notify?rev=9c1dac81a48ec85d742ebf236172b4d92aab2f3f";
+    zsh-notify.flake = false;
+    zsh-vi-mode.url =
+      "github:wochap/zsh-vi-mode?rev=6091e2bc63cd076f94583a6d51eda74980566d28";
+    zsh-vi-mode.flake = false;
+    zsh-pnpm-shell-completion.url = "github:g-plane/pnpm-shell-completion";
+    zsh-pnpm-shell-completion.flake = false;
+    figlet-fonts.url = "github:wochap/figlet-fonts";
+    figlet-fonts.flake = false;
+    ipwebcam-gst.url =
+      "github:agarciadom/ipwebcam-gst?rev=5a02ffae8597ab1cc7461f096f86ca233f925a07";
+    ipwebcam-gst.flake = false;
+    tmux-sessionx.url =
+      "github:omerxx/tmux-sessionx?rev=ac9b0ec219c2e36ce6beb3f900ef852ba8888095";
+    nnn-cppath.url =
+      "github:raffaem/nnn-cppath?rev=1d3f4f64d43533d203af82c61f4a93afc8d5aaf5";
+    nnn-cppath.flake = false;
+
+    # themes
+    dracula-lsd.url =
+      "github:dracula/lsd?rev=75f3305a2bba4dacac82b143a15d278daee28232";
+    dracula-lsd.flake = false;
+    catppuccin-grub.url = "github:catppuccin/grub";
+    catppuccin-grub.flake = false;
+    catppuccin-alacritty.url = "github:catppuccin/alacritty";
+    catppuccin-alacritty.flake = false;
+    catppuccin-amfora.url = "github:catppuccin/amfora";
+    catppuccin-amfora.flake = false;
+    catppuccin-bat.url = "github:catppuccin/bat";
+    catppuccin-bat.flake = false;
+    catppuccin-bottom.url = "github:catppuccin/bottom";
+    catppuccin-bottom.flake = false;
+    catppuccin-dircolors.url = "github:wochap/dircolors";
+    catppuccin-dircolors.flake = false;
+    catppuccin-discord.url = "github:catppuccin/discord";
+    catppuccin-discord.flake = false;
+    catppuccin-kitty.url = "github:catppuccin/kitty";
+    catppuccin-kitty.flake = false;
+    catppuccin-lazygit.url = "github:catppuccin/lazygit";
+    catppuccin-lazygit.flake = false;
+    catppuccin-delta.url = "github:catppuccin/delta";
+    catppuccin-delta.flake = false;
+    catppuccin-neomutt.url = "github:catppuccin/neomutt";
+    catppuccin-neomutt.flake = false;
+    catppuccin-newsboat.url = "github:catppuccin/newsboat";
+    catppuccin-newsboat.flake = false;
+    catppuccin-qutebrowser.url = "github:catppuccin/qutebrowser";
+    catppuccin-qutebrowser.flake = false;
+    catppuccin-zathura.url = "github:catppuccin/zathura";
+    catppuccin-zathura.flake = false;
+    catppuccin-zsh-fsh.url = "github:catppuccin/zsh-fsh";
+    catppuccin-zsh-fsh.flake = false;
+    catppuccin-tmux.url = "github:catppuccin/tmux";
+    catppuccin-tmux.flake = false;
+    catppuccin-mpv.url = "github:catppuccin/mpv";
+    catppuccin-mpv.flake = false;
+    catppuccin-foot.url = "github:catppuccin/foot";
+    catppuccin-foot.flake = false;
+    catppuccin-obs.url = "github:catppuccin/obs";
+    catppuccin-obs.flake = false;
   };
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+
+  outputs = inputs:
     let
-      settings = import (./. + "/settings.nix") { inherit pkgs; };
-      pkgs = import nixpkgs { system = settings.system; };
-    in {
-      # NixOS configuration entrypoint.
-      # 'nixos-rebuild switch --flake .#hostname
-      nixosConfigurations = {
-        ${settings.hostname} = nixpkgs.lib.nixosSystem {
+      mkLib = pkgs: system:
+        let
+          lib = pkgs.lib.extend (final: prev: {
+            home-manager = inputs.home-manager.lib.hm;
+            _custom = import ./lib {
+              pkgs = import pkgs { inherit system; };
+              inherit inputs;
+              inherit lib;
+            };
+          });
+        in lib;
+      mkNixosSystem = pkgs: system: hostName:
+        pkgs.lib.nixosSystem {
+          inherit system;
           modules = [
-            inputs.stylix.nixosModules.stylix
-            (./. + "/profiles" + ("/" + settings.profile)
-              + "/configuration.nix")
+            inputs.home-manager.nixosModules.home-manager
+            inputs.nur.nixosModules.nur
+            inputs.chaotic.nixosModules.default
+            inputs.ucodenix.nixosModules.default
+            ./overlays
+            ./modules/archetypes
+            ./modules/nixos
+            ./modules/shared
+            ./packages
+            (./. + "/hosts/${hostName}")
+            {
+              nixpkgs.config.allowUnfree = true;
+              nixpkgs.config.permittedInsecurePackages =
+                [ "nodejs-14.21.3" "openssl-1.1.1u" "openssl-1.1.1v" ];
+              networking.hostName = hostName;
+            }
           ];
           specialArgs = {
             inherit inputs;
-            inherit settings;
+            inherit system;
+            lib = mkLib pkgs system;
+            nixpkgs = pkgs;
           };
         };
-      };
-
-      # Standalone home-manager configuration entrypoint.
-      # 'home-manager switch --flake .#username
-      homeConfigurations = {
-        ${settings.username} = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${settings.system};
-          modules = [
-            (./. + "/profiles" + ("/" + settings.profile) + "/home.nix")
-            inputs.stylix.homeManagerModules.stylix
-            inputs.nixvim.homeManagerModules.nixvim
-          ];
-          extraSpecialArgs = {
-            inherit inputs;
-            inherit settings;
-          };
-        };
+    in {
+      nixosConfigurations = {
+        gdesktop = mkNixosSystem inputs.nixpkgs "x86_64-linux" "gdesktop";
+        glegion = mkNixosSystem inputs.nixpkgs "x86_64-linux" "glegion";
       };
     };
 }
