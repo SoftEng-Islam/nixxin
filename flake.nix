3f999f84c664f4f4790e09a837080bca35930a1a
Islam Ahmed <softeng.islam@gmail.com>
2024-12-20 11:20:14 +0200

    update

--- flake.nix
+++ flake.nix
@@ -4,46 +4,25 @@
   inputs = {
     # channels
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # `ucodenix` is a Nix flake providing AMD microcode updates for unsupported CPUs.
     ucodenix.url = "github:e-tho/ucodenix";
+    # System-wide colorscheming and typography for NixOS
     stylix.url = "github:danth/stylix";
+    # Efficient animated wallpaper daemon for wayland, controlled at runtime
     swww.url = "github:LGFae/swww";
-    nur.url = "github:nix-community/NUR";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
     # superfile.url = "github:yorukot/superfile";
 
     # third party nixpkgs|overlays|modules
-    chaotic = {
-      url = "github:chaotic-cx/nyx";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    matcha = {
-      url = "git+https://codeberg.org/QuincePie/matcha";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-gaming = {
-      url = "github:fufexan/nix-gaming";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs-wayland = {
-      url = "github:nix-community/nixpkgs-wayland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    android-nixpkgs = {
-      url = "github:tadfisher/android-nixpkgs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     ags = {
       url = "github:Aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-ld = {
       url = "github:Mic92/nix-ld";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-alien = {
-      url = "github:thiagokokada/nix-alien";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
@@ -63,56 +42,25 @@
       url = "github:hyprland-community/pyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    lobster = {
-      url = "github:justchokingaround/lobster";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    scenefx = {
-      url = "github:wlrfx/scenefx";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    wayfreeze = {
-      url = "github:Jappie3/wayfreeze";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    arkenfox = {
-      url = "github:dwarfmaster/arkenfox-nixos";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     # others
-    easy-effects-presets.url = "github:JackHack96/EasyEffects-Presets";
-    easy-effects-presets.flake = false;
     retroarch-shaders.url = "github:libretro/glsl-shaders";
     retroarch-shaders.flake = false;
-    reversal-extra.url =
-      "github:wochap/Reversal-Extra?rev=cab51b96a08289d4ad5eed8e3a58d06d55dcc961";
-    reversal-extra.flake = false;
-    wluma.url =
-      "github:maximbaz/wluma?rev=f7aa9967231ef5c821eeea7e43a9929ace0c4fa8";
-    wluma.flake = false;
 
     # terminal tools
-    fuzzy-sys.url =
-      "github:NullSense/fuzzy-sys?rev=ddd8f87bee2260f1a27bd5f9b6362a4e904e1e8f";
+    fuzzy-sys.url = "github:NullSense/fuzzy-sys";
     fuzzy-sys.flake = false;
-    kitty-scrollback-nvim.url =
-      "github:mikesmithgh/kitty-scrollback.nvim?rev=3f430ff8829dc2b0f5291d87789320231fdb65a1"; # (20 aug 2024)
+
+    kitty-scrollback-nvim.url = "github:mikesmithgh/kitty-scrollback.nvim";
     kitty-scrollback-nvim.flake = false;
     kitty-smart-scroll.url = "github:yurikhan/kitty-smart-scroll";
     kitty-smart-scroll.flake = false;
     kitty-smart-tab.url = "github:yurikhan/kitty-smart-tab";
     kitty-smart-tab.flake = false;
-    powerlevel10k.url =
-      "github:romkatv/powerlevel10k?rev=35833ea15f14b71dbcebc7e54c104d8d56ca5268";
-    powerlevel10k.flake = false;
-    smart-splits-nvim.url =
-      "github:mrjones2014/smart-splits.nvim?rev=aee30930689ae427729aad41568ccaae57c167fe"; # (30 aug 2024)
+
+    smart-splits-nvim.url = "github:mrjones2014/smart-splits.nvim";
     smart-splits-nvim.flake = false;
+
     zsh-autocomplete.url =
       "github:wochap/zsh-autocomplete?rev=d52da825c2b60b664f33e8d82fdfc1c3b647b753";
     zsh-autocomplete.flake = false;
