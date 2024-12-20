73cebf990d9cf44b33469690ac6709372e8e1050
Islam Ahmed <softeng.islam@gmail.com>
2024-12-20 18:28:46 +0200

    update

--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -17,9 +17,8 @@
 
   users.users.${settings.username}.isNormalUser = true;
   home-manager.users.${settings.username} = { pkgs, ... }: {
-    home.packages = [ pkgs.atool pkgs.httpie ];
+    # home.packages = [ pkgs.atool pkgs.httpie ];
     programs.bash.enable = true;
-
     # The state version is required and should stay at the version you
     # originally installed.
     home.stateVersion = "24.11";
