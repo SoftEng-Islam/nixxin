ffc6d5dc0d7f4538c964fe91923a8c5384ec0f01
Islam Ahmed <softeng.islam@gmail.com>
2024-12-21 13:53:32 +0200

    update

--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -21,9 +21,14 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   services.ssh-agent.enable = true;
+  users.users.${mySettings.username}.isNormalUser = true;
   home-manager.users.${mySettings.username} = { pkgs, ... }: {
+    home.packages = [ pkgs.atool pkgs.httpie ];
     programs.bash.enable = true;
 
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "24.11";
     # GTK Settings
     gtk = {
       enable = true;
