87add3099d7c960e3b172d7a5f2b93dc60e18dd7
Islam Ahmed <softeng.islam@gmail.com>
2024-12-23 10:28:13 +0200

    update

--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -1,76 +1,74 @@
-{ config, pkgs, mySettings, ... }: {
+{ config, pkgs, lib, mySettings, ... }: {
   useGlobalPkgs = true;
   useUserPackages = false;
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "softeng";
-  home.homeDirectory = "/home/softeng";
+  stylix.targets.hyprland.enable = false;
+  home = {
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    username = mySettings.username;
+    homeDirectory = "/home/${mySettings.username}";
+    stateVersion = mySettings.homeStateVersion;
+    sessionVariables = {
+      EDITOR = mySettings.editor;
+      TERM = mySettings.term;
+      BROWSER = mySettings.browser;
+    };
+  };
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      name = mySettings.cursorTheme;
+      size = mySettings.cursorSize;
+      package = mySettings.cursorPackage;
+    };
+    font = {
+      name = mySettings.fontName;
+      package = mySettings.fontPackage;
+      size = mySettings.fontSize;
+    };
+    iconTheme = {
+      name = mySettings.iconName;
+      package = mySettings.iconPackage;
+    };
+    theme = {
+      name = lib.mkForce mySettings.gtkTheme;
+      package = lib.mkForce mySettings.gtkPackage;
+    };
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+  };
+  xdg = {
+    enable = true;
+    configFile."gtk-4.0/gtk.css".enable = lib.mkForce false;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      music = "${config.home.homeDirectory}/Music";
+      videos = "${config.home.homeDirectory}/Videos";
+      pictures = "${config.home.homeDirectory}/Pictures";
+      download = "${config.home.homeDirectory}/Downloads";
+      documents = "${config.home.homeDirectory}/Documents";
+      templates = null;
+      desktop = null;
+      publicShare = null;
+      extraConfig = {
+        XDG_DOTFILES_DIR = "${mySettings.dotfilesDir}";
+        XDG_BOOK_DIR = "${config.home.homeDirectory}/Books";
+      };
+    };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/softeng/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+  # disable manuals as nmd fails to build often
+  manual = {
+    html.enable = false;
+    json.enable = false;
+    manpages.enable = false;
   };
 
+  # QT Settings
+  qt = {
+    enable = true;
+    platformTheme.name = mySettings.qtPlatformTheme;
+    style.name = mySettings.qtStyle;
+  };
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
