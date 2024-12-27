34d0ac618893e469064e01b39b311c4839bf7070
Islam Ahmed <softeng.islam@gmail.com>
2024-12-27 08:35:54 +0200

    update

--- profiles/desktop/system/dev/python/default.nix
+++ profiles/desktop/system/dev/python/default.nix
@@ -35,11 +35,11 @@
     # python3Packages.material-color-utilities
     # python3Packages.materialyoucolor
     # Override pallets-sphinx-themes to fix the build
-    (python3Packages.pallets-sphinx-themes.overrideAttrs (old: {
-      doCheck = false; # Disable build checks
-      doInstallCheck = false; # Disable install checks
-      buildInputs = (old.buildInputs or [ ])
-        ++ [ python3Packages.wheel ]; # Add wheel
-    }))
+    # (python3Packages.pallets-sphinx-themes.overrideAttrs (old: {
+    #   doCheck = false; # Disable build checks
+    #   doInstallCheck = false; # Disable install checks
+    #   buildInputs = (old.buildInputs or [ ])
+    #     ++ [ python3Packages.wheel ]; # Add wheel
+    # }))
   ];
 }
