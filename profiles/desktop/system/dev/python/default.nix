38e63298ea07f2a7a91c730bbd52baa6eaafe898
Islam Ahmed <softeng.islam@gmail.com>
2024-12-28 08:35:48 +0200

    update

--- profiles/desktop/system/dev/python/default.nix
+++ profiles/desktop/system/dev/python/default.nix
@@ -6,17 +6,25 @@
     python3
     python3Packages.pip
     # NOTE: add here any python package you need globally
+    python-hyprpy
+    python-xwaykeyz
     python3Packages.beautifulsoup4
     python3Packages.build
     python3Packages.cairosvg
     python3Packages.certifi
     python3Packages.charset-normalizer
     python3Packages.click
+    python3Packages.dbus-python
+    python3Packages.django
     python3Packages.html2text
+    python3Packages.i3ipc
     python3Packages.icalendar
     python3Packages.idna
+    python3Packages.lockfile
     python3Packages.loguru
     python3Packages.markupsafe
+    python3Packages.material-color-utilities
+    python3Packages.materialyoucolor
     python3Packages.opencv-python # Open Computer Vision Library with more than 500 algorithms
     python3Packages.pillow
     python3Packages.psutil
@@ -26,17 +34,32 @@
     python3Packages.pytz
     python3Packages.pywal
     python3Packages.pywayland
+    python3Packages.pywlroots
     python3Packages.requests
+    python3Packages.setuptools
     python3Packages.setuptools-scm
+    python3Packages.sv-ttk
+    python3Packages.systemd
+    python3Packages.tkinter
     python3Packages.tzlocal
     python3Packages.urllib3
+    python3Packages.virtualenv
+    python3Packages.watchdog
     python3Packages.wheel
-    python3Packages.django
-    python3Packages.material-color-utilities
-    python3Packages.materialyoucolor
-    pip-audit
+
     (python3.withPackages (ps: [ ps.jinja2 ]))
+    cairo.dev
+    dbus.dev
+    glib.dev
+    gobject-introspection
+    gobject-introspection.dev
     jinja2-cli
-    # python312Packages.jinja2
+    libffi.dev
+    linuxHeaders
+    pip-audit
+    pkg-config
+    systemd.dev
+    wrapGAppsHook
+
   ];
 }
