b5f4bd2380d7e38701b8c417a2da96e74289eb3d
Islam Ahmed <softeng.islam@gmail.com>
2024-12-29 06:59:56 +0200

    update

--- profiles/desktop/system/networking.nix
+++ profiles/desktop/system/networking.nix
@@ -77,7 +77,7 @@
           [ "8.8.8.8" "8.8.4.4" "2001:4860:4860::8888" "2001:4860:4860::8844" ];
         # Provide DHCP settings (if applicable)
         dhcpRange = "10.42.0.10,10.42.0.100,12h"; # Adjust to your network
-        dhcpLeaseTime = "12h";
+        # dhcpLeaseTime = "12h";
       };
     };
   };
