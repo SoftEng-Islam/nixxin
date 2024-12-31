cb67c1f4cdc24ea0b19015db04dff9fc35dc420e
Islam Ahmed <softeng.islam@gmail.com>
2024-12-31 04:09:47 +0200

    update

--- profiles/desktop/system/wm/hyprland/default.nix
+++ profiles/desktop/system/wm/hyprland/default.nix
@@ -24,12 +24,12 @@
     WLR_NO_HARDWARE_CURSORS = "1"; # disable hardware cursors for wlroots
     NIXOS_XDG_OPEN_USE_PORTAL = "1"; # needed to open apps after web login
   };
+  home-manager.extraSpecialArgs = { inherit inputs settings self impurity; };
   home-manager.users.${settings.username} = {
     # home.file.".config/hypr/hyprland.conf".text = builtins.readFile ./hypr/hyprland.conf;
     # home.file.".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
     # home.file.".config/hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
     # home.file.".config/hypr/scripts/hyprlock-time.sh".source = ./hypr/scripts/hyprlock-time.sh;
-    extraSpecialArgs = { inherit inputs settings self impurity; };
 
     imports = [ ./end ];
   };
