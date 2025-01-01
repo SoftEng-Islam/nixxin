521d15dc40b52eae981a1345f5d692ea4596c5c6
Islam Ahmed <softeng.islam@gmail.com>
2025-01-01 06:28:39 +0200

    update

--- configs/modules/terminal/wezterm/default.nix
+++ configs/modules/terminal/wezterm/default.nix
@@ -1,6 +1,4 @@
 { pkgs, ... }: {
-  imports = [ ./wezterm.nix ];
-
   terminals.wezterm = let
     gnome-light = let
       fg = "#171717";
@@ -80,7 +78,7 @@
     };
   in {
     enable = true;
-    font = "CaskaydiaCove NF";
+    # font = "CaskaydiaCove NF";
 
     themes = {
       Dark = "Charmful Dark";
@@ -119,39 +117,5 @@
 
       default_prog = [ "${pkgs.tmux}/bin/tmux" ];
     };
-
-    extraLua = ''
-      local wa = wezterm.action
-
-      wezterm.on("padding-off", function(window)
-      	local overrides = window:get_config_overrides() or {}
-      	if not overrides.window_padding then
-      		overrides.window_padding = {
-      			top = "0",
-      			right = "0",
-      			bottom = "0",
-      			left = "0",
-      		}
-      	else
-      		overrides.window_padding = nil
-      	end
-      	window:set_config_overrides(overrides)
-      end)
-
-      wezterm.on("toggle-opacity", function(window)
-      	local overrides = window:get_config_overrides() or {}
-      	if not overrides.window_background_opacity then
-      		overrides.window_background_opacity = 0.7
-      	else
-      		overrides.window_background_opacity = nil
-      	end
-      	window:set_config_overrides(overrides)
-      end)
-
-      config.keys = {
-      	{ key = "p", mods = "CTRL", action = wa.EmitEvent("padding-off") },
-      	{ key = "o", mods = "CTRL", action = wa.EmitEvent("toggle-opacity") },
-      }
-    '';
   };
 }
