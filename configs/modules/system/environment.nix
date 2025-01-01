b982c877af33e2fa919a3c49202b94100ab0fae9
Islam Ahmed <softeng.islam@gmail.com>
2025-01-01 08:15:34 +0200

    update

--- configs/modules/system/environment.nix
+++ configs/modules/system/environment.nix
@@ -88,7 +88,7 @@
     };
     sessionVariables = {
       XCURSOR_THEME = settings.cursorTheme;
-      XCURSOR_SIZE = "${settings.cursorSize}";
+      # XCURSOR_SIZE = "${settings.cursorSize}";
       # WLR_NO_HARDWARE_CURSORS = 1; # IF your cursor becomes invisible
       # Enables portal-based access for apps like VSCode on Wayland
       GTK_USE_PORTAL = "1";
