49bee1d2b4559ff9a406c6a6f1b1f83d3ca989fc
Islam Ahmed <softeng.islam@gmail.com>
2025-01-03 15:12:26 +0200

    update

--- configs/modules/dev/python/default.nix
+++ configs/modules/dev/python/default.nix
@@ -3,50 +3,53 @@
     # Python --------------------------------
     meson # An open source, fast and friendly build system made in Python
     pyenv # Simple Python version management
-    python313Full
-
-    python313Packages.pip
-    python313Packages.beautifulsoup4
-    python313Packages.build
-    python313Packages.cairosvg
-    python313Packages.certifi
-    python313Packages.charset-normalizer
-    python313Packages.click
-    python313Packages.dbus-python
-    python313Packages.django
-    python313Packages.html2text
-    python313Packages.i3ipc
-    python313Packages.icalendar
-    python313Packages.idna
-    python313Packages.lockfile
-    python313Packages.loguru
-    python313Packages.markupsafe
-    python313Packages.material-color-utilities
-    python313Packages.materialyoucolor
-    python313Packages.opencv-python # Open Computer Vision Library with more than 500 algorithms
-    python313Packages.pillow
-    python313Packages.psutil
-    python313Packages.pycairo
-    python313Packages.pygobject3
-    python313Packages.pynvim # required by nvim
-    python313Packages.pytz
-    python313Packages.pywal
-    python313Packages.pywayland
-    python313Packages.pywlroots
-    python313Packages.requests
-    python313Packages.setuptools
-    python313Packages.setuptools-scm
-    python313Packages.sv-ttk
-    python313Packages.systemd
-    python313Packages.tkinter
-    python313Packages.tzlocal
-    python313Packages.urllib3
-    python313Packages.virtualenv
-    python313Packages.watchdog
-    python313Packages.wheel
-    python313Packages.ruff
-    python313Packages.mypy
-    python313Packages.jinja2
+    python3Full
+    python3Packages.pip
+    python3Packages.beautifulsoup4
+    python3Packages.build
+    python3Packages.cairosvg
+    python3Packages.certifi
+    python3Packages.charset-normalizer
+    python3Packages.click
+    python3Packages.dbus-python
+    python3Packages.django
+    python3Packages.html2text
+    python3Packages.i3ipc
+    python3Packages.icalendar
+    python3Packages.idna
+    python3Packages.lockfile
+    python3Packages.loguru
+    python3Packages.markupsafe
+    python3Packages.material-color-utilities
+    python3Packages.materialyoucolor
+    python3Packages.opencv-python # Open Computer Vision Library with more than 500 algorithms
+    python3Packages.pillow
+    python3Packages.psutil
+    python3Packages.pycairo
+    python3Packages.pygobject3
+    python3Packages.pynvim # required by nvim
+    python3Packages.pytz
+    python3Packages.pywal
+    python3Packages.pywayland
+    python3Packages.pywlroots
+    python3Packages.requests
+    python3Packages.setuptools
+    python3Packages.setuptools-scm
+    python3Packages.sv-ttk
+    python3Packages.systemd
+    python3Packages.tkinter
+    python3Packages.tzlocal
+    python3Packages.urllib3
+    python3Packages.virtualenv
+    python3Packages.watchdog
+    python3Packages.wheel
+    python3Packages.ruff
+    python3Packages.mypy
+    python3Packages.jinja2
+    python3Packages.jinja2-git
+    python3Packages.jinja2-time
+    python3Packages.jinja2-ansible-filters
+    python3Packages.jinja2-pluralize
 
     cairo.dev
     dbus.dev
