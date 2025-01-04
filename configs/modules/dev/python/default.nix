bb1c052df71a055157c236689374855cf5709cf1
Islam Ahmed <softeng.islam@gmail.com>
2025-01-04 10:20:51 +0200

    update

--- configs/modules/dev/python/default.nix
+++ configs/modules/dev/python/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 let
-  pythonWithOverrides = pkgs.python3.override {
+  pythonWithOverrides = pkgs.python313.override {
     packageOverrides = python-self: python-super: {
       pallets-sphinx-themes = python-super.pallets-sphinx-themes.overrideAttrs
         (old: {
@@ -22,77 +22,85 @@ in {
     # Python --------------------------------
     meson # An open source, fast and friendly build system made in Python
     pyenv # Simple Python version management
-    python3Full
-    python3Packages.babel
-    python3Packages.beautifulsoup4
-    python3Packages.build
-    python3Packages.cairosvg
-    python3Packages.certifi
-    python3Packages.charset-normalizer
-    python3Packages.click
-    python3Packages.dbus-python
-    python3Packages.django
-    python3Packages.flit-core
-    python3Packages.html2text
-    python3Packages.i3ipc
-    python3Packages.icalendar
-    python3Packages.idna
-    python3Packages.installer
-    python3Packages.lockfile
-    python3Packages.loguru
-    python3Packages.markupsafe
-    python3Packages.material-color-utilities
-    python3Packages.materialyoucolor
-    python3Packages.mypy
-    python3Packages.opencv-python # Open Computer Vision Library with more than 500 algorithms
-    python3Packages.pillow
-    python3Packages.pip
     pipx
-    python3Packages.psutil
-    python3Packages.pycairo
-    python3Packages.pygobject-stubs
-    python3Packages.pygobject3
-    python3Packages.pynvim # required by nvim
-    python3Packages.pytest
-    python3Packages.pytz
-    python3Packages.pywal
-    python3Packages.pywayland
-    python3Packages.pywlroots
-    python3Packages.requests
-    python3Packages.ruff
-    python3Packages.setuptools
-    python3Packages.setuptools-scm
-    python3Packages.sv-ttk
-    python3Packages.systemd
-    python3Packages.tkinter
-    python3Packages.trio
-    python3Packages.tzlocal
-    python3Packages.urllib3
-    python3Packages.virtualenv
-    python3Packages.watchdog
-    python3Packages.types-requests
-    python3Packages.wheel
+    python313Full
+    python313Packages.virtualenv
+    python313Packages.babel
+    python313Packages.beautifulsoup4
+    python313Packages.build
+    python313Packages.cairosvg
+    python313Packages.certifi
+    python313Packages.charset-normalizer
+    python313Packages.click
+    python313Packages.dbus-python
+    python313Packages.django
+    python313Packages.flit-core
+    python313Packages.html2text
+    python313Packages.i3ipc
+    python313Packages.icalendar
+    python313Packages.idna
+    python313Packages.installer
+    python313Packages.lockfile
+    python313Packages.loguru
+    python313Packages.markupsafe
+    python313Packages.material-color-utilities
+    python313Packages.materialyoucolor
+    python313Packages.mypy
+    python313Packages.opencv-python # Open Computer Vision Library with more than 500 algorithms
+    python313Packages.pillow
+    python313Packages.pip
+    python313Packages.psutil
+    python313Packages.pycairo
+    python313Packages.pygobject-stubs
+    python313Packages.pygobject3
+    python313Packages.pynvim # required by nvim
+    python313Packages.pytest
+    python313Packages.pytz
+    python313Packages.pywal
+    python313Packages.pywayland
+    python313Packages.pywlroots
+    python313Packages.requests
+    python313Packages.ruff
+    python313Packages.setuptools
+    python313Packages.setuptools-scm
+    python313Packages.sv-ttk
+    python313Packages.systemd
+    python313Packages.tkinter
+    python313Packages.trio
+    python313Packages.tzlocal
+    python313Packages.urllib3
+    python313Packages.virtualenv
+    python313Packages.watchdog
+    python313Packages.types-requests
+    python313Packages.wheel
 
     # python313Packages.jinja2
-
     pythonWithOverrides
     (pythonWithOverrides.withPackages
       (ps: with ps; [ jinja2 babel beautifulsoup4 ]))
 
+    bzip2
     cairo.dev
     dbus.dev
     devtoolbox # Development tools at your fingertips
     fakeroot # Give a fake root environment through LD_PRELOAD
+    gcc
     glib.dev
     gobject-introspection
     gobject-introspection.dev
+    libffi
     libffi.dev
     libtool # GNU Libtool, a generic library support script
     linuxHeaders
+    make
+    openssl
     pip-audit
     pkg-config
+    sqlite
     systemd.dev # System and service manager for Linux
     wrapGAppsHook
+    xz
+    zlib
 
   ];
 }
