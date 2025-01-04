774cc39f1ccd8a96c12e42e950d942a1536ed0e3
Islam Ahmed <softeng.islam@gmail.com>
2025-01-04 20:01:09 +0200

    update

--- configs/modules/terminal/zsh.nix
+++ configs/modules/terminal/zsh.nix
@@ -42,8 +42,8 @@
     # Load custom theme
     # source ~/.oh-my-zsh/themes/theme.zsh-theme
 
-    PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
-     %F{green}→%f "
+    PROMPT="%U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
+    %F{green}$%f "
     RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
     [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
     bindkey '^P' history-beginning-search-backward
