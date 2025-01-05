0d30acf4fbd07d7788523c4b2a2e5efde368cbc0
Islam Ahmed <softeng.islam@gmail.com>
2025-01-05 07:01:49 +0200

    update

--- configs/modules/applications.nix
+++ configs/modules/applications.nix
@@ -19,7 +19,7 @@
     # Video Editors
     kdenlive # Video editor
     davinci-resolve # Professional video editing, color, effects and audio post-processing
-    davinci-resolve-studio # Professional video editing, color, effects and audio post-processing
+    # davinci-resolve-studio # Professional video editing, color, effects and audio post-processing
 
     testdisk # Data recovery utilities
 
