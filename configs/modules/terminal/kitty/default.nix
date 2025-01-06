ca2bfd42a41e83a79d8f996d32b616c1eb124d22
Islam Ahmed <softeng.islam@gmail.com>
2025-01-06 16:42:57 +0200

    update

--- configs/modules/terminal/kitty/default.nix
+++ configs/modules/terminal/kitty/default.nix
@@ -2,6 +2,35 @@
   home-manager.users.${settings.username} = {
     home.file.".config/kitty/kitty.conf".text = ''
       # include ~/.config/kitty/base24.conf
+      # The 16 terminal colors
+      # normal
+      color0 #282c34
+      color1 #e05561
+      color2 #8cc265
+      color3 #e6b965
+      color4 #4aa5f0
+      color5 #c162de
+      color6 #42b3c2
+      color7 #abb2bf
+
+      # bright
+      color8 #4f5666
+      color9 #ff616e
+      color10 #a5e075
+      color11 #f0a45d
+      color12 #4dc4ff
+      color13 #de73ff
+      color14 #4cd1e0
+      color15 #ffffff
+
+      # extended base16 colors
+      color16 #d18f52
+      color17 #bf4034
+      color18 #2e3440
+      color19 #3f4859
+      color20 #6c7686
+      color21 #d8dee9
+
 
       # The basic colors
       background #282c34
@@ -40,38 +69,6 @@
       tab_title_max_length 25
       tab_title_template "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title}"
 
-
-
-
-      # The 16 terminal colors
-      # normal
-      color0 #282c34
-      color1 #e05561
-      color2 #8cc265
-      color3 #e6b965
-      color4 #4aa5f0
-      color5 #c162de
-      color6 #42b3c2
-      color7 #abb2bf
-
-      # bright
-      color8 #4f5666
-      color9 #ff616e
-      color10 #a5e075
-      color11 #f0a45d
-      color12 #4dc4ff
-      color13 #de73ff
-      color14 #4cd1e0
-      color15 #ffffff
-
-      # extended base16 colors
-      color16 #d18f52
-      color17 #bf4034
-      color18 #2e3440
-      color19 #3f4859
-      color20 #6c7686
-      color21 #d8dee9
-
       # URL underline color when hovering with mouse
       url_color #0087bd
       url_style curly
@@ -87,23 +84,20 @@
       bold_italic_font auto
       font_size ${toString settings.TerminalsFontSize}
 
-      terminal_select_modifiers shift
-      strip_trailing_spaces never
-
-      #adjust_line_height  30%
-      #adjust_column_width 5%
       input_delay 3
       kitty -o allow_remote_control=yes
       mouse_hide_wait 1
       repaint_delay 10
+      strip_trailing_spaces never
       sync_to_monitor yes
+      terminal_select_modifiers shift
       wayland_enable_ime no
       window_decorations none
 
       # Cursor
       cursor_shape beam
       cursor_shape_unfocused beam
-      cursor_beam_thickness 2.5
+      cursor_beam_thickness 3
       cursor_blink_interval 0.3 ease-in-out
       cursor_trail 1
       cursor_trail_decay 0.1 0.4
@@ -115,7 +109,6 @@
       scrollbar yes
 
       # Windows Customization
-      #window_margin_width 15
       remember_window_size no
       window_padding_width 14
       background_opacity 1
