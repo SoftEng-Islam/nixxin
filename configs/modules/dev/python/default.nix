43b9886789f7818746faba08880ba9ee6387674c
Islam Ahmed <softeng.islam@gmail.com>
2025-01-07 12:08:52 +0200

    update

--- configs/modules/dev/python/default.nix
+++ configs/modules/dev/python/default.nix
@@ -9,22 +9,24 @@ in {
   # imports = [ ./globalEnv.nix ];
   # Set environment variables for Python
   environment.sessionVariables = {
-    PYENV_ROOT = "$HOME/.pyenv";
+    # PYENV_ROOT = "$HOME/.pyenv";
+    PYENV_ROOT = "${pkgs.pyenv}";
     # pyenv flags to be able to install Python
     CPPFLAGS =
-      "-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
+      "-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl_3_3.dev}/include";
     CXXFLAGS =
-      "-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
-    CFLAGS = "-I${pkgs.openssl.dev}/include";
+      "-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl_3_3.dev}/include";
+    CFLAGS = "-I${pkgs.openssl_3_3.dev}/include";
     LDFLAGS =
-      "-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib";
-    CONFIGURE_OPTS = "-with-openssl=${pkgs.openssl.dev}";
+      "-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl_3_3.out}/lib";
+    CONFIGURE_OPTS = "-with-openssl=${pkgs.openssl_3_3.dev}";
     PYENV_VIRTUALENV_DISABLE_PROMPT = "1";
     PYTHONPATH = "${pkgs.python3.sitePackages}:$PYTHONPATH";
     LD_LIBRARY_PATH = lib.mkForce (lib.makeLibraryPath ldLibraryPaths);
     PYTHON_BUILD_HOOK = "echo 'Using NixOS dependencies'";
     PYTHON_CFLAGS = "$CPPFLAGS";
     PYTHON_LDFLAGS = "$LDFLAGS";
+    PYTHON_CONFIGURE_OPTS = "--enable-shared";
     PATH = "$PYENV_ROOT/bin:$PATH";
     # PIP_PREFIX = "$(pwd)/_build/pip_packages";
     # CPPFLAGS = "-I${pkgs.readline.dev}/include";
@@ -34,44 +36,56 @@ in {
     # LDFLAGS = "-L${pkgs.zlib.dev}/lib -L${pkgs.libffi.dev}/lib -L${pkgs.readline.dev}/lib -L${pkgs.bzip2.dev}/lib -L${pkgs.openssl.dev}/lib";
   };
   environment.systemPackages = with pkgs; [
-    # Python
-    meson # An open source, fast and friendly build system made in Python
-    pyenv # Simple Python version management
-    pyenv.out
-    pychess # Advanced GTK chess client written in Python
-    pipx # Install and run Python applications in isolated environments
     python312Full # High-level dynamically-typed programming language
+    python310
+    python314
+    openssl_3_3
+    autoconf
+    automake
+    base16-schemes
     bzip2 # High-quality data compression program
     bzip2.dev
+    c-ares
     cairo
     cairo.dev
+    clang
+    cmake
     dbus.dev
     devtoolbox # Development tools at your fingertips
     fakeroot # Give a fake root environment through LD_PRELOAD
+    gcc
     gdbm
     gdbm.dev
+    git
     glib.dev
     gnumake
     gobject-introspection
     gobject-introspection.dev
+    icu
+    libedit
     libffi
     libffi.dev
+    libtool
     libtool # GNU Libtool, a generic library support script
     libuuid
     linuxHeaders
-    # makeWrapper
+    lz4
+    meson # An open source, fast and friendly build system made in Python
     ncurses
     ncurses.dev
+    nghttp2
     openssl
     openssl.dev
     pip-audit
+    pipx # Install and run Python applications in isolated environments
     pkg-config
     pkgconf
-    pyenv
-    base16-schemes
+    pychess # Advanced GTK chess client written in Python
+    pyenv # Simple Python version management
+    pyenv.out
+    pypy3
     readline
     readline.dev
-    libedit
     sqlite
     sqlite.dev
     systemd.dev # System and service manager for Linux
@@ -81,26 +95,9 @@ in {
     xz
     xz.dev
     zlib
-    zlib.dev
     zlib-ng
     zlib-ng.dev
-    # buildLinux
-    git
-    gcc
-    clang
-    c-ares
-    gnumake
-    cmake
-    nghttp2
-    autoconf
-    automake
-    libtool
-    ncurses
-    openssl
-    icu
-    zlib
-    lz4
-    pypy3
+    zlib.dev
     (python3.withPackages (subpkgs: with subpkgs; [ requests ]))
     (python3.withPackages (ps:
       with ps; [
