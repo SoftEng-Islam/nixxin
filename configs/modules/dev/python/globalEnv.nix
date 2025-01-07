01b07ec5eef2c16c2057dfab6f542c9f263079d4
Islam Ahmed <softeng.islam@gmail.com>
2025-01-07 07:14:59 +0200

    update

--- configs/modules/dev/python/globalEnv.nix
+++ configs/modules/dev/python/globalEnv.nix
@@ -1,27 +1,21 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    # Python
-    appdirs
-    dbus-python
-    evdev
-    hatchling
-    inotify-simple
-    material-color-utilities
-    materialyoucolor
-    ordered-set
-    pillow
-    pip
-    poetry-core
-    psutil
-    pycairo
-    pydbus
-    pygobject3
-    pywal
-    pywayland
-    setuptools-scm
-    six
-    watchdog
-    wheel
-    xkeysnail
-  ];
+  environment.systemPackages = with pkgs;
+    with pkgs.python3Packages; [
+      # Python
+      evdev-proto
+      pillow
+      pip
+      poetry-core
+      psutil
+      pycairo
+      pydbus
+      pygobject3
+      pywal
+      pywayland
+      setuptools-scm
+      six
+      watchdog
+      wheel
+      xkeysnail
+    ];
 }
