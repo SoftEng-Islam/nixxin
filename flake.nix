befbf46fd4f83272444071d54ae21b233463a664
Islam Ahmed <softeng.islam@gmail.com>
2025-01-09 13:46:44 +0200

    update

--- flake.nix
+++ flake.nix
@@ -1,43 +1,41 @@
 {
-  description = "NixOS configuration";
+  description = "Nixxin Configuration.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # superfile.url = "github:yorukot/superfile";
     xremap-flake.url = "github:xremap/nix-flake";
 
     # System-wide colorscheming and typography for NixOS
     stylix.url = "github:danth/stylix";
 
-    # Efficient animated wallpaper daemon for wayland, controlled at runtime
+    # Efficient Animated Wallpaper Daemon For Wayland, controlled at runtime
     swww.url = "github:LGFae/swww";
 
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
-    aagl.url = "github:ezKEa/aagl-gtk-on-nix";
-    aagl.inputs.nixpkgs.follows = "nixpkgs";
-
-    # ignis.url = "github:linkfrg/ignis";
-    # ignis.inputs.nixpkgs.follows = "nixpkgs";
+    # aagl.url = "github:ezKEa/aagl-gtk-on-nix";
+    # aagl.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-ld.url = "github:Mic92/nix-ld";
     nix-ld.inputs.nixpkgs.follows = "nixpkgs";
 
-    impurity.url = "github:outfoxxed/impurity.nix";
-    thorium.url = "github:end-4/nix-thorium";
+    # impurity.url = "github:outfoxxed/impurity.nix";
+    # thorium.url = "github:end-4/nix-thorium";
+
+    # ignis.url = "github:linkfrg/ignis";
+    # ignis.inputs.nixpkgs.follows = "nixpkgs";
 
     hyprland.url = "github:hyprwm/Hyprland/?submodules=true";
     hyprland.inputs.nixpkgs.follows = "nixpkgs"; # MESA/OpenGL HW workaround
 
-    hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
+    # hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
     # hyprland-plugins.inputs.hyprland.follows = "hyprland";
-
-    # A material you color generation tool
-    matugen.url = "github:/InioX/Matugen";
 
     # hyprspace.url = "github:hyprspace/hyprspace";
     # hyprspace.inputs.flake-parts.follows = "flake-parts";
@@ -48,6 +46,9 @@
 
     pyprland.url = "github:hyprland-community/pyprland";
     pyprland.inputs.nixpkgs.follows = "nixpkgs";
+
+    # A Material You Color Generation Tool
+    matugen.url = "github:/InioX/Matugen";
 
     anyrun.url = "github:Kirottu/anyrun";
     anyrun.inputs.nixpkgs.follows = "nixpkgs";
