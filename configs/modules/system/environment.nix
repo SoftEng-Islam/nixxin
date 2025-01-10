19923d493bc5ff4f7b1d0f2e3b9d95c674ce348a
Islam Ahmed <softeng.islam@gmail.com>
2025-01-10 14:38:42 +0200

    update

--- configs/modules/system/environment.nix
+++ configs/modules/system/environment.nix
@@ -9,10 +9,10 @@
     variables = {
       # Add Ignis, Python, npm, yarn, and Sass paths
       PATH = [
-        "${pkgs.nodejs}/bin"
+        # "${pkgs.nodejs}/bin"
         "${pkgs.nodePackages.npm}/bin"
-        "${pkgs.python3}/bin"
-        "${pkgs.sass}/bin"
+        # "${pkgs.python3}/bin"
+        # "${pkgs.sass}/bin"
         "${pkgs.yarn}/bin"
         "$HOME/.bun/bin"
         "$HOME/.cache/ignis/bin" # If Ignis places executables here
