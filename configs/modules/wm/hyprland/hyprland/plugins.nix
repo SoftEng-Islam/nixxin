d39b5cf77c01056a11b6ec183bcd19028d35bfc8
Islam Ahmed <softeng.islam@gmail.com>
2025-01-11 09:23:58 +0200

    update

--- configs/modules/wm/hyprland/hyprland/plugins.nix
+++ configs/modules/wm/hyprland/hyprland/plugins.nix
@@ -23,6 +23,7 @@
       hyprbars = {
         # Honestly idk if it works like css, but well, why not
         bar_text_font = "${settings.fontName}";
+        bar_text_size = 12;
         bar_height = 30;
         bar_padding = 10;
         bar_button_padding = 5;
@@ -32,9 +33,9 @@
         col.text = "rgba(DCE3EEFF)";
         # example buttons (R -> L)
         # hyprbars-button = color, size, on-click
-        # hyprbars-button = "rgb(DCE3EE), 13, 󰖭, hyprctl dispatch killactive;";
-        # hyprbars-button = "rgb(DCE3EE), 13, 󰖯, hyprctl dispatch fullscreen 1;";
-        # hyprbars-button = "rgb(DCE3EE), 13, 󰖰, hyprctl dispatch movetoworkspacesilent special;";
+        # hyprbars-button = rgb(ff4040), 20, , hyprctl dispatch killactive
+        # hyprbars-button = rgb(eeee11), 20, , hyprctl dispatch fullscreen 1
+        # hyprbars-button = rgb(eeee21), 20, , hyprctl dispatch togglefloating
       };
     };
   };
