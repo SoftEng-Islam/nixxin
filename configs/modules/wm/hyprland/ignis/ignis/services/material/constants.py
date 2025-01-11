4ffaade2813349966512703263af6e1177715855
Islam Ahmed <softeng.islam@gmail.com>
2025-01-11 11:52:17 +0200

    update

--- configs/modules/wm/hyprland/ignis/ignis/services/material/constants.py
+++ configs/modules/wm/hyprland/ignis/ignis/services/material/constants.py
@@ -6,6 +6,6 @@ from ignis.utils import Utils
 MATERIAL_CACHE_DIR = f"{ignis.CACHE_DIR}/material"  # type: ignore
 
 TEMPLATES = Utils.get_current_dir() + "/templates"
-SAMPLE_WALL = Utils.get_current_dir() + "/sample_wall.png"
+SAMPLE_WALL = Utils.get_current_dir() + "/default.png"
 
 os.makedirs(MATERIAL_CACHE_DIR, exist_ok=True)
