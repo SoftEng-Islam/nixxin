371d54a91c73a961de8aaaf3c7be2ed0c050b8d5
Islam Ahmed <softeng.islam@gmail.com>
2025-01-11 09:45:13 +0200

    update

--- configs/modules/wm/hyprland/hyprland/exec.nix
+++ configs/modules/wm/hyprland/hyprland/exec.nix
@@ -1,43 +1,45 @@
 { settings, pkgs, ... }: {
-  wayland.windowManager.hyprland.settings = {
-    exec-once = [
-      # "swww init"
-      "swww-daemon --format xrgb &"
-      # "swww img ~/Downloads/nixos-chan.png"
+  home-manager.users.${settings.username} = {
+    wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        # "swww init"
+        "swww-daemon --format xrgb &"
+        # "swww img ~/Downloads/nixos-chan.png"
 
-      # Input method
-      "fcitx5"
+        # Input method
+        "fcitx5"
 
-      # Core components (authentication, lock screen, notification daemon)
-      "gnome-keyring-daemon --start --components=secrets"
+        # Core components (authentication, lock screen, notification daemon)
+        "gnome-keyring-daemon --start --components=secrets"
 
-      # polkit-gnome on nixos
-      "${pkgs.polkit_gnome}/polkit-gnome-authentication-agent-1 &"
+        # polkit-gnome on nixos
+        "${pkgs.polkit_gnome}/polkit-gnome-authentication-agent-1 &"
 
-      "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-      # "dbus-update-activation-environment --all"
-      # "sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP" # Some fix idk
+        "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+        # "dbus-update-activation-environment --all"
+        # "sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP" # Some fix idk
 
-      "hyprpm reload"
+        "hyprpm reload"
 
-      # "hypridle"
+        # "hypridle"
 
-      # Clipboard: history
-      # "clipman store &"
-      # "wl-paste --watch cliphist store &"
-      "wl-paste --type text --watch cliphist store"
-      "wl-paste --type image --watch cliphist store"
+        # Clipboard: history
+        # "clipman store &"
+        # "wl-paste --watch cliphist store &"
+        "wl-paste --type text --watch cliphist store"
+        "wl-paste --type image --watch cliphist store"
 
-      # Cursor
-      "hyprctl setcursor ${settings.cursorTheme}  ${
-        toString settings.cursorSize
-      }"
+        # Cursor
+        "hyprctl setcursor ${settings.cursorTheme}  ${
+          toString settings.cursorSize
+        }"
 
-      # To start ignis
-      "ignis init"
+        # To start ignis
+        "ignis init"
 
-      # Apps To Start
-      # "telegram-desktop -startintray"
-    ];
+        # Apps To Start
+        # "telegram-desktop -startintray"
+      ];
+    };
   };
 }
