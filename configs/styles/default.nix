246aa0425e714c20b7e6b689c7530349a6abf4ee
Islam Ahmed <softeng.islam@gmail.com>
2025-01-16 15:04:57 +0200

    update

--- configs/styles/default.nix
+++ configs/styles/default.nix
@@ -1,20 +1,41 @@
 { pkgs, lib, settings, ... }:
 let
-  base24 = builtins.fetchurl {
-    url =
-      "file:///home/${settings.username}/nixxin/configs/styles/base24.yaml"; # Local path to the file
-    # To Generate The sha256
-    # sha256sum ~/nixxin/configs/styles/base24.yaml
-    sha256 =
-      "8aa7ecf8e86f7149f958ad9674f5ca0766bef6d9d2775ebb425186d42cc56049"; # The hash you got
-  };
+  # base24 = builtins.fetchurl {
+  #   url =
+  #     "file:///home/${settings.username}/nixxin/configs/styles/base24.yaml"; # Local path to the file
+  #   # To Generate The sha256
+  #   # sha256sum ~/nixxin/configs/styles/base24.yaml
+  #   sha256 =
+  #     "8aa7ecf8e86f7149f958ad9674f5ca0766bef6d9d2775ebb425186d42cc56049"; # The hash you got
+  # };
 in {
   stylix = {
     enable = true;
     image = ./wallpapers/eveningSky.png;
     polarity = "dark";
     # base16Scheme = ${pkgs.base16-schemes}/share/themes/${settings.themeName}.yaml
-    base16Scheme = base24;
+    # base16Scheme = base24;
+    base16Scheme = {
+      author = "Islam Ahmed";
+      scheme = "Nixxin";
+      slug = "nixxin";
+      base00 = "191724";
+      base01 = "1f1d2e";
+      base02 = "26233a";
+      base03 = "6e6a86";
+      base04 = "908caa";
+      base05 = "e0def4";
+      base06 = "e0def4";
+      base07 = "524f67";
+      base08 = "e91e63";
+      base09 = "ff9800";
+      base0A = "ffeb3b";
+      base0B = "4caf50";
+      base0C = "00bcd4";
+      base0D = "3f51b5";
+      base0E = "673ab7";
+      base0F = "f44336";
+    };
     opacity = {
       # terminal = settings.opacity;
       # applications = settings.opacity;
