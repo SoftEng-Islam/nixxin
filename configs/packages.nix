c6ea670b043183ff25f727ca1ec9164568aa282f
Islam Ahmed <softeng.islam@gmail.com>
2025-01-21 14:20:06 +0200

    Updates & Improvments

--- configs/packages.nix
+++ configs/packages.nix
@@ -6,31 +6,8 @@
     settings.termPkg
     settings.cursorPackage
 
-    # ---- Screenshot ---- #
-    grim # Grab images from a Wayland compositor
-    grimblast # A helper for screenshots within Hyprland, based on grimshot
-    swappy # A Wayland native snapshot editing tool, inspired by Snappy on macOS
-    flameshot # Powerful yet simple to use screenshot software
-    scrot # A command-line screen capture utility
-    wayshot # A native, blazing-fast screenshot tool for wlroots based compositors such as sway and river
-
-    # ---- Editors ---- #
-    vscode # Open source source code editor developed by Microsoft for Windows, Linux and macOS
-    zed-editor # Zed editor (like vscode)
-    neovim # Vim text editor fork focused on extensibility and agility
-
-    # ---- clipboard ---- #
-    cliphist # Wayland clipboard manager
-    wl-clipboard-rs # Command-line copy/paste utilities for Wayland
-    # gpaste # Clipboard management system with GNOME integration
-    # clipman # A simple clipboard manager for Wayland
-
-    # ---- Text Search ---- #
-    # silver-searcher # Code-searching tool similar to ack, but faster
-    # ripgrep # Utility that combines the usability of The Silver Searcher with the raw speed of grep
-
     proot # User-space implementation of chroot, mount --bind and binfmt_misc
-    matugen # A material you color generation tool
+    # matugen # A material you color generation tool
 
     # ---- Disks & Filesystem ---- #
     gvfs # Virtual Filesystem support library
@@ -59,11 +36,6 @@
     nettle # Cryptographic library
     libgcrypt # General-purpose cryptographic library
 
-    # ---- Image Viewers ---- #
-    imv # A command line image viewer for tiling window managers
-    feh # A light-weight image viewer
-    loupe # A simple image viewer application written with GTK4 and Rust
-
     # ---- Notifications ---- #
     notify # Notify allows sending the output from any tool to Slack, Discord and Telegram
     libnotify # A library that sends desktop notifications to a notification daemon
@@ -90,9 +62,7 @@
     # Other Tools
     upower # D-Bus service for power management
     bpftune # BPF-based auto-tuning of Linux system parameters
-    axel # Console downloading program with some features for parallel connections for faster downloading
     ddcutil # Query and change Linux monitor settings using DDC/CI and USB
-    gammastep # Screen color temperature manager
     gojq # Pure Go implementation of jq
     rsync # Fast incremental file transfer utility
     starship # Minimal, blazing fast, and extremely customizable prompt for any shell
@@ -109,38 +79,30 @@
     # developers packages
     # bintools # Tools for manipulating binaries (linker, assembler, etc.) (wrapper script)
     at-spi2-atk # Assistive Technology Service Provider Interface protocol definitions and daemon for D-Bus
-    atk.dev
     atkmm # C++ wrappers for ATK accessibility toolkit
     bun # Incredibly fast JavaScript runtime, bundler, transpiler and package manager – all in one
     cairo # A 2D graphics library with support for multiple output devices
-    cairo.dev
     cairomm # C++ bindings for the Cairo vector graphics library
     gdk-pixbuf # A library for image loading and manipulation
-    gdk-pixbuf.dev
     glib # C library of programming buildings blocks
     glib-networking
-    glib.dev
     glibc
     glibc_memusage
-    glibc.dev
     glibtool
     gobject-introspection # A middleware layer between C libraries and language bindings
     gobject-introspection-unwrapped
     gobject-introspection.dev
     gtksourceviewmm
     harfbuzz # An OpenType text shaping engine
-    harfbuzz.dev
     jetbrains-toolbox
 
     libgcc
     libpulseaudio
-    libpulseaudio.dev
     llvmPackages_12.bintools # System binary utilities (wrapper script)
     meson
     mlocate
     nodejs_20
     pango # A library for laying out and rendering of text, with an emphasis on internationalization
-    pango.dev
     pkg-config # A tool that allows packages to find out information about other packages (wrapper script)
     tinyxml-2
     zlib # Lossless data-compression library
@@ -153,10 +115,10 @@
 
     # Sass (Css)
     dart-sass # The reference implementation of Sass, written in Dart
-    libsass # A C/C++ implementation of a Sass compiler
-    rsass # Sass reimplemented in rust with nom
-    grass-sass # A Sass compiler written purely in Rust
-    sassc # A front-end for libsass
+    # libsass # A C/C++ implementation of a Sass compiler
+    # rsass # Sass reimplemented in rust with nom
+    # grass-sass # A Sass compiler written purely in Rust
+    # sassc # A front-end for libsass
 
     # Ruby
     ruby_3_3 # An object-oriented language for quick and easy programming
