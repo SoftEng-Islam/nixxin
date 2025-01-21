916bbd49c4e9faf1b46f151b9405db0fd8ec74d0
Islam Ahmed <softeng.islam@gmail.com>
2025-01-21 08:43:46 +0200

    Updates & Improvments

---
+++ configs/modules/system/default.nix
@@ -0,0 +1,21 @@
+{ settings, pkgs, ... }: {
+  imports = [
+    ./audio.nix
+    ./boot.nix
+    ./dbus.nix
+    ./dm.nix
+    ./env.nix
+    ./locale.nix
+    ./networking.nix
+    ./power.nix
+    ./programs.nix
+    ./sddm.nix
+    ./security.nix
+    ./services.nix
+    ./systemd.nix
+    ./users.nix
+    ./utilities.nix
+    ./virtualisation.nix
+    ./zram.nix
+  ];
+}
