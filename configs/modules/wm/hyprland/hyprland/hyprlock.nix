ff444de146cdaedf75f31fe7df09d0aa696d14c6
Islam Ahmed <softeng.islam@gmail.com>
2025-01-21 10:26:14 +0200

    Updates & Improvments

--- configs/modules/wm/hyprland/hyprland/hyprlock.nix
+++ configs/modules/wm/hyprland/hyprland/hyprlock.nix
@@ -20,24 +20,24 @@
         #   vibrancy = 0.17;
         #   vibrancy_darkness = 0;
         # }];
-        input-field = [{
-          monitor = "";
-          size = "250, 60";
-          outline_thickness = 2;
-          dots_size = 0.2;
-          dots_spacing = 0.2;
-          dots_center = true;
-          outer_color = "rgba(0, 0, 0, 0)";
-          inner_color = "rgba(0, 0, 0, 0.5)";
-          font_color = "rgb(200, 200, 200)";
-          fade_on_empty = false;
-          placeholder_text =
-            ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
-          hide_input = false;
-          position = "0, -120";
-          halign = "center";
-          valign = "center";
-        }];
+        # input-field = [{
+        #   monitor = "";
+        #   size = "250, 60";
+        #   outline_thickness = 2;
+        #   dots_size = 0.2;
+        #   dots_spacing = 0.2;
+        #   dots_center = true;
+        #   outer_color = "rgba(0, 0, 0, 0)";
+        #   inner_color = "rgba(0, 0, 0, 0.5)";
+        #   font_color = "rgb(200, 200, 200)";
+        #   fade_on_empty = false;
+        #   placeholder_text =
+        #     ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
+        #   hide_input = false;
+        #   position = "0, -120";
+        #   halign = "center";
+        #   valign = "center";
+        # }];
         label = [{
           monitor = "";
           text = "$TIME";
