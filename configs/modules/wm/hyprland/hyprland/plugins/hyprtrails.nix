80a4d28d88de9d12cc336d4f6083c771cad0c007
Islam Ahmed <softeng.islam@gmail.com>
2025-01-27 08:15:58 +0200

    update

--- configs/modules/wm/hyprland/hyprland/plugins/hyprtrails.nix
+++ configs/modules/wm/hyprland/hyprland/plugins/hyprtrails.nix
@@ -2,7 +2,7 @@
   home-manager.users.${settings.username} = {
     wayland.windowManager.hyprland.plugins = [
       pkgs.hyprlandPlugins.hyprtrails
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
+      # inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
     ];
     wayland.windowManager.hyprland.extraConfig = ''
       plugin:hyprtrails {
