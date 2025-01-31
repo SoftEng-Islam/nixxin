9e3240f21b6b0b50921f7042b1ec223e9d461183
Islam Ahmed <softeng.islam@gmail.com>
2025-01-31 18:35:05 +0200

    update

--- modules/desktop/media/default.nix
+++ modules/desktop/media/default.nix
@@ -1,41 +1,39 @@
-{ settings, pkgs, ... }: {
+{ lib, settings, pkgs, ... }: {
   imports = [ ];
 
-  environment.systemPackages = with pkgs; [
-    # Audio Control
-    pulsemixer
-    pwvucontrol
+  environment.systemPackages = with pkgs;
+  # ---- Media Players ---- #
+  # mpv is a free and open-source general-purpose video player, based on the MPlayer and mplayer2 projects, with great improvements above both.
+    (lib.optional settings.system.mediaPlayers.mpv mpv.override {
+      scripts = [ mpvScripts.mpris ];
+    }) ++ [
+      # Audio Control
+      pulsemixer
+      pwvucontrol
 
-    # Command-line utility and library for controlling media players that implement MPRIS
-    playerctl
+      # Command-line utility and library for controlling media players that implement MPRIS
+      playerctl
 
-    # ---- Media Players ---- #
-    # mpv is a free and open-source general-purpose video player, based on the MPlayer and mplayer2 projects, with great improvements above both.
-    (if settings.system.mediaPlayers.mpv then
-      mpv.override { scripts = [ mpvScripts.mpris ]; }
-    else
-      "")
+      # Simple GTK frontend for the mpv video player
+      # (if settings.system.mediaPlayers.celluloid then celluloid else "")
 
-    # Simple GTK frontend for the mpv video player
-    (if settings.system.mediaPlayers.celluloid then celluloid else "")
+      # Cross-platform media player and streaming server
+      # (if settings.system.mediaPlayers.vlc then vlc else "")
 
-    # Cross-platform media player and streaming server
-    (if settings.system.mediaPlayers.vlc then vlc else "")
+      # A GNOME media player built using GTK4 toolkit and powered by GStreamer with OpenGL rendering
+      # (if settings.system.mediaPlayers.clapper then clapper else "")
 
-    # A GNOME media player built using GTK4 toolkit and powered by GStreamer with OpenGL rendering
-    (if settings.system.mediaPlayers.clapper then clapper else "")
+      # Linux/macOS media player based on GStreamer and GTK
+      # (if settings.system.mediaPlayers.glide then glide-media-player else "")
 
-    # Linux/macOS media player based on GStreamer and GTK
-    (if settings.system.mediaPlayers.glide then glide-media-player else "")
+      # Jellyfin Desktop Client based on Plex Media Player
+      # (if settings.system.mediaPlayers.jellyfin then
+      #   jellyfin-media-player
+      # else
+      #   "")
 
-    # Jellyfin Desktop Client based on Plex Media Player
-    (if settings.system.mediaPlayers.jellyfin then
-      jellyfin-media-player
-    else
-      "")
-
-    # ---- Video Editors ---- #
-    (if settings.system.videoEditors.kdenlive then kdenlive else "")
-    (if settings.system.videoEditors.shotcut then shotcut else "")
-  ];
+      # ---- Video Editors ---- #
+      # (if settings.system.videoEditors.kdenlive then kdenlive else "")
+      # (if settings.system.videoEditors.shotcut then shotcut else "")
+    ];
 }
