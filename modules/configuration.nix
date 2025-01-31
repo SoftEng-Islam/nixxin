f855ffeef2472e3aa205197463da602fd4738597
Islam Ahmed <softeng.islam@gmail.com>
2025-01-31 14:13:02 +0200

    update

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on your system. Help is available in the
 # configuration.nix(5) man page and in the NixOS manual (accessible by running ‘nixos-help’).
 { lib, settings, pkgs, ... }: {
-  imports = (lib.optional settings.system.features.btop.enable [ ./btop ]) ++ [
+  imports = (lib.optionals settings.system.features.btop.enable [ ./btop ]) ++ [
     ./android
     ./apps_launcher
     ./cli
