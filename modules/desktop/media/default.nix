19956e25a5dad9110e389839e11f084cfc9cc835
Islam Ahmed <softeng.islam@gmail.com>
2025-01-31 18:29:01 +0200

    update

--- modules/desktop/media/default.nix
+++ modules/desktop/media/default.nix
@@ -11,31 +11,34 @@
 
     # ---- Media Players ---- #
     # mpv is a free and open-source general-purpose video player, based on the MPlayer and mplayer2 projects, with great improvements above both.
-    (if settings.features.mediaPlayers.mpv then
+    (if settings.system.features.mediaPlayers.mpv then
       mpv.override { scripts = [ mpvScripts.mpris ]; }
     else
       "")
 
     # Simple GTK frontend for the mpv video player
-    (if settings.features.mediaPlayers.celluloid then celluloid else "")
+    (if settings.system.features.mediaPlayers.celluloid then celluloid else "")
 
     # Cross-platform media player and streaming server
-    (if settings.features.mediaPlayers.vlc then vlc else "")
+    (if settings.system.features.mediaPlayers.vlc then vlc else "")
 
     # A GNOME media player built using GTK4 toolkit and powered by GStreamer with OpenGL rendering
-    (if settings.features.mediaPlayers.clapper then clapper else "")
+    (if settings.system.features.mediaPlayers.clapper then clapper else "")
 
     # Linux/macOS media player based on GStreamer and GTK
-    (if settings.features.mediaPlayers.glide then glide-media-player else "")
+    (if settings.system.features.mediaPlayers.glide then
+      glide-media-player
+    else
+      "")
 
     # Jellyfin Desktop Client based on Plex Media Player
-    (if settings.features.mediaPlayers.jellyfin then
+    (if settings.system.features.mediaPlayers.jellyfin then
       jellyfin-media-player
     else
       "")
 
     # ---- Video Editors ---- #
-    (if settings.features.videoEditors.kdenlive then kdenlive else "")
-    (if settings.features.videoEditors.shotcut then shotcut else "")
+    (if settings.system.features.videoEditors.kdenlive then kdenlive else "")
+    (if settings.system.features.videoEditors.shotcut then shotcut else "")
   ];
 }
