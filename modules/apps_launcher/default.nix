82f6e89089d460714fd99162f564c80376ffb417
Islam Ahmed <softeng.islam@gmail.com>
2025-01-31 13:02:49 +0200

    update

--- modules/apps_launcher/default.nix
+++ modules/apps_launcher/default.nix
@@ -1,7 +1,13 @@
 { settings, pkgs, ... }: {
   imports = [
     ./albert.nix
-    (if settings.system.features.apps_launcher then ./albert.nix else null)
-    (if settings.system.features.apps_launcher then ./ulauncher.nix else null)
+    (if settings.system.features.apps_launcher.albert then
+      ./albert.nix
+    else
+      null)
+    (if settings.system.features.apps_launcher.ulauncher then
+      ./ulauncher.nix
+    else
+      null)
   ];
 }
