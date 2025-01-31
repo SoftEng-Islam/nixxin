4347ce485fe62e7caf0462fa4da2a4c9d58deb1b
Islam Ahmed <softeng.islam@gmail.com>
2025-01-31 17:17:24 +0200

    update

--- modules/system/window_manager/default.nix
+++ modules/system/window_manager/default.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, settings, ... }: {
-  imports = (lib.optional settings.hyprland.enable [ ./hyprland ]);
+  imports = [ ./hyprland ];
   services.seatd.enable = lib.mkForce false;
   services.seatd.user = "root";
 }
