8f58fc3103ee92d27e510487762a99320151e157
Islam Ahmed <softeng.islam@gmail.com>
2025-01-31 17:14:52 +0200

    update

--- modules/system/window_manager/hyprland/default.nix
+++ modules/system/window_manager/hyprland/default.nix
@@ -1,5 +1,29 @@
 { pkgs, lib, settings, ... }: {
-  imports = [ ./packages.nix ./configs ];
+  imports = [
+    ./packages.nix
+
+    ./configs/animations.nix
+    ./configs/binds.nix
+    ./configs/cursor.nix
+    ./configs/decoration.nix
+    ./configs/env.nix
+    ./configs/exec.nix
+    ./configs/general.nix
+    ./configs/gestures.nix
+    # ./configs/hypridle.nix
+    # ./configs/hyprlock.nix
+    # ./configs/hyprpaper.nix
+    ./configs/input.nix
+    ./configs/keybinding.nix
+    ./configs/misc.nix
+    ./configs/monitor.nix
+    ./configs/plugins
+    ./configs/render.nix
+    ./configs/rules.nix
+    # ./configs/scripts.nix
+    ./configs/source.nix
+
+  ];
   programs = {
     uwsm.enable = false;
     hyprlock.enable = true;
@@ -98,4 +122,66 @@
     # Scripts for Hyprland
     home.file.".config/hypr/scripts".source = ./configs/scripts;
   };
+  environment.systemPackages = with pkgs; [
+    # Install Hyprland from Inputes (latest version)
+    # inputs.hyprland.packages.${system}.hyprland
+
+    # A wlroots-compatible Wayland color picker that does not suck
+    # inputs.hyprpicker.packages.${system}.hyprpicker
+    # inputs.hyprpolkitagent.packages."${system}".hyprpolkitagent
+    hyprpolkitagent
+
+    # albert # Fast and flexible keyboard launcher
+    # hyprgui # unstable GUI for configuring Hyprland written in Rust
+    fd # A simple, fast and user-friendly alternative to find
+    gtk-engine-murrine # for gtk themes
+    hyprcursor # The hyprland cursor format, library and utilities
+    hyprland
+    hyprland-protocols # Wayland protocol extensions for Hyprland
+    hyprlang # The official implementation library for the hypr config language
+    hyprlock # Hyprland's GPU-accelerated screen locking utility
+    hyprpaper # A blazing fast wayland wallpaper utility
+    hyprpicker
+    hyprshot # Hyprshot is an utility to easily take screenshots in Hyprland using your mouse.
+    hyprutils # Small C++ library for utilities used across the Hypr* ecosystem
+    # hyprwayland-scanner # A Hyprland version of wayland-scanner in and for C++
+    hyprprop
+
+    # gui
+    yad
+
+    # tools
+    gojq
+    showmethekey
+    ydotool
+
+    # hyprland
+    temurin-jre-bin
+    grim
+    tesseract
+    imagemagick
+    pavucontrol
+    playerctl
+    swappy
+    slurp
+    swww
+    wayshot
+    wlsunset
+    wf-recorder
+
+    # langs
+    nodejs
+    bun
+    cargo
+    go
+    typescript
+    eslint
+
+    # very important stuff
+    uwuify
+
+    adwaita-qt6
+    adw-gtk3
+    material-symbols
+  ];
 }
