af403536437357cc42720adf0453f672f6a10fc3
Islam Ahmed <softeng.islam@gmail.com>
2025-01-31 18:22:48 +0200

    update

--- modules/system/statusbar/ashell.nix
+++ modules/system/statusbar/ashell.nix
@@ -6,9 +6,11 @@
       (import (pkgs.callPackage (pkgs.fetchFromGitHub {
         owner = "MalpenZibo";
         repo = "ashell";
+        # "rev": "018f6a26e242d8438921ecb1c65ddef4a07fe070",
         rev = "refs/heads/main"; # Or specify the branch/tag you need
+        # $ nix-prefetch-git https://github.com/MalpenZibo/ashell
         sha256 =
-          "018f6a26e242d8438921ecb1c65ddef4a07fe070"; # Replace with the correct hash
+          "0f44vzf28nx2fj13bnlrk1j5ad6gmr74np4pk5ysyx0k17xjgvzl"; # Replace with the correct hash
       }) { }).defaultPackage.x86_64-linux)
 
       # inputs.ashell.packages.${pkgs.system}.default
