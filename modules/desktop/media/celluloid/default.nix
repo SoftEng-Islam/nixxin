cf5c93ead51ff17e4be42a3b64e7b8a31120d9a2
Islam Ahmed <softeng.islam@gmail.com>
2025-02-01 14:23:12 +0200

    update

--- modules/desktop/media/celluloid/default.nix
+++ modules/desktop/media/celluloid/default.nix
@@ -1,21 +1,10 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-
-let
-  inherit (lib) mapAttrs mkIf;
-  cfg = config.icedos;
-in
-mkIf (cfg.applications.celluloid) {
+{ settings, pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     (writeShellScriptBin "celluloid-hdr" "celluloid --mpv-profile=HDR $@")
     celluloid
   ];
 
-  home-manager.users = mapAttrs (user: _: {
+  home-manager.users."${settings.users.selected.username}" = {
     home.file.".config/celluloid" = {
       source = ./config;
       recursive = true;
@@ -23,16 +12,15 @@ mkIf (cfg.applications.celluloid) {
 
     dconf.settings = {
       "io/github/celluloid-player/celluloid" = {
-        mpv-config-file = "file:///home/${user}/.config/celluloid/celluloid.conf";
+        mpv-config-file =
+          "file:///home/${settings.users.selected.username}/.config/celluloid/celluloid.conf";
       };
 
-      "io/github/celluloid-player/celluloid" = {
-        mpv-config-enable = true;
-      };
+      "io/github/celluloid-player/celluloid" = { mpv-config-enable = true; };
 
       "io/github/celluloid-player/celluloid" = {
         always-append-to-playlist = true;
       };
     };
-  }) cfg.system.users;
+  };
 }
