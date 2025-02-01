7c28439151694bf9f0934046eaa96d4d32080a81
Islam Ahmed <softeng.islam@gmail.com>
2025-02-01 12:47:55 +0200

    update

--- modules/desktop/graphics/default.nix
+++ modules/desktop/graphics/default.nix
@@ -2,19 +2,19 @@
 { settings, lib, pkgs, ... }:
 let
   # Free design software that keeps your flow with AI tools and built-in graphics
-  lunacyApp = lib.optional settings.graphics.lunacy pkgs.lunacy;
+  lunacyApp = lib.optional settings.system.graphics.lunacy pkgs.lunacy;
   # Unofficial Electron-based Figma desktop app for Linux
-  figmaApp = lib.optional settings.graphics.figmaLinux pkgs.figma-linux;
+  figmaApp = lib.optional settings.system.graphics.figmaLinux pkgs.figma-linux;
   # A desktop application for creating diagrams
-  drawioApp = lib.optional settings.graphics.drawio pkgs.drawio;
+  drawioApp = lib.optional settings.system.graphics.drawio pkgs.drawio;
   # 3D Creation/Animation/Publishing System
-  blenderApp = lib.optional settings.graphics.blender pkgs.blender-hip;
+  blenderApp = lib.optional settings.system.graphics.blender pkgs.blender-hip;
   # GNU Image Manipulation Program
-  gimpApp = lib.optional settings.graphics.gimp pkgs.gimp;
+  gimpApp = lib.optional settings.system.graphics.gimp pkgs.gimp;
   # Vector graphics editor
-  inkscapeApp = lib.optional settings.graphics.inkscape pkgs.inkscape;
+  inkscapeApp = lib.optional settings.system.graphics.inkscape pkgs.inkscape;
   # Virtual lighttable and darkroom for photographers
-  darktableApp = lib.optional settings.graphics.darktable pkgs.darktable;
+  darktableApp = lib.optional settings.system.graphics.darktable pkgs.darktable;
 in {
   environment.systemPackages = [
     lunacyApp
