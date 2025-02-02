b9258191ff5875ca43eb10eff23573b3073f3a02
Islam Ahmed <softeng.islam@gmail.com>
2025-02-02 08:48:59 +0200

    update

--- modules/desktop/fileManager/default.nix
+++ modules/desktop/fileManager/default.nix
@@ -1,6 +1,11 @@
 { settings, pkgs, ... }: {
-  imports =
-    [ ./dircolors.nix ./files.nix ./nautilus.nix ./nemo.nix ./thunar.nix ];
+  imports = [
+    ./dircolors.nix
+    ./files.nix
+    ./nautilus.nix
+    ./nemo.nix
+    # ./thunar.nix
+  ];
   # services.gvfs.enable = true; # Mount, trash, and other functionalities
   # services.gvfs.package = pkgs.gnome.gvfs;
   services.tumbler.enable = true; # Thumbnail support for images
