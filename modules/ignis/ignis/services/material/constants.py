c66699f75c9b5e976e80217f6f9fcd781e8e8cd6
Islam Ahmed <softeng.islam@gmail.com>
2025-02-03 08:38:20 +0200

    update

--- modules/ignis/ignis/services/material/constants.py
+++ modules/ignis/ignis/services/material/constants.py
@@ -6,7 +6,7 @@ from ignis.utils import Utils
 MATERIAL_CACHE_DIR = f"{ignis.CACHE_DIR}/material"  # type: ignore
 
 TEMPLATES = Utils.get_current_dir() + "/templates"
-# SAMPLE_WALL = Utils.get_current_dir() + "/sample_wall.png"
-SAMPLE_WALL = f"{ignis.CACHE_DIR}/wallpaper"
+SAMPLE_WALL = Utils.get_current_dir() + "/sample_wall.png"
+# SAMPLE_WALL = f"{ignis.CACHE_DIR}/wallpaper"
 
 os.makedirs(MATERIAL_CACHE_DIR, exist_ok=True)
