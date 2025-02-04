ccaa1407921f0cfeedbcaafce522adec4615f9fb
Islam Ahmed <softeng.islam@gmail.com>
2025-02-04 06:12:10 +0200

    update

--- modules/android/waydroid.nix
+++ modules/android/waydroid.nix
@@ -93,11 +93,6 @@
   # /var/lib/waydroid/waydroid_base.prop
   #- sys.use_memfd=true
 
-  fileSystems."/dev/binderfs" = {
-    fsType = "binder";
-    options = [ "none" ];
-  };
-
   environment.systemPackages = with pkgs; [
     waydroid # Waydroid is a container-based approach to boot a full Android system on a regular GNU/Linux system like Ubuntu
     wl-clipboard
