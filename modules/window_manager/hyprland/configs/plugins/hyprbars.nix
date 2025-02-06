e6a4a0ce99d2e558749e80ffcd52c7957b7a2042
Islam Ahmed <softeng.islam@gmail.com>
2025-02-06 11:33:47 +0200

    update

--- modules/window_manager/hyprland/configs/plugins/hyprbars.nix
+++ modules/window_manager/hyprland/configs/plugins/hyprbars.nix
@@ -42,9 +42,9 @@ in {
         # example buttons (R -> L)
         # hyprbars-button = Background Color, Size, On-click, Foreground
         hyprbars-button = rgba(E62D42ff),20, , close-window, rgb(FFFFFF) # Close
-        # hyprbars-button = rgba(9141ACff),20, , hyprctl dispatch fullscreen 2, rgb(FFFFFF) # Fullscreen mode 2
-        hyprbars-button = rgba(3A944Aff),20, , hyprctl dispatch fullscreen 1, rgb(FFFFFF) # Maximize
-        hyprbars-button = rgba(C88800ff),20, , hyprctl dispatch togglefloating, rgb(FFFFFF) # Minimize / Floating toggle
+        # hyprbars-button = rgba(9141ACff),20,, hyprctl dispatch fullscreen 2, rgb(FFFFFF) # Fullscreen mode 2
+        hyprbars-button = rgba(3A944Aff),20,, hyprctl dispatch fullscreen 1, rgb(FFFFFF) # Maximize
+        hyprbars-button = rgba(C88800ff),20,, hyprctl dispatch togglefloating, rgb(FFFFFF) # Minimize / Floating toggle
       }
     '';
   };
