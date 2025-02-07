65456944af4915f58e109fbff6ae27829a550884
Islam Ahmed <softeng.islam@gmail.com>
2025-02-07 18:51:05 +0200

    update

--- docs/flake.nix
+++ docs/flake.nix
@@ -4,13 +4,14 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    devShells.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [
-        nixpkgs.python3
-        nixpkgs.python3Packages.pip
-        nixpkgs.python3Packages.mkdocs
-        nixpkgs.python3Packages.mkdocs-material
-      ];
-    };
+    devShells.x86_64-linux.default =
+      nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        buildInputs = [
+          nixpkgs.python3
+          nixpkgs.python3Packages.pip
+          nixpkgs.python3Packages.mkdocs
+          nixpkgs.python3Packages.mkdocs-material
+        ];
+      };
   };
 }
