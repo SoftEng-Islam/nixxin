faf108ce375903855bc3f084c5d28714700a5674
Islam Ahmed <softeng.islam@gmail.com>
2025-02-12 15:50:33 +0200

    update

--- modules/documents/zathura.nix
+++ modules/documents/zathura.nix
@@ -5,4 +5,9 @@ in mkIf (settings.modules.documents) {
   home-manager.users."${settings.user.username}" = {
     # You code
   };
+  environment.systemPackages = with pkgs;
+    [
+      # athura is a highly customizable and functional PDF viewer based on the poppler rendering library and the GTK toolkit.
+      zathura
+    ];
 }
