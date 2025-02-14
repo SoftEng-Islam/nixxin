3bfa689d0e22ddd17e795ffab56fc6885e9718bf
Islam Ahmed <softeng.islam@gmail.com>
2025-02-14 05:14:40 +0200

    update

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -1,7 +1,6 @@
 { settings, lib, pkgs, ... }:
 let inherit (lib) mkIf;
 in {
-  imports = [ ./toggle-services.nix ];
   services = {
     # Tumbler, A D-Bus thumbnailer service.
     tumbler.enable = true;
@@ -38,4 +37,35 @@ in {
       HandlePowerKey=ignore
     '';
   };
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "toggle-services" ''
+      SERVICES=("$@")
+
+      toggleService() {
+          SERVICE="$1"
+
+          if [[ ! "$SERVICE" == *".service"* ]]; then SERVICE="''${SERVICE}.service"; fi
+
+          if systemctl list-unit-files "$SERVICE" &>/dev/null; then
+              if systemctl is-active --quiet "$SERVICE"; then
+                  echo "Stopping \"$SERVICE\"..."
+                  sudo systemctl stop "$SERVICE"
+              else
+                  echo "Starting \"$SERVICE\"..."
+                  sudo systemctl start "$SERVICE"
+              fi
+          else
+              echo "\"$SERVICE\" does not exist"
+          fi
+      }
+
+      # Retain sudo
+      trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
+
+      for i in "''${SERVICES[@]}"
+      do
+          toggleService "$i"
+      done
+    '')
+  ];
 }
