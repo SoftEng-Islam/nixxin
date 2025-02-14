14dcdd42dc12bbec80e6e30bdaf678f953450ec7
Islam Ahmed <softeng.islam@gmail.com>
2025-02-14 05:10:37 +0200

    update

--- modules/drivers/vulkan/default.nix
+++ modules/drivers/vulkan/default.nix
@@ -3,9 +3,18 @@
     dxvk # A Vulkan-based translation layer for Direct3D
     vkbasalt # Vulkan post processing layer for Linux
     vkquake # Vulkan Quake port based on QuakeSpasm
+    vulkan-caps-viewer
+    vulkan-cts
     vulkan-extension-layer # Layers providing Vulkan features when native support is unavailable
+    vulkan-hdr-layer-kwin6
     vulkan-headers # Vulkan Header files and API registry
+    vulkan-helper
+    vulkan-loader
+    vulkan-memory-allocator
     vulkan-tools # Khronos official Vulkan Tools and Utilities
+    vulkan-tools-lunarg
     vulkan-utility-libraries # Set of utility libraries for Vulkan
+    vulkan-validation-layers
+    vulkan-volk
   ];
 }
