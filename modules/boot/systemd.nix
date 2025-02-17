5b78453da43471788e10c8f214c7cf51f3f63812
Islam Ahmed <softeng.islam@gmail.com>
2025-02-17 03:22:13 +0200

    update

--- modules/boot/systemd.nix
+++
@@ -1,9 +0,0 @@
-{ settings, lib, pkgs, ... }: {
-  # ---- Systemd Boot Manager ---- #
-  # NOTE!! disable to use GRUB instead of systemd-boot
-  boot.loader.systemd-boot = {
-    enable =
-      if (settings.boot.loader.manager.name == "SYSTEMD") then true else false;
-  };
-
-}
--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -4,9 +4,6 @@
     # Change kernal to zen kernal
     kernelPackages = pkgs.linuxPackages_zen;
 
-    bootspec.enable =
-      if (settings.boot.loader.manager.name == "SYSTEMD") then true else false;
-
     tmp.cleanOnBoot = true;
     supportedFilesystems = [ "btrfs" "ext4" "fat32" "nfs" "ntfs" ];
     consoleLogLevel = 0;
@@ -16,6 +13,48 @@
 
     # Whether the installation process is allowed to modify EFI boot variables.
     loader.efi.canTouchEfiVariables = true;
+
+    # NOTE!! disable to use GRUB instead of systemd-boot
+    loader.systemd-boot.enable =
+      if (settings.boot.loader.manager.name == "SYSTEMD") then true else false;
+
+    bootspec.enable =
+      if (settings.boot.loader.manager.name == "SYSTEMD") then true else false;
+
+    loader.grub = {
+      enable =
+        if (settings.boot.loader.manager.name == "GRUB") then true else false;
+
+      fontSize = settings.boot.loader.manager.grub.fontSize;
+      # theme = settings.boot.loader.manager.grub.theme;
+      efiSupport = settings.boot.loader.manager.grub.efiSupport;
+      gfxmodeEfi = settings.boot.loader.manager.grub.gfxmodeEfi;
+      devices = settings.boot.loader.manager.grub.devices;
+      device = settings.boot.loader.manager.grub.device;
+      useOSProber = settings.boot.loader.manager.grub.osProber;
+      extraConfig = settings.boot.loader.manager.grub.extraConfig;
+      # extraEntries = ''
+      #   menuentry 'Arch Linux' {
+      #   	insmod part_gpt
+      #   	insmod ext2
+      #   	search --no-floppy --fs-uuid --set=root d8ac40a1-c821-402f-b593-baf82f4efc31
+      #   	linux /boot/vmlinuz-linux root=UUID=d8ac40a1-c821-402f-b593-baf82f4efc31 rw
+      #   	initrd /boot/initramfs-linux.img
+      #   }
+
+      #   menuentry 'Windows Boot Manager (on /dev/sdb2)' --class windows --class os $menuentry_id_option 'osprober-efi-0AEE-1E17' {
+      #   	insmod part_gpt
+      #   	insmod fat
+      #   	set root='hd0,gpt1'
+      #   	if [ x$feature_platform_search_hint = xy ]; then
+      #   	  search --no-floppy --fs-uuid --set=root --hint-bios=hd0,gpt1 --hint-efi=hd0,gpt1 --hint-baremetal=ahci0,gpt1  0AEE-1E17
+      #   	else
+      #   	  search --no-floppy --fs-uuid --set=root 0AEE-1E17
+      #   	fi
+      #   	chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+      #   }
+      # '';
+    };
 
     initrd = {
       verbose = false;
@@ -222,5 +261,14 @@
     interval = "weekly";
   };
 
-  environment.systemPackages = with pkgs; [ acpid fatcat ];
+  environment.systemPackages = with pkgs; [
+    acpid
+    fatcat
+
+    os-prober
+    grub2_efi
+    grub2_full
+    sleek-grub-theme
+    nixos-grub2-theme
+  ];
 }
