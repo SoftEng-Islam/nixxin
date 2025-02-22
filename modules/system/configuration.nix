2f76c3c6c0e9e12b390191ea87109ac99aca0b36
Islam Ahmed <softeng.islam@gmail.com>
2025-02-22 04:54:37 +0200

    update

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -287,36 +287,6 @@ in {
   # ------------------------------------------------
   # ---- Hardware Configuration
   # ------------------------------------------------
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/ba8daecb-c5d6-4dc9-bc51-a38b344ca6ed";
-    fsType = "btrfs";
-    options = [ "subvol=@" ];
-  };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/7FD3-5156";
-    fsType = "vfat";
-    # options = [ "fmask=0077" "dmask=0077" ];
-    options = [ "rw" ];
-  };
-  fileSystems."/data" = {
-    device = "/dev/disk/by-uuid/67F7388D1080E3AB";
-    fsType = "ntfs-3g";
-    options = [
-      "rw"
-      "nofail"
-      "nodev"
-      "uid=1000"
-      "gid=1000"
-      "utf8"
-      "umask=022"
-      "exec"
-      "x-gvfs-show"
-    ];
-  };
-  swapDevices = [{
-    device = "/dev/disk/by-uuid/d06b1b0e-01c1-4874-98af-9f8e2cc53b4e";
-    # size = 4 * 1024; # Size in MB for a 4GB swap file
-  }];
   hardware = {
     uinput.enable = true;
     enableAllFirmware = true;
---
+++ modules/system/swap_devices.nix
@@ -0,0 +1,6 @@
+{ ... }: {
+  swapDevices = [{
+    device = "/dev/disk/by-uuid/d06b1b0e-01c1-4874-98af-9f8e2cc53b4e";
+    # size = 4 * 1024; # Size in MB for a 4GB swap file
+  }];
+}
