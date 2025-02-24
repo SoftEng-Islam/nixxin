c0a4a2b3cf118c1d5155759341e1938477ab90fd
Islam Ahmed <softeng.islam@gmail.com>
2025-02-24 09:43:42 +0200

    update

--- users/user1/desktop/default.nix
+++ users/user1/desktop/default.nix
@@ -1,9 +1,22 @@
-{ pkgs, ... }:
-# let _user = import (./. + "/../user.nix");
-# in
-rec {
-  imports = [ ../user.nix ];
+{ pkgs, ... }: rec {
+  user = {
 
+    name = "Islam Ahmed"; # Name/Identifier
+    username = "softeng"; # Username
+    email = "softeng.islam@gmail.com"; # Email (git config)
+    devices = {
+      desktop = {
+        system = {
+          hostname = "nixos";
+          architecture = "x86_64-linux";
+          stateVersion = "24.05";
+        };
+        home = { };
+        language = { };
+      };
+      laptop = { };
+    };
+  };
   # ----------------------------------------------
   # ---- System Information And Configuration
   # ----------------------------------------------
