c6e1b74d37458a24ec15ef58a0e7d7437f88ece6
Islam Ahmed <softeng.islam@gmail.com>
2025-02-24 18:15:18 +0200

    update

--- modules/recording/default.nix
+++ modules/recording/default.nix
@@ -1,12 +1,5 @@
-{ settings, lib, pkgs, ... }:
-let
-  inherit (lib) mkIf;
-  _imports = [
-    (lib.optional settings.modules.recording.screen.enable ./screen)
-    (lib.optional settings.modules.recording.sound.enable ./sound)
-  ];
-in mkIf (settings.modules.recording.enable) {
-  imports = lib.flatten _imports;
+{ settings, pkgs, ... }: {
+  imports = [ ./screen ./sound ];
   environment.systemPackages = with pkgs; [
     gpu-screen-recorder # A screen recorder that has minimal impact on system performance by recording a window using the GPU only
     gpu-screen-recorder-gtk # GTK frontend for gpu-screen-recorder.
