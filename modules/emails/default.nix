7d8d5baf1388d623e65cfd19298574dd13cb44d3
Islam Ahmed <softeng.islam@gmail.com>
2025-02-24 18:29:10 +0200

    update

--- modules/emails/default.nix
+++ modules/emails/default.nix
@@ -5,4 +5,4 @@ let
     (lib.optional settings.modules.emails.client.enable ./client)
     (lib.optional settings.modules.emails.handlers.enable ./handlers)
   ];
-in mkIf (settings.modules.emails.enable) { imports = lib.flatten _imports; }
+in { imports = lib.flatten _imports; }
