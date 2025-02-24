78118ce96574de038dc0e18f8058bb42a6615263
Islam Ahmed <softeng.islam@gmail.com>
2025-02-24 18:04:31 +0200

    update

--- modules/window_manager/hyprland/configs/plugins/default.nix
+++ modules/window_manager/hyprland/configs/plugins/default.nix
@@ -1,11 +1,15 @@
 { settings, lib, pkgs, ... }:
 let
   _hyprPlugins = [
-    (lib.optional settings.modules.hyprland.plugins.bordersPlus
+    (lib.optional settings.modules.window_manager.hyprland.plugins.bordersPlus
       ./borders-plus.nix)
-    (lib.optional settings.modules.hyprland.plugins.hyprbars ./hyprbars.nix)
-    (lib.optional settings.modules.hyprland.plugins.hyprexpo ./hyprexpo.nix)
-    (lib.optional settings.modules.hyprland.plugins.hyprspace ./hyprspace.nix)
-    (lib.optional settings.modules.hyprland.plugins.hyprtrails ./hyprtrails.nix)
+    (lib.optional settings.modules.window_manager.hyprland.plugins.hyprbars
+      ./hyprbars.nix)
+    (lib.optional settings.modules.window_manager.hyprland.plugins.hyprexpo
+      ./hyprexpo.nix)
+    (lib.optional settings.modules.window_manager.hyprland.plugins.hyprspace
+      ./hyprspace.nix)
+    (lib.optional settings.modules.window_manager.hyprland.plugins.hyprtrails
+      ./hyprtrails.nix)
   ];
 in { imports = lib.flatten _hyprPlugins; }
