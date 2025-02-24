d00f068c24266ba2828507e6524355c72cc59fec
Islam Ahmed <softeng.islam@gmail.com>
2025-02-24 09:34:39 +0200

    update

--- _settings.nix
+++ _settings.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: rec {
   # ---- Users ---- #
-  path = "/users/user1/desktop"; # Path of the Profile
+  path = "./users/user1/desktop"; # Path of the Profile
   profile = import (./. + path);
 }
