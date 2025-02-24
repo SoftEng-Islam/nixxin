3a8a0eff90162e2f60538a00737294dfa5283c3b
Islam Ahmed <softeng.islam@gmail.com>
2025-02-24 10:02:35 +0200

    update

--- modules/recording/default.nix
+++ modules/recording/default.nix
@@ -6,7 +6,7 @@ let
     (lib.optional settings.modules.recording.sound.enable ./sound)
   ];
 in mkIf (settings.modules.recording.enable) {
-  import = lib.flatten _imports;
+  imports = lib.flatten _imports;
   environment.systemPackages = with pkgs; [
     gpu-screen-recorder # A screen recorder that has minimal impact on system performance by recording a window using the GPU only
     gpu-screen-recorder-gtk # GTK frontend for gpu-screen-recorder.
