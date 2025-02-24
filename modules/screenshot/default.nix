ec2f23c5f1c48c9f6fe6d8a50eeeae4ea8f05c97
Islam Ahmed <softeng.islam@gmail.com>
2025-02-24 18:14:12 +0200

    update

--- modules/screenshot/default.nix
+++ modules/screenshot/default.nix
@@ -1,8 +1,4 @@
-{ settings, lib, pkgs, ... }:
-let
-  inherit (lib) mkIf;
-  _imports = [
-    (lib.optional settings.modules.screenshot.flameshot ./flameshot.nix)
-    (lib.optional settings.modules.screenshot.slurp ./slurp.nix)
-  ];
-in mkIf (settings.modules.screenshot.enable) { imports = lib.flatten _imports; }
+{ settings, pkgs, ... }:
+{
+  # imports = [ ./flameshot.nix ./slurp.nix ];
+}
