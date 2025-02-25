5af45828b097678a07272b97a2897890bcd76c93
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 09:02:40 +0200

    update

--- modules/browsers/default.nix
+++ modules/browsers/default.nix
@@ -2,12 +2,11 @@
 let
   inherit (lib) mkIf;
   _browsers = with pkgs; [
-    (lib.optional settings.modules.browsers.firefox.enable firefox)
-    (lib.optional settings.modules.browsers.firefox-beta.enable firefox-beta)
-    (lib.optional settings.modules.browsers.brave.enable brave)
-    (lib.optional settings.modules.browsers.google-chrome.enable google-chrome)
-    (lib.optional settings.modules.browsers.microsoft-edge.enable
-      microsoft-edge)
+    (lib.optional settings.modules.browsers.firefox firefox)
+    (lib.optional settings.modules.browsers.firefox-beta firefox-beta)
+    (lib.optional settings.modules.browsers.brave brave)
+    (lib.optional settings.modules.browsers.google-chrome google-chrome)
+    (lib.optional settings.modules.browsers.microsoft-edge.microsoft-edge)
   ];
 in mkIf (settings.modules.browsers.enable) {
   environment.systemPackages = lib.flatten _browsers;
