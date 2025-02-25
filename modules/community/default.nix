a70456492a1a91c5ce98249a12fa2a7c82b613a0
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 09:00:12 +0200

    update

--- modules/community/default.nix
+++ modules/community/default.nix
@@ -3,15 +3,15 @@
 let
   inherit (lib) mkIf;
   _cummunity = with pkgs; [
-    (lib.optional settings.modules.cummunity.discord discord)
-    (lib.optional settings.modules.cummunity.ferdium ferdium)
-    (lib.optional settings.modules.cummunity.mumble mumble)
-    (lib.optional settings.modules.cummunity.revolt revolt-desktop)
-    (lib.optional settings.modules.cummunity.signal signal-desktop)
-    (lib.optional settings.modules.cummunity.slack slack)
-    (lib.optional settings.modules.cummunity.telegram telegram-desktop)
-    (lib.optional settings.modules.cummunity.vesktop vesktop)
-    (lib.optional settings.modules.cummunity.zoom zoom-us)
+    (lib.optional settings.modules.community.discord discord)
+    (lib.optional settings.modules.community.ferdium ferdium)
+    (lib.optional settings.modules.community.mumble mumble)
+    (lib.optional settings.modules.community.revolt revolt-desktop)
+    (lib.optional settings.modules.community.signal signal-desktop)
+    (lib.optional settings.modules.community.slack slack)
+    (lib.optional settings.modules.community.telegram telegram-desktop)
+    (lib.optional settings.modules.community.vesktop vesktop)
+    (lib.optional settings.modules.community.zoom zoom-us)
   ];
 in mkIf (settings.modules.community.enable) {
   environment.systemPackages = lib.flatten _cummunity;
