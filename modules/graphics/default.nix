0dd0bef913ad4423c8873c6580697bf62baa53c8
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 08:42:02 +0200

    update

--- modules/graphics/default.nix
+++ modules/graphics/default.nix
@@ -2,15 +2,15 @@
 let
   inherit (lib) mkIf;
   _graphics = with pkgs; [
-    (lib.optional settings.modules.graphics.blender.enable blender)
-    (lib.optional settings.modules.graphics.darktable.enable darktable)
-    (lib.optional settings.modules.graphics.davinci.enable davinci-resolve)
-    (lib.optional settings.modules.graphics.drawio.enable drawio)
-    (lib.optional settings.modules.graphics.figmaLinux.enable figma-linux)
-    (lib.optional settings.modules.graphics.gimp.enable gimp)
-    (lib.optional settings.modules.graphics.inkscape.enable inkscape)
-    (lib.optional settings.modules.graphics.lunacy.enable lunacy)
-    (lib.optional settings.modules.graphics.kolourpaint.enable kolourpaint)
+    (lib.optional settings.modules.graphics.blender blender)
+    (lib.optional settings.modules.graphics.darktable darktable)
+    (lib.optional settings.modules.graphics.davinci davinci-resolve)
+    (lib.optional settings.modules.graphics.drawio drawio)
+    (lib.optional settings.modules.graphics.figmaLinux figma-linux)
+    (lib.optional settings.modules.graphics.gimp gimp)
+    (lib.optional settings.modules.graphics.inkscape inkscape)
+    (lib.optional settings.modules.graphics.lunacy lunacy)
+    (lib.optional settings.modules.graphics.kolourpaint kolourpaint)
 
   ];
 in mkIf (settings.modules.graphics.enable) {
