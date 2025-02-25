86f270ccebc09d32dab7f978ca0bd444cff385ce
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 06:59:56 +0200

    update

--- modules/android/default.nix
+++ modules/android/default.nix
@@ -2,15 +2,15 @@
 let
   inherit (lib) mkIf;
   _android = [
-    (lib.optional settings.moudles.android.development ./development.nix)
-    (lib.optional settings.moudles.android.android_studio ./android-studio.nix)
-    (lib.optional settings.moudles.android.scrcpy ./scrcpy.nix)
-    (lib.optional settings.moudles.android.waydroid ./waydroid.nix)
+    (lib.optional settings.modules.android.development ./development.nix)
+    (lib.optional settings.modules.android.android_studio ./android-studio.nix)
+    (lib.optional settings.modules.android.scrcpy ./scrcpy.nix)
+    (lib.optional settings.modules.android.waydroid ./waydroid.nix)
   ];
 in {
   imports = lib.flatten _android;
 
-  config = mkIf (settings.moudles.android.enable) {
+  config = mkIf (settings.modules.android.enable) {
     programs.adb.enable = true;
     environment.systemPackages = with pkgs;
       [
