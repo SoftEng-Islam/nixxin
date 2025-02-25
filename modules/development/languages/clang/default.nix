283c3a293a34d7667f42204aef55540c6d932231
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 05:00:09 +0200

    update

--- modules/development/languages/clang/default.nix
+++ modules/development/languages/clang/default.nix
@@ -1,46 +1,52 @@
-{ settings, lib, pkgs, ... }:
+{ settings, config, lib, pkgs, ... }:
 let inherit (lib) mkIf;
-in mkIf (settings.modules.development.languages.clang) {
+in {
 
-  environment.variables = {
-    # Use gcc for compilation
+  options.settings.modules.development.languages.clang.enable =
+    lib.mkEnableOption "Enable Clang module";
 
-    # CC="${pkgs.gcc}/bin/gcc";
-    # CXX="${pkgs.gcc}/bin/c++";
+  config = mkIf config.settings.modules.development.languages.clang {
+    environment.variables = {
+      # Use gcc for compilation
 
-    # CC = "${pkgs.clang}/bin/clang";
-    # CXX = "${pkgs.clang}/bin/clang++";
+      # CC="${pkgs.gcc}/bin/gcc";
+      # CXX="${pkgs.gcc}/bin/c++";
+
+      # CC = "${pkgs.clang}/bin/clang";
+      # CXX = "${pkgs.clang}/bin/clang++";
+    };
+    environment.systemPackages = with pkgs; [
+      valgrind # Valgrind is an award-winning instrumentation framework for building dynamic analysis tools.
+
+      # build tools
+      cmake
+      cmake-format
+      cmake-language-server
+      cmakeCurses
+      gcc
+      gnumake
+      ninja
+      pkg-config
+      clang-analyzer
+
+      # generates .cache and compile_commands.json
+      # files required by clangd
+      bear
+
+      # provides clangd (LSP)
+      # provides libraries
+      # NOTE: make sure mason.nvim don't install clangd
+      clang-tools
+
+      # required by codelldb (debugger)
+      # lldb # libraries conflicts with clang-tools
+      gdb
+
+      # libs
+      gpp # c++ module?, decrypt
+      gecode # c++ module
+    ];
+    # programs.vscode.extensions = [ pkgs.vscode-extensions.ms-vscode.cpptools ];
   };
-  environment.systemPackages = with pkgs; [
-    valgrind # Valgrind is an award-winning instrumentation framework for building dynamic analysis tools.
 
-    # build tools
-    cmake
-    cmake-format
-    cmake-language-server
-    cmakeCurses
-    gcc
-    gnumake
-    ninja
-    pkg-config
-    clang-analyzer
-
-    # generates .cache and compile_commands.json
-    # files required by clangd
-    bear
-
-    # provides clangd (LSP)
-    # provides libraries
-    # NOTE: make sure mason.nvim don't install clangd
-    clang-tools
-
-    # required by codelldb (debugger)
-    # lldb # libraries conflicts with clang-tools
-    gdb
-
-    # libs
-    gpp # c++ module?, decrypt
-    gecode # c++ module
-  ];
-  # programs.vscode.extensions = [ pkgs.vscode-extensions.ms-vscode.cpptools ];
 }
