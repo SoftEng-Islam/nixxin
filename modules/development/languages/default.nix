96551bb73fbb75064cf27f3f5dee5edde66438f3
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 05:15:12 +0200

    update

--- modules/development/languages/default.nix
+++ modules/development/languages/default.nix
@@ -1,8 +1,17 @@
 { settings, config, lib, pkgs, ... }:
-let inherit (lib) mkIf;
+let
+  inherit (lib) mkIf;
+  _imports = mkIf (settings.modules.development.languages.enable or false) [
+    ./clang
+    ./go
+    ./python
+    ./ruby
+    ./rust
+    ./web
+  ];
 in {
+  imports = lib.flatten _imports;
   config = mkIf (settings.modules.development.languages.enable or false) {
-    imports = [ ./clang ./go ./python ./ruby ./rust ./web ];
     environment.systemPackages = with pkgs;
       [
         # Command-line benchmarking tool
