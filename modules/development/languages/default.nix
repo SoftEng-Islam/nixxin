1ce85ab692e783a016affa55ce0f0b8983a457ab
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 05:03:21 +0200

    update

--- modules/development/languages/default.nix
+++ modules/development/languages/default.nix
@@ -1,26 +1,32 @@
-{ settings, lib, pkgs, ... }:
+{ settings, config, lib, pkgs, ... }:
 let inherit (lib) mkIf;
-in mkIf (settings.modules.development.languages.enable) {
-  imports = [ ./clang ./go ./python ./ruby ./rust ./web ];
-  environment.systemPackages = with pkgs;
-    [
-      # Command-line benchmarking tool
-      # hyperfine
-      # insomnia
-      # scc
-    ];
-  # programs.vscode = {
-  #   enable = true;
-  #   package = pkgs.vscodium;
-  #   extensions = with pkgs.vscode-extensions; [
-  #     aaron-bond.better-comments
-  #     catppuccin.catppuccin-vsc
-  #     catppuccin.catppuccin-vsc-icons
-  #     eamodio.gitlens
-  #     jnoortheen.nix-ide
-  #     mkhl.direnv
-  #     ms-vscode.hexeditor
-  #     tamasfe.even-better-toml
-  #   ];
-  # };
+in {
+
+  option.settings.modules.development.languages.enable =
+    lib.mkEnableOption "Enable Languages Module";
+
+  config = mkIf config.settings.modules.development.languages.enable {
+    imports = [ ./clang ./go ./python ./ruby ./rust ./web ];
+    environment.systemPackages = with pkgs;
+      [
+        # Command-line benchmarking tool
+        # hyperfine
+        # insomnia
+        # scc
+      ];
+    # programs.vscode = {
+    #   enable = true;
+    #   package = pkgs.vscodium;
+    #   extensions = with pkgs.vscode-extensions; [
+    #     aaron-bond.better-comments
+    #     catppuccin.catppuccin-vsc
+    #     catppuccin.catppuccin-vsc-icons
+    #     eamodio.gitlens
+    #     jnoortheen.nix-ide
+    #     mkhl.direnv
+    #     ms-vscode.hexeditor
+    #     tamasfe.even-better-toml
+    #   ];
+    # };
+  };
 }
