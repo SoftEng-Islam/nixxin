a0a2f60b3dc46f9dc9ce4ab01b34dd5fcdbd894b
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 08:39:57 +0200

    update

--- modules/android/android-studio.nix
+++ modules/android/android-studio.nix
@@ -1,5 +1,5 @@
 { settings, lib, pkgs, ... }:
 let inherit (lib) mkIf;
-in mkIf (settings.modules.android.android-studio.enable) {
+in mkIf (settings.modules.android.android-studio) {
   environment.systemPackages = with pkgs; [ android-studio ];
 }
