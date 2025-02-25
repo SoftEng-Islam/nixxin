2af8a599a625c5ee05ba7aa5d1916834b71d2c04
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 09:31:28 +0200

    update

--- modules/browsers/default.nix
+++ modules/browsers/default.nix
@@ -6,7 +6,7 @@ let
     (lib.optional settings.modules.browsers.firefox-beta firefox-beta)
     (lib.optional settings.modules.browsers.brave brave)
     (lib.optional settings.modules.browsers.google-chrome google-chrome)
-    (lib.optional settings.modules.browsers.microsoft-edge.microsoft-edge)
+    (lib.optional settings.modules.browsers.microsoft-edge microsoft-edge)
   ];
 in mkIf (settings.modules.browsers.enable) {
   environment.systemPackages = lib.flatten _browsers;
