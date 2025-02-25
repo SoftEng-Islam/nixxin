58aebbd66b098f1e4859416322293d5d5a7e6125
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 05:35:31 +0200

    update

--- modules/computing/default.nix
+++ modules/computing/default.nix
@@ -1,6 +1,13 @@
-{ settings, lib, pkgs, ... }:
-let
-  inherit (lib) mkIf;
+{ settings, config, lib, pkgs, ... }:
+let inherit (lib) optionals mkIf;
+in {
+
   # "pocl" OR "opencl"
-  _imports = [ (./. + settings.modules.computing.default + ".nix") ];
-in mkIf (settings.modules.computing.enable) { imports = lib.flatten _imports; }
+  imports = optionals (settings.modules.computing.enable)
+    [ (./. + settings.modules.computing.default + ".nix") ];
+
+  config = mkIf (settings.modules.computing.enable) {
+
+  };
+
+}
