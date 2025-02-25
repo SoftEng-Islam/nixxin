8561249e1831d9b96f9898e69cbb33bd9fbdd8dd
Islam Ahmed <softeng.islam@gmail.com>
2025-02-25 09:54:16 +0200

    update

--- users/user1/desktop/default.nix
+++ users/user1/desktop/default.nix
@@ -137,7 +137,7 @@
       development = true;
       android_studio = false;
     };
-    ashell = { enable = true; };
+    ashell = { enable = false; };
     audio = {
       enable = true;
       rnnoise.enable = true; # Noise Canceling
