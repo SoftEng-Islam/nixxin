6c2a53ee8196d0c72222f94b7ac9b8a53c7cd698
Islam Ahmed <softeng.islam@gmail.com>
2025-02-26 04:29:52 +0200

    update

--- modules/ashell/default.nix
+++ modules/ashell/default.nix
@@ -1,15 +1,5 @@
 { settings, lib, config, inputs, pkgs, ... }:
-let
-  inherit (lib) optionals mkIf;
-
-  ashellSrc = pkgs.fetchFromGitHub {
-    owner = "MalpenZibo";
-    repo = "ashell";
-    rev = "4a1c9e0c788e0e1c4aac9522d39a44cce7c24ef2"; # From nix-prefetch-git
-    sha256 = "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv";
-  };
-  ashell = import ashellSrc { inherit pkgs; }; # Import properly
-
+let inherit (lib) optionals mkIf;
 in {
 
   # imports = optionals (settings.modules.ashell.enable) [ ./ashell.nix ];
@@ -37,17 +27,6 @@ in {
     };
 
     environment.systemPackages = with pkgs;
-      [
-        ashell.defaultPackage.${pkgs.system}
-        # inputs.ashell
-        # (import (pkgs.callPackage (pkgs.fetchFromGitHub {
-        #   owner = "MalpenZibo";
-        #   repo = "ashell";
-        #   rev = "refs/heads/main"; # Or specify the branch/tag you need
-        #   # nix-prefetch-git https://github.com/MalpenZibo/ashell
-        #   sha256 =
-        #     "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the correct hash
-        # }) { }).packages.${settings.system.architecture})
-      ];
+      [ inputs.ashell.defaultPackage.${pkgs.system} ];
   };
 }
