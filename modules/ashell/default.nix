b2ea430f4cb7e3d7f0e86ac8c631184a0af90eb9
Islam Ahmed <softeng.islam@gmail.com>
2025-02-26 03:45:43 +0200

    update

--- modules/ashell/default.nix
+++ modules/ashell/default.nix
@@ -6,38 +6,37 @@ in {
 
   config = mkIf (settings.modules.ashell.enable) {
     home-manager.users.${settings.user.username} = {
-      systemd.user.services.ashell = {
-        Unit = {
-          Description = "Ashell shell";
-          PartOf = [ "hyprland-session.target" ];
-          After = [ "hyprland-session.target" ];
-        };
-        Install = { WantedBy = [ "hyprland-session.target" ]; };
-        Service = {
-          ExecStart =
-            "${inputs.ashell.defaultPackage.${pkgs.system}}/bin/ashell";
-          Restart = "on-failure";
-          Type = "simple";
-        };
-      };
+      # systemd.user.services.ashell = {
+      #   Unit = {
+      #     Description = "Ashell shell";
+      #     PartOf = [ "hyprland-session.target" ];
+      #     After = [ "hyprland-session.target" ];
+      #   };
+      #   Install = { WantedBy = [ "hyprland-session.target" ]; };
+      #   Service = {
+      #     ExecStart =
+      #       "${inputs.ashell.defaultPackage.${pkgs.system}}/bin/ashell";
+      #     Restart = "on-failure";
+      #     Type = "simple";
+      #   };
+      # };
 
       # Ashell Configs
       home.file.".config/ashell.yml".source = ./ashell.yml;
 
     };
 
-    environment.systemPackages = with pkgs;
-      [
-        inputs.ashell.defaultPackage.${pkgs.system}
+    environment.systemPackages = with pkgs; [
+      inputs.ashell.defaultPackage.${pkgs.system}
 
-        # (import (pkgs.callPackage (pkgs.fetchFromGitHub {
-        #   owner = "MalpenZibo";
-        #   repo = "ashell";
-        #   rev = "refs/heads/main"; # Or specify the branch/tag you need
-        #   # nix-prefetch-git https://github.com/MalpenZibo/ashell
-        #   sha256 =
-        #     "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the correct hash
-        # }) { }))
-      ];
+      (import (pkgs.callPackage (pkgs.fetchFromGitHub {
+        owner = "MalpenZibo";
+        repo = "ashell";
+        rev = "refs/heads/main"; # Or specify the branch/tag you need
+        # nix-prefetch-git https://github.com/MalpenZibo/ashell
+        sha256 =
+          "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the correct hash
+      }) { }).defaultPackage.${pkgs.system})
+    ];
   };
 }
