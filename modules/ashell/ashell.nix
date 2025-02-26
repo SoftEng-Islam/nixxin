6ad5fd0482d9f9a28c06410330b72f95389f1962
Islam Ahmed <softeng.islam@gmail.com>
2025-02-26 03:12:58 +0200

    update

---
+++ modules/ashell/ashell.nix
@@ -0,0 +1,31 @@
+{ lib, stdenv, fetchFromGitHub, gcc }:
+
+stdenv.mkDerivation rec {
+  pname = "ashell";
+  version = "latest";
+
+  src = fetchFromGitHub {
+    owner = "MalpenZibo";
+    repo = "ashell";
+    rev = "main"; # You can change this to a specific commit
+    sha256 =
+      "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the actual hash
+  };
+
+  nativeBuildInputs = [ gcc ];
+
+  buildPhase = ''
+    gcc -o ashell ashell.c
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ashell $out/bin/
+  '';
+
+  meta = with lib; {
+    description = "A simple shell written in C";
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+}
--- modules/ashell/default.nix
+++ modules/ashell/default.nix
@@ -1,58 +1,44 @@
-{ settings, lib, inputs, pkgs, ... }:
-let inherit (lib) mkIf;
-in mkIf (settings.modules.ashell.enable) {
-  # home-manager.users.${settings.user.username} = {
-  #   systemd.user.services = {
-  #     ashell = {
-  #       Unit = {
-  #         Description = "ashell status bar";
-  #         PartOf = [ "hyprland-session.target" ];
-  #       };
+{ settings, lib, config, inputs, pkgs, ... }:
+let inherit (lib) optionals mkIf;
+in {
 
-  #       Service = {
-  #         ExecStart = "${inputs.ashell.defaultPackage.x86_64-linux}/bin/ashell";
-  #         Restart = "on-failure";
-  #       };
+  imports = optionals (settings.modules.ashell.enable) [ ./ashell.nix ];
 
-  #       Install.WantedBy = [ "hyprland-session.target" ];
-  #     };
-  #   };
-  # };
+  config = mkIf (settings.modules.ashell.enable) {
+    # home-manager.users.${settings.user.username} = {
+    #   systemd.user.services = {
+    #     ashell = {
+    #       Unit = {
+    #         Description = "ashell status bar";
+    #         PartOf = [ "hyprland-session.target" ];
+    #       };
 
-  home-manager.users.${settings.user.username} = {
-    home.file.".config/ashell.yml".source = ./ashell.yml;
+    #       Service = {
+    #         ExecStart = "${inputs.ashell.defaultPackage.x86_64-linux}/bin/ashell";
+    #         Restart = "on-failure";
+    #       };
+
+    #       Install.WantedBy = [ "hyprland-session.target" ];
+    #     };
+    #   };
+    # };
+
+    home-manager.users.${settings.user.username} = {
+      home.file.".config/ashell.yml".source = ./ashell.yml;
+    };
+
+    environment.systemPackages = with pkgs;
+      [
+        # inputs.ashell.defaultPackage.x86_64-linux
+
+        # (import (pkgs.callPackage (pkgs.fetchFromGitHub {
+        #   owner = "MalpenZibo";
+        #   repo = "ashell";
+        #   rev = "refs/heads/main"; # Or specify the branch/tag you need
+        #   # nix-prefetch-git https://github.com/MalpenZibo/ashell
+        #   sha256 =
+        #     "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the correct hash
+        # }) { }))
+      ];
   };
-
-  environment.systemPackages = with pkgs;
-    [
-      # inputs.ashell.defaultPackage.x86_64-linux
-
-      # (import (pkgs.callPackage (pkgs.fetchFromGitHub {
-      #   owner = "MalpenZibo";
-      #   repo = "ashell";
-      #   rev = "refs/heads/main"; # Or specify the branch/tag you need
-      #   # nix-prefetch-git https://github.com/MalpenZibo/ashell
-      #   sha256 =
-      #     "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the correct hash
-      # }) { }))
-
-      (pkgs.stdenv.mkDerivation {
-        pname = "ashell";
-        version = "latest";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "MalpenZibo";
-          repo = "ashell";
-          rev = "main"; # You can replace with a tag or commit hash
-          sha256 =
-            "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Run nix-prefetch-git to get this
-        };
-
-        installPhase = ''
-          mkdir -p $out/bin
-          cp ashell $out/bin/
-        '';
-
-      })
-    ];
 }
