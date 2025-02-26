5eda5064825ef339563defb01d9dc1c0e0ec6f4c
Islam Ahmed <softeng.islam@gmail.com>
2025-02-26 02:56:11 +0200

    update

--- modules/ashell/default.nix
+++ modules/ashell/default.nix
@@ -18,16 +18,35 @@ in mkIf (settings.modules.ashell.enable) {
   #     };
   #   };
   # };
-  environment.systemPackages = with pkgs;
-    [
-      (import (pkgs.callPackage (pkgs.fetchFromGitHub {
+  environment.systemPackages = with pkgs; [
+    # inputs.ashell.defaultPackage.x86_64-linux
+
+    (import (pkgs.callPackage (pkgs.fetchFromGitHub {
+      owner = "MalpenZibo";
+      repo = "ashell";
+      rev = "refs/heads/main"; # Or specify the branch/tag you need
+      # nix-prefetch-git https://github.com/MalpenZibo/ashell
+      sha256 =
+        "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the correct hash
+    }) { }))
+
+    (pkgs.stdenv.mkDerivation {
+      pname = "ashell";
+      version = "latest";
+
+      src = pkgs.fetchFromGitHub {
         owner = "MalpenZibo";
         repo = "ashell";
-        rev = "refs/heads/main"; # Or specify the branch/tag you need
-        # nix-prefetch-git https://github.com/MalpenZibo/ashell
+        rev = "main"; # You can replace with a tag or commit hash
         sha256 =
-          "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Replace with the correct hash
-      }) { }))
-      # inputs.ashell.defaultPackage.x86_64-linux
-    ];
+          "1fvk3yl5z1sirm6ngi45j59r5b0raa5xszjbh23bkc389sbkzxiv"; # Run nix-prefetch-git to get this
+      };
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ashell $out/bin/
+      '';
+
+    })
+  ];
 }
