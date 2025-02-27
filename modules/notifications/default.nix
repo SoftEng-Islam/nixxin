a5572f5083db09a1f0cc87c6c99dae09156e3316
Islam Ahmed <softeng.islam@gmail.com>
2025-02-27 19:56:34 +0200

    update

--- modules/notifications/default.nix
+++ modules/notifications/default.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: {
-  imports = [ ./dunst ];
+  # imports = [ ./dunst ];
 
   environment.systemPackages = with pkgs; [
-    dunst # Lightweight and customizable notification daemon
+    # dunst # Lightweight and customizable notification daemon
     notify # Notify allows sending the output from any tool to Slack, Discord and Telegram
     libnotify # A library that sends desktop notifications to a notification daemon
     # avizo # Neat notification daemon for Wayland
