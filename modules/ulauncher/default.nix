1068344a00f146a937c91b2574326312b7ce8687
Islam Ahmed <softeng.islam@gmail.com>
2025-03-03 07:31:54 +0200

    update

--- modules/ulauncher/default.nix
+++ modules/ulauncher/default.nix
@@ -3,17 +3,26 @@ let inherit (lib) mkIf;
 in {
   config = lib.optionals (settings.modules.ulauncher.enable) {
     environment.systemPackages = with pkgs; [ ulauncher ];
-    home-manager.users.${settings.user.username} = {
-      # Ulauncher service configuration
-      systemd.user.services.ulauncher = {
-        description = "Ulauncher application launcher service";
-        after = [ "graphical-session.target" ];
-        install = { wantedBy = [ "default.target" ]; };
-        serviceConfig = {
-          ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
-          Restart = "on-failure"; # Restart only on failure
-        };
+    # Ulauncher service configuration
+    systemd.services.ulauncher = {
+      # WantedBy=graphical-session.target
+      wantedBy = [ "graphical-session.target" ];
+      description = "Start the ulauncher";
+      unitConfig = { Documentation = "https://ulauncher.io"; };
+      serviceConfig = {
+        # BusName=io.ulauncher.Ulauncher
+        # Type=dbus
+        Type = "dbus";
+        BusName = "io.ulauncher.Ulauncher";
+        # Restart=always
+        Restart = "always";
+        # RestartSec=1;
+        RestartSec = 1;
+        # ExecStart=/usr/bin/ulauncher --hide-window
+        ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
       };
+    };
+    home-manager.users.${settings.user.username} = {
       # Source ulauncher configuration from this repository
       xdg.configFile = {
         "ulauncher" = {
