563c0d39f89e746ce1c861dfa7ffcb9644e0ee0a
Islam Ahmed <softeng.islam@gmail.com>
2025-03-03 06:09:35 +0200

    update

--- modules/ulauncher/default.nix
+++ modules/ulauncher/default.nix
@@ -3,23 +3,22 @@ let inherit (lib) mkIf;
 in {
   config = lib.optionals (settings.modules.ulauncher.enable) {
     environment.systemPackages = with pkgs; [ ulauncher ];
-    # Ulauncher service configuration
-    systemd.services.ulauncher = {
-      Unit = {
-        Description = "Ulauncher application launcher service";
-        Documentation = "https://ulauncher.io";
-        After = [ "graphical-session.target" ];
-      };
-      restartIfChanged = false; # Prevent unnecessary restarts during rebuild.
-
-      Service = {
-        ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
-        Restart = "on-failure"; # Restart only on failure
-      };
-
-      Install = { WantedBy = [ "default.target" ]; };
-    };
     home-manager.users.${settings.user.username} = {
+      # Ulauncher service configuration
+      systemd.user.services.ulauncher = {
+        Unit = {
+          Description = "Ulauncher application launcher service";
+          Documentation = "https://ulauncher.io";
+          After = [ "graphical-session.target" ];
+        };
+
+        Service = {
+          ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
+          Restart = "on-failure"; # Restart only on failure
+        };
+
+        Install = { WantedBy = [ "default.target" ]; };
+      };
       # Source ulauncher configuration from this repository
       xdg.configFile = {
         "ulauncher" = {
