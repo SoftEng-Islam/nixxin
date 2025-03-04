b0aaae5233306b151cefe8bf2705151fd40ecb20
Islam Ahmed <softeng.islam@gmail.com>
2025-03-04 13:48:11 +0200

    update

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -1,9 +1,8 @@
 { settings, lib, pkgs, ... }:
 let
   inherit (lib) mkIf;
-  # flatpak remote-add --if-not-exists
-  # flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 in mkIf (settings.modules.flatpak.enable) {
   services.flatpak = { enable = true; };
-  environment.systemPackages = with pkgs; [ flatpak ];
+  environment.systemPackages = with pkgs; [ flatpak gnome-software ];
 }
