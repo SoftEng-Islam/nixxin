e95053fbe4395031a3b629f4a4a61fdcd5fd1105
Islam Ahmed <softeng.islam@gmail.com>
2025-03-14 04:29:31 +0200

    update

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,11 +1,12 @@
 { settings, config, lib, pkgs, ... }:
-# git config --global url."https://github.com/".insteadOf "git@github.com:"
-# git config --global http.postBuffer 1048576000
-# git config --global http.lowSpeedTime 60
 
 # ---- Use gh to login ---- #
 # gh auth login
 
+# Ex: To set git configs in terminal:
+# git config --global url."https://github.com/".insteadOf "git@github.com:"
+# git config --global http.postBuffer 1048576000
+# git config --global http.lowSpeedTime 60
 let inherit (lib) mkIf;
 in mkIf (settings.modules.git.enable) {
   environment.variables = {
