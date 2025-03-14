ed46717f3952792a3cb21ba46060668e7b67c787
Islam Ahmed <softeng.islam@gmail.com>
2025-03-14 08:20:00 +0200

    update

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -6,7 +6,6 @@ let
     ms-vscode.cpptools-extension-pack
     ms-vscode.cmake-tools
     denoland.vscode-deno
-    EditorConfig.EditorConfig
     syler.sass-indented
     timonwong.shellcheck
     rangav.vscode-thunder-client
@@ -69,9 +68,9 @@ let
     arcticicestudio.nord-visual-studio-code
     esbenp.prettier-vscode
     editorconfig.editorconfig
-    elixir-lsp.vscode-elixir-ls
+    # elixir-lsp.vscode-elixir-ls
     firefox-devtools.vscode-firefox-debug
-    golang.go
+    # golang.go
     gleam.gleam
     phoenixframework.phoenix
     jnoortheen.nix-ide
@@ -88,30 +87,20 @@ let
     tomoki1207.pdf
   ];
   market-extensions = with pkgs.vscode-marketplace; [
-    denoland.vscode-deno
-    brian-anders.sublime-duplicate-text
     cardinal90.multi-cursor-case-preserve
     # pkgs.vscode-marketplace."cuelang.org".cue
-    pgourlain.erlang
+    # pgourlain.erlang
     jakebecker.elixir-ls
     jallen7usa.vscode-cue-fmt
     maximus136.change-string-case
     ms-python.isort
-    ms-vscode.sublime-keybindings
     # ms-vscode.cpptools-extension-pack
     nico-castell.linux-desktop-file
     # platformio.platformio-ide
     # unifiedjs.vscode-mdx
     # VisualStudioExptTeam.vscodeintellicode
-    xoronic.pestfile
-    # silabs.siliconlabssupportextension
-    # marus25.cortex-debug
-    # mcu-debug.debug-tracker-vscode
-    # mcu-debug.memory-view
-    # mcu-debug.rtos-views
-    # mcu-debug.peripheral-viewer
-    antfu.unocss
-    bradlc.vscode-tailwindcss
+    # xoronic.pestfile
+    # antfu.unocss
   ];
 in {
   environment.systemPackages = with pkgs; [ vscode-fhs ];
