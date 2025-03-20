5f32afd8438c7339fc2ab525943acdcf5163cdda
Islam Ahmed <softeng.islam@gmail.com>
2025-03-20 18:27:19 +0200

    update

--- modules/development/tools/default.nix
+++ modules/development/tools/default.nix
@@ -1,13 +1,20 @@
-{ settings, lib, pkgs, ... }:
+{ settings, config, lib, pkgs, ... }:
 let inherit (lib) mkIf;
 in mkIf (settings.modules.development.tools.enable) {
-  environment.systemPackages = with pkgs; [
-    ansible # automation scripts
-    awscli # Unified tool to manage your AWS services
-    mkcert # create certificates (HTTPS)
-    ngrok # expose web server
-    stripe-cli # Command-line tool for Stripe
-    watchman # required by react native
-    jira-cli-go # Feature-rich interactive Jira command line
+  imports = lib.optionals (settings.modules.development.tools.enable) [
+    ./devdocs
+    ./ide
   ];
+  config = mkIf (settings.modules.development.tools.enable) {
+
+    environment.systemPackages = with pkgs; [
+      ansible # automation scripts
+      awscli # Unified tool to manage your AWS services
+      mkcert # create certificates (HTTPS)
+      ngrok # expose web server
+      stripe-cli # Command-line tool for Stripe
+      watchman # required by react native
+      jira-cli-go # Feature-rich interactive Jira command line
+    ];
+  };
 }
--- modules/development/tools/devdocs/default.nix
+++ modules/development/tools/devdocs/default.nix
@@ -1,14 +1,44 @@
-{ settings, lib, pkgs, ... }:
-let inherit (lib) mkIf;
-in mkIf (settings.modules.developement.tools.devdocs.enable) {
-  # git clone https://github.com/freeCodeCamp/devdocs && cd devdocs
-  # - Install The Requirements
-  # - Ruby 3.4.1
-  # - libcurl
-  # Once you have these installed, run the following commands:
-  # git clone https://github.com/freeCodeCamp/devdocs.git && cd devdocs
-  # gem install bundler
-  # bundle install
-  # bundle exec thor docs:download --default
-  # bundle exec rackup
+{ settings, lib, pkgs ? import <nixpkgs> { } }:
+# git clone https://github.com/freeCodeCamp/devdocs
+let
+  rubyEnv = pkgs.bundlerEnv {
+    name = "devdocs-gems";
+    ruby = pkgs.ruby;
+    gemdir = ./devdocs; # Path to the cloned repository
+  };
+in lib.mkIf (settings.modules.developement.tools.devdocs.enable)
+pkgs.stdenv.mkDerivation rec {
+  pname = "devdocs";
+  version = "latest";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "freeCodeCamp";
+    repo = "devdocs";
+    rev = "master";
+    sha256 = ""; # Run `nix-prefetch-git` to get the hash
+  };
+
+  nativeBuildInputs = [ pkgs.ruby pkgs.bundler pkgs.thor pkgs.rack rubyEnv ];
+
+  buildPhase = ''
+    export GEM_HOME=$PWD/gems
+    export PATH=$GEM_HOME/bin:$PATH
+
+    gem install bundler
+    bundle install --path=$GEM_HOME
+
+    bundle exec thor docs:download --default
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r * $out
+  '';
+
+  meta = {
+    description = "DevDocs offline documentation";
+    homepage = "https://github.com/freeCodeCamp/devdocs";
+    license = pkgs.lib.licenses.mit;
+    platforms = pkgs.lib.platforms.unix;
+  };
 }
