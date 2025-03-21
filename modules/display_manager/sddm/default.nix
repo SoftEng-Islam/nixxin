b2d611cd6bf1893f5b32222331833cf281628881
Islam Ahmed <softeng.islam@gmail.com>
2025-03-21 06:28:05 +0200

    update

--- modules/display_manager/sddm/default.nix
+++ modules/display_manager/sddm/default.nix
@@ -1,11 +1,27 @@
 { settings, lib, pkgs, ... }:
-let inherit (lib) mkIf;
+let
+  inherit (lib) mkIf;
+  imgLink = (./. + "./orange_sunset.jpg");
+
 in {
-  imports = [ ./sddm-theme.nix ];
   services = {
-    # ---- SDDM ---- #
     displayManager.sddm.enable = true;
     displayManager.sddm.wayland.enable = true;
     displayManager.sddm.package = pkgs.plasma5Packages.sddm;
   };
+  environment.systemPackages = with pkgs;
+    [
+      # sddm # QML based X11 display manager
+
+      (sddm-chili-theme.override {
+        themeConfig = {
+          background = imgLink;
+          ScreenWidth = 1920;
+          ScreenHeight = 1080;
+          blur = true;
+          recursiveBlurLoops = 3;
+          recursiveBlurRadius = 5;
+        };
+      })
+    ];
 }
