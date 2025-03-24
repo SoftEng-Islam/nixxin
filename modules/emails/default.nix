f04f5560a8666de06ebb27591f32008ca47a9477
Islam Ahmed <softeng.islam@gmail.com>
2025-03-24 14:42:00 +0200

    update

--- modules/emails/default.nix
+++ modules/emails/default.nix
@@ -3,6 +3,16 @@
 let inherit (lib) mkIf;
 in {
   config = mkIf (settings.modules.emails.enable or true) {
+
+    home-manager.users.${settings.user.username} = {
+      programs.thunderbird = {
+        enable = true;
+        # programs.thunderbird.nativeMessagingHosts = [];
+        programs.thunderbird.package = pkgs.thunderbird-latest;
+
+      };
+    };
+
     environment.systemPackages = with pkgs; [ thunderbird-latest birdtray ];
   };
 }
