1fa234858e18924f337c6f6c0cc6f0db266f2c03
Islam Ahmed <softeng.islam@gmail.com>
2025-04-08 22:10:47 +0200

    update

--- modules/overclock/lactd.nix
+++ modules/overclock/lactd.nix
@@ -3,17 +3,17 @@ let inherit (lib) mkIf;
 in mkIf (settings.modules.overclock.lactd.enable or false) {
   # Linux GPU Configuration Tool for AMD and NVIDIA
   # We are creating the lact daemon service manually because the provided one hangs
-  # systemd.services.lactd = {
-  #   enable = true;
-  #   description = "Radeon GPU monitor";
-  #   after = [ "syslog.target" "systemd-modules-load.service" ];
-  #   unitConfig = { ConditionPathExists = "${pkgs.lact}/bin/lact"; };
-  #   serviceConfig = {
-  #     User = "root";
-  #     ExecStart = "${pkgs.lact}/bin/lact daemon";
-  #   };
-  #   wantedBy = [ "multi-user.target" ];
-  # };
+  systemd.services.lactd = {
+    enable = true;
+    description = "Radeon GPU monitor";
+    after = [ "syslog.target" "systemd-modules-load.service" ];
+    unitConfig = { ConditionPathExists = "${pkgs.lact}/bin/lact"; };
+    serviceConfig = {
+      User = "root";
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 
   systemd.services.lact = {
     enable = true;
