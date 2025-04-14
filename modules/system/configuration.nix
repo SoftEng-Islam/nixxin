b3f7234d784eed0d268f56da401be45982f77578
Islam Ahmed <softeng.islam@gmail.com>
2025-04-14 19:57:14 +0200

    update

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -346,6 +346,14 @@ in {
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
+        # OpenCL ICD definition for AMD GPUs using the ROCm stack
+        # rocmPackages.clr.icd
+        # OpenCL runtime for AMD GPUs, part of the ROCm stack
+        # rocmPackages.clr
+
+        # AMD Open Source Driver For Vulkan
+        amdvlk
+
         # amf
         amdvlk # AMD Vulkan driver
         vulkan-validation-layers
@@ -362,13 +370,19 @@ in {
         xorg.libXv
         xorg.libXvMC
 
+        # Hardware-accelerated video playpack
+        # VDPAU driver for the VAAPI library
+        vaapiVdpau
+        # VDPAU driver with OpenGL/VAAPI backend
         libvdpau-va-gl
+        # Tool to query the Video Decode and Presentation API for Unix (VDPAU) abilities of the system
+        vdpauinfo
         libGL
         libGLU
         libGLX
         libva
         libva-utils
-        vaapiVdpau
+
         # ---- Unlocks OpenCL GPU Acceleration ---- #
         # rocmPackages.clr
         rocmPackages.clr.icd
@@ -636,5 +650,32 @@ in {
     radeon-profile
     radeontools
     radeontop
+
+    # Inspect and manipulate PCI devices
+    pciutils
+
+    # Print all known information about all available OpenCL platforms and devices in the system
+    # clinfo
+
+    # Top-like tool for viewing AMD Radeon GPU utilization
+    radeontop
+
+    # Application to read current clocks of AMD Radeon cards
+    radeon-profile
+
+    # ROCm Application for Reporting System Info
+    # rocmPackages.rocminfo
+
+    # System management interface for AMD GPUs supported by ROCm
+    # rocmPackages.rocm-smi
+
+    # Platform runtime for ROCm
+    # rocmPackages.rocm-runtime
+
+    # CMake modules for common build tasks for the ROCm stack
+    # rocmPackages.rocm-cmake
+
+    # Radeon open compute thunk interface
+    # rocmPackages.rocm-thunk
   ];
 }
