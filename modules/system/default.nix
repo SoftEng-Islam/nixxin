55da4603d63ca6b64d4545d8ed21560d646343b3
Islam Ahmed <softeng.islam@gmail.com>
2025-04-14 20:05:12 +0200

    update

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,4 +1,4 @@
 # ---- docs.nix ---- #
-{ settings, lib, pkgs, ... }:
-let inherit (lib) mkIf;
-in { imports = [ ./configuration.nix ./nixos.nix ./radeon.nix ./systemd.nix ]; }
+{ ... }: {
+  imports = [ ./configuration.nix ./nixos.nix ./radeon.nix ./systemd.nix ];
+}
