c8a33f2a6eeb9ab7ea32fe172337fb16f8a140f2
Islam Ahmed <softeng.islam@gmail.com>
2025-04-16 11:16:02 +0200

    update

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,4 +1,9 @@
 # ---- docs.nix ---- #
-{ ... }: {
+{ pkgs, ... }: {
   imports = [ ./configuration.nix ./nixos.nix ./radeon.nix ./systemd.nix ];
+  environment.systemPackages = with pkgs; [
+    e2fsprogs
+    smartmontools
+    util-linux
+  ];
 }
