0def932423e70f14f0644a67c6c673c053533a4e
Islam Ahmed <softeng.islam@gmail.com>
2025-04-29 01:26:51 +0300

    update

--- modules/development/databases/mysql/default.nix
+++ modules/development/databases/mysql/default.nix
@@ -8,7 +8,7 @@ in {
       ensureDatabases = [ "wordpress" ];
       ensureUsers = [{
         name = "softeng";
-        password = "*77B48D6366D102139D3719F48B811EAE123B2CA5";
+        # password = "*77B48D6366D102139D3719F48B811EAE123B2CA5";
         privileges = { "wordpress.*" = "ALL PRIVILEGES"; };
       }];
     };
