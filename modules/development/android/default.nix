28327c7bfd0755fdfffbd74a5491c47eefbd6513
Islam Ahmed <softeng.islam@gmail.com>
2025-05-07 22:35:07 +0300

    update

--- modules/development/android/default.nix
+++ modules/development/android/default.nix
@@ -7,13 +7,6 @@ in mkIf (settings.modules.development.android.enable) {
     gradle # Enterprise-grade build system
     dart
     flutter
-    (androidenv.composeAndroidPackages {
-      toolsVersion = "26.1.1";
-      platformToolsVersion = "34.0.4";
-      buildToolsVersions = [ "30.0.3" ];
-      includeEmulator = false;
-      platformVersions = [ "34" ];
-      abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-    })
+
   ];
 }
