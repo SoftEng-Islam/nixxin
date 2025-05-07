1445d63263ec0dd2e1d25f15adb45842a3e68f37
Islam Ahmed <softeng.islam@gmail.com>
2025-05-07 18:36:26 +0300

    update

--- modules/networking/wifi_driver.nix
+++ modules/networking/wifi_driver.nix
@@ -1,10 +1,9 @@
 { settings, config, pkgs, ... }: {
   boot = {
     blacklistedKernelModules = [ "rtl8812au" "rtl8xxxu" "r8188eu" ];
-    extraModulePackages = with config.boot.kernelPackages;
-      [ rtl8188eus-aircrack ];
+    # extraModulePackages = with config.boot.kernelPackages;[ rtl8188eus-aircrack ];
   };
-  networking.wireless.driver = "rtl8188eus-aircrack";
+  # networking.wireless.driver = "rtl8188eus-aircrack";
   environment.systemPackages = with pkgs;
     [
       # RealTek RTL8188eus WiFi driver with monitor mode & frame injection support
