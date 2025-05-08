804ea1a48d3d66b2ab928f65f70fa3efdb309a05
Islam Ahmed <softeng.islam@gmail.com>
2025-05-08 14:37:20 +0300

    update

--- modules/office/default.nix
+++ modules/office/default.nix
@@ -1,9 +1,16 @@
 { settings, config, lib, pkgs, ... }:
 let
   inherit (lib) optionals mkIf;
+
   _pkgs = [
     # Powerful knowledge base that works on top of a local folder of plain text Markdown files
-    (optionals settings.modules.office.obsidian pkgs.obsidian)
+    (optionals settings.modules.office.obsidian (pkgs.obsidian.overrideAttrs
+      (oldAttrs: {
+        postInstall = oldAttrs.postInstall or "" + ''
+          wrapProgram $out/bin/${oldAttrs.pname} \
+            --add-flags "--wayland-text-input-version=3"
+        '';
+      })))
 
     # Privacy-first personal knowledge management system that supports complete offline usage, as well as end-to-end encrypted data sync
     (optionals settings.modules.office.siyuan pkgs.siyuan)
