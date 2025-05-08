ea69c7ad6be091e5d6c1af5c81e2769b37e9cf8f
Islam Ahmed <softeng.islam@gmail.com>
2025-05-08 20:16:02 +0300

    update

--- modules/development/web/default.nix
+++ modules/development/web/default.nix
@@ -1,5 +1,8 @@
 { settings, config, pkgs, ... }: {
-  imports = [ ./bun.nix ./wordpress.nix ];
+  imports = [
+    ./bun.nix
+    # ./wordpress.nix
+  ];
   environment.variables = {
     NODE_PATH = "${pkgs.nodejs_latest}/lib/node_modules";
 
