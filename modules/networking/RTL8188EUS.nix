a07dfc57c4e6e1bb0366bdedfb0f511449fd3d83
Islam Ahmed <softeng.islam@gmail.com>
2025-05-25 12:04:16 +0300

    update & fix

--- modules/networking/RTL8188EUS.nix
+++ modules/networking/RTL8188EUS.nix
@@ -5,10 +5,14 @@
   boot.blacklistedKernelModules = [ "rtl8xxxu" "r8188eu" ];
 
   # Install the patched rtl8188eus driver (supports monitor mode)
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    rtl88xxau-aircrack
-    rtl8188eus-aircrack
+  boot.extraModulePackages = [
+    # rtl88xxau-aircrack
+    pkgs.linuxKernel.packages.linux_zen.rtl8188eus-aircrack
   ];
+  boot.kernelModules = [ "8188eu" ];
+
+  # Optional: Disable power saving for better WiFi stability
+  boot.kernelParams = [ "rtl8188eus.ips_mode=0" ];
 
   # Optional: Enable Aircrack-NG tools
   environment.systemPackages = with pkgs;
@@ -16,8 +20,5 @@
       linuxKernel.packages.linux_zen.rtl8188eus-aircrack
       # linuxKernel.packages.linux_6_14.rtl8188eus-aircrack
     ];
-
-  # Optional: Disable power saving for better WiFi stability
-  boot.kernelParams = [ "rtl8188eus.ips_mode=0" ];
 
 }
