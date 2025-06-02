ea2b16dbadb344449f4e53ce40cc1725b45bf5d7
Islam Ahmed <softeng.islam@gmail.com>
2025-06-02 14:00:43 +0300

    update

--- modules/office/obsidian.nix
+++ modules/office/obsidian.nix
@@ -4,17 +4,17 @@ in mkIf (settings.modules.office.obsidian or true) {
   environment.systemPackages = with pkgs;
     [
       # Powerful knowledge base that works on top of a local folder of plain text Markdown files
-      # (writeShellScriptBin "obsidian" ''
-      #   exec ${pkgs.obsidian}/bin/obsidian \
-      #     --disable-gpu-sandbox \
-      #     --use-gl=desktop \
-      #     --enable-features=UseOzonePlatform \
-      #     --ozone-platform=x11 \
-      #     "$@"
-      # '')
+      (writeShellScriptBin "obsidian" ''
+        exec ${pkgs.obsidian}/bin/obsidian \
+          --disable-gpu-sandbox \
+          --use-gl=desktop \
+          --enable-features=UseOzonePlatform \
+          --ozone-platform=x11 \
+          "$@"
+      '')
       # (optionals settings.modules.office.obsidian obsidian)
       # Privacy-first personal knowledge management system that supports complete offline usage, as well as end-to-end encrypted data sync
-      obsidian
+      # obsidian
     ];
   home-manager.users.${settings.user.username} = {
     home.packages = [
@@ -27,15 +27,15 @@ in mkIf (settings.modules.office.obsidian or true) {
       #     "$@"
       # '')
     ];
-    # xdg.desktopEntries.obsidian = {
-    #   name = "Obsidian";
-    #   genericName = "Note-taking App";
-    #   comment = "Markdown-based note-taking with GPU workarounds";
-    #   exec = "obsidian %U";
-    #   icon = "obsidian"; # or path to a custom icon
-    #   terminal = false;
-    #   type = "Application";
-    #   categories = [ "Office" "Utility" ];
-    # };
+    xdg.desktopEntries.obsidian = {
+      name = "Obsidian";
+      genericName = "Note-taking App";
+      comment = "Markdown-based note-taking with GPU workarounds";
+      exec = "obsidian %U";
+      icon = "obsidian"; # or path to a custom icon
+      terminal = true;
+      type = "Application";
+      categories = [ "Office" "Utility" ];
+    };
   };
 }
