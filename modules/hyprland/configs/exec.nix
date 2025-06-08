46eee5d2485a38de78012adafbc579afc55c9453
Islam Ahmed <softeng.islam@gmail.com>
2025-06-08 16:18:56 +0300

    update & Fix

--- modules/hyprland/configs/exec.nix
+++ modules/hyprland/configs/exec.nix
@@ -15,11 +15,13 @@ let
     # dconf write /org/gnome/desktop/interface/font-name "'Noto Sans Medium 11'"
     # dconf write /org/gnome/desktop/interface/monospace-font-name "'Noto Sans Mono Medium 11'"
 
-    # gsettings set org.gnome.desktop.interface gtk-theme ${settings.common.gtk.theme}
-    # gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+    gsettings set org.gnome.desktop.interface gtk-theme ${settings.common.gtk.theme}
+    gsettings set org.gnome.desktop.interface color-scheme ${settings.common.styles.colorScheme}
     gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Classic
     gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
-    gsettings set org.gnome.desktop.interface font-name "CaskaydiaCove Nerd Font 10"
+    gsettings set org.gnome.desktop.interface font-name "${settings.modules.fonts.main.name} ${
+      toString settings.modules.fonts.main.size.main
+    }"
 
     # ---- hyprpolkitagent for Hyprland ---- #
     systemctl --user start hyprpolkitagent
