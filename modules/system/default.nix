351f50c094c10fe23a722daa0aec510df19adc10
Islam Ahmed <softeng.islam@gmail.com>
2025-06-13 10:36:48 +0300

    update & Fix

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,6 +1,16 @@
 # ---- docs.nix ---- #
 { pkgs, ... }: {
-  imports = [ ./configuration.nix ./nixos.nix ./radeon.nix ./systemd.nix ];
+  imports = [
+    ./configuration.nix
+    ./nixos.nix
+
+    ./opengl.nix
+
+    #  Optional If using Radeon drivers
+    ./radeon.nix
+
+    ./systemd.nix
+  ];
   environment.systemPackages = with pkgs; [
     e2fsprogs
     smartmontools
