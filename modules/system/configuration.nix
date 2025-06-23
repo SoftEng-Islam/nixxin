314dc495a5c9a2b00dd728df39c944da3bfe21c8
Islam Ahmed <softeng.islam@gmail.com>
2025-06-23 12:22:24 +0300

    update & Fix

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -336,7 +336,7 @@
   # ---- Hardware Configuration
   # ------------------------------------------------
   hardware = {
-    firmware = pkgs.linux-firmware;
+    firmware = [ pkgs.linux-firmware ];
     uinput.enable = true;
     enableAllFirmware = true;
     cpu.amd.updateMicrocode = true;
