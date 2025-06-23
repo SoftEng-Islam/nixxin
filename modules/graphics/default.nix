71bc6de15e8eb45df0465c0a7d8e760cdc2e6be2
Islam Ahmed <softeng.islam@gmail.com>
2025-06-23 19:04:59 +0300

    update & Fix

--- modules/graphics/default.nix
+++ modules/graphics/default.nix
@@ -13,7 +13,7 @@ let
     (lib.optional settings.modules.graphics.kolourpaint kolourpaint)
   ];
 in {
-  imports = [ ./davinci-resolve.nix ];
+  # imports = [ ./davinci-resolve.nix ];
   config = lib.optionals (settings.modules.graphics.enable) {
     environment.systemPackages = lib.flatten _graphics;
   };
