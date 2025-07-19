852ef42f14ab87815d2e3d251f889c4ea8664e05
Islam Ahmed <softeng.islam@gmail.com>
2025-07-19 15:41:15 +0300

    update & Fix

--- modules/cli_tools/utilities/emacs.nix
+++ modules/cli_tools/utilities/emacs.nix
@@ -2,4 +2,10 @@
   home-manager.users.${settings.user.username} = {
     programs.emacs = { enable = true; };
   };
+  environment.systemPackages = with pkgs; [
+    emacs
+    emacs-all-the-icons-fonts
+    emacs-gtk
+    emacs-lsp-booster
+  ];
 }
