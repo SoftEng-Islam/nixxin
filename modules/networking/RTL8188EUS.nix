b36418a67b4a3586d326b7cf1c2e89d37d045495
Islam Ahmed <softeng.islam@gmail.com>
2025-07-31 11:11:33 +0300

    updates & Improvements

--- modules/networking/RTL8188EUS.nix
+++ modules/networking/RTL8188EUS.nix
@@ -30,8 +30,8 @@ lib.mkIf (settings.modules.networking.rtl8188eus-aircrack or false) {
   # Up to kernel v6.5+ ... And a bunch of various wifi chipsets
   # Blacklist conflicting drivers
   boot.blacklistedKernelModules = [
-    # "rtl8xxxu" # My Default Wifi Driver
-    # "r8188eu"
+    "rtl8xxxu" # My Default Wifi Driver
+    "r8188eu"
   ];
 
   # Install the patched rtl8188eus driver (supports monitor mode)
@@ -39,11 +39,10 @@ lib.mkIf (settings.modules.networking.rtl8188eus-aircrack or false) {
     [
       # Aircrack-ng kernel module for Realtek 88XXau network cards (8811au, 8812au, 8814au and 8821au chipsets) with monitor mode and injection support.
       # rtl88xxau-aircrack
-
       rtl8188eus-aircrack
     ];
 
-  # boot.kernelModules = [ "8188eu" ];
+  boot.kernelModules = [ "8188eu" ];
 
   # Optional: Disable power saving for better WiFi stability
   boot.kernelParams = [ "rtl8188eus.ips_mode=0" ];
