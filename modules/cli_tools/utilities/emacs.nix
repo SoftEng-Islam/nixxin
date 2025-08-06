04c73fa82c83982d79b087b5acfc9cd7e7d2279f
Islam Ahmed <softeng.islam@gmail.com>
2025-08-07 00:56:45 +0300

    updates & Improvements

--- modules/cli_tools/utilities/emacs.nix
+++ modules/cli_tools/utilities/emacs.nix
@@ -5,20 +5,20 @@ lib.mkIf (settings.modules.cli_tools.utilities.emacs.enable or false) {
   services.emacs.package = pkgs.emacs;
   services.emacs.defaultEditor = false;
 
-  home-manager.users.${settings.user.username} = {
-    programs.emacs = {
-      enable = true;
-      extraPackages = with pkgs; [
-        emacs-all-the-icons-fonts
-        emacs-gtk
-        emacs-lsp-booster
-      ];
-      config = {
-        # Add your Emacs configuration here
-        # For example, you can set the theme, keybindings, etc.
-      };
-    };
-  };
+  # home-manager.users.${settings.user.username} = {
+  #   programs.emacs = {
+  #     enable = true;
+  #     extraPackages = with pkgs; [
+  #       emacs-all-the-icons-fonts
+  #       emacs-gtk
+  #       emacs-lsp-booster
+  #     ];
+  #     config = {
+  #       # Add your Emacs configuration here
+  #       # For example, you can set the theme, keybindings, etc.
+  #     };
+  #   };
+  # };
 
   environment.systemPackages = with pkgs; [
     emacs
