ace80702ca45d9f61ea31e8f841efa5dc7b60374
Islam Ahmed <softeng.islam@gmail.com>
2025-08-31 14:06:30 +0300

    updates & Improvements

--- modules/security/vpn/mullvad/default.nix
+++ modules/security/vpn/mullvad/default.nix
@@ -1,11 +1,3 @@
-{ config, lib, pkgs, ... }:
-
-let
-  inherit (lib) mkIf optional;
-
-  cfg = config.icedos.apps;
-in mkIf (cfg.mullvad.enable) {
-  environment.systemPackages = mkIf (cfg.mullvad.gui) [ pkgs.mullvad-vpn ];
-
-  services.mullvad-vpn.enable = true;
+{ settings, lib, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ mullvad-vpn ];
 }
