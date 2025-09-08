75a5dc4819ec18680993fb7a79f26308c0e60274
Islam Ahmed <softeng.islam@gmail.com>
2025-09-08 22:52:18 +0300

    updates & Improvements

--- modules/cli_tools/terminals/wezterm/default.nix
+++ modules/cli_tools/terminals/wezterm/default.nix
@@ -59,7 +59,7 @@
         config.audible_bell = 'Disabled'
         config.colors = {
           -- The color of the scrollbar "thumb"; the portion that represents the current viewport
-          scrollbar_thumb = '#303030',
+          scrollbar_thumb = '#161616',
         }
 
 
