f48ca039584a4fa30a7f2b9d9c20c3c0c418fd97
Islam Ahmed <softeng.islam@gmail.com>
2025-09-09 09:02:43 +0300

    updates & Improvements

--- modules/security/OpenVPN.nix
+++ modules/security/OpenVPN.nix
@@ -1,4 +1,11 @@
 { settings, lib, pkgs, ... }: {
+  # See `man openvpn' and `man 5 openvpn' for details on the options below.
+  # To debug connection issues, check the status of the OpenVPN service with
+  # `sudo systemctl status openvpn-<name>', where `<name>' is the name of the
+  # entry below (e.g. `home' for the example below). For example:
+  # sudo systemctl status openvpn-home
+  # sudo journalctl -u openvpn-home -e | tail -n 50
+
   services.openvpn.servers = {
     # Unless `autoStart = false;', all entries here start automatically as a
     # systemd service. To stop the `home' OpenVPN client service, run `sudo
