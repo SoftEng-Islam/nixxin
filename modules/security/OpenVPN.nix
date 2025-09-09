70f720035f90b962f7f54011ff80e98b9a4aee34
Islam Ahmed <softeng.islam@gmail.com>
2025-09-09 08:10:23 +0300

    updates & Improvements

--- modules/security/OpenVPN.nix
+++ modules/security/OpenVPN.nix
@@ -3,8 +3,10 @@
     # Unless `autoStart = false;', all entries here start automatically as a
     # systemd service. To stop the `home' OpenVPN client service, run `sudo
     # systemctl stop openvpn-home'.
-    home = { config = builtins.readFile ./home.ovpn; };
-    autoStart = true;
+    home = {
+      config = builtins.readFile ./home.ovpn;
+      autoStart = true;
+    };
   };
   environment.systemPackages = with pkgs; [
     openvpn # Robust and highly flexible tunneling application
