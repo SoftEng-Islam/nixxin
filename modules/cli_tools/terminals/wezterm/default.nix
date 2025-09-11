309eb0c465718e76e0582c5e51cbdb07e6eb2689
Islam Ahmed <softeng.islam@gmail.com>
2025-09-11 22:33:52 +0300

    updates & Improvements

--- modules/cli_tools/terminals/wezterm/default.nix
+++ modules/cli_tools/terminals/wezterm/default.nix
@@ -118,7 +118,28 @@
             mods = 'CTRL|ALT',
             action = wezterm.action.SpawnTab 'CurrentPaneDomain'
           },
-        }
+          {
+            key = 'c',
+            mods = 'CTRL',
+            action = wezterm.action_callback(function(window, pane)
+              local sel = window:get_selection_text_for_pane(pane)
+              if (not sel or sel == "") then
+                window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+              else
+                window:perform_action(wezterm.action{ CopyTo = 'ClipboardAndPrimarySelection' }, pane)
+              end
+            end),
+          },
+          { key = 'v', mods = 'CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
+          { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+            window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+          },
+          { key = 'V', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+            window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+          },
+          { key = 'c', mods = 'ALT', action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection' },
+          { key = 'v', mods = 'ALT', action = wezterm.action.PasteFrom 'Clipboard' },
+        },
 
         -- Finally, return the configuration to wezterm:
         return config
