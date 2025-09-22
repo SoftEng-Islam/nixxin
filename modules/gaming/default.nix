e1cb6e34a84dcede737165006622b28bea9bf145
Islam Ahmed <softeng.islam@gmail.com>
2025-09-22 23:57:34 +0300

    updates & Improvements

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -23,7 +23,7 @@ in {
           };
           gpu = {
             apply_gpu_optimisations = "accept-responsibility";
-            gpu_device = 0;
+            gpu_device = 1;
             amd_performance_level = "high";
             nv_powermizer_mode = 1;
             # APU specific settings
