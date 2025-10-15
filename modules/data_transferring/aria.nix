3f61e7d37594ca536d5e4e23fa6cfc24c2292e58
Islam Ahmed <softeng.islam@gmail.com>
2025-10-16 00:43:22 +0300

    updates & Improvements

--- modules/data_transferring/aria.nix
+++ modules/data_transferring/aria.nix
@@ -4,13 +4,6 @@ in mkIf (settings.modules.data_transferring.aria.enable or true) {
   home-manager.users.${settings.user.username} = {
     home.file.".config/aria2/aria2.conf".text = lib.mkForce ''
       continue=true
-      async-dns=false
-      max-concurrent-downloads=5
-      summary-interval=5
-      console-log-level=warn
-      split=16
-      max-connection-per-server=16
-      min-split-size=1M
       timeout=15
       max-tries=20
       retry-wait=5
@@ -19,10 +12,6 @@ in mkIf (settings.modules.data_transferring.aria.enable or true) {
       max-file-not-found=2
       enable-dht=true
       enable-dht6=false
-      listen-port=6881-6999
-      max-overall-upload-limit=1M
-      max-upload-limit=50K
-      check-integrity=true
     '';
   };
 
