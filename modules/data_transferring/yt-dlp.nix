d296b3aa2e0c0fdcb475e3b36179c70421d6b4ac
Islam Ahmed <softeng.islam@gmail.com>
2025-10-16 03:55:56 +0300

    updates & Improvements

--- modules/data_transferring/yt-dlp.nix
+++ modules/data_transferring/yt-dlp.nix
@@ -9,7 +9,7 @@
         sponsorblock-mark = "all";
         # downloader = lib.getExe pkgs.aria2;
         downloader = "aria2c";
-        downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+        downloader-args = "aria2c:'-c -x8 -s8 -k1024M'";
         cookies-from-browser = "chrome";
         format = "bestvideo[height<=?1080][fps<=?60]+bestaudio/best";
       };
