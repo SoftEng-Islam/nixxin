4f166b9e1c6c6806e8d4485933250a8d11904c43
Islam Ahmed <softeng.islam@gmail.com>
2025-10-20 01:43:44 +0300

    updates & Improvements

--- pkgs/clvk.nix
+++ pkgs/clvk.nix
@@ -11,14 +11,14 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-TKmtbVBeu1hHoff+E5dkmcb5p8JaXUNv6Sg8lH/bSSQ=";
   };
 
-  nativeBuildInputs = [ cmake python3 git pkg-config ];
-  buildInputs = [ llvmPackages.clang llvmPackages.llvm ];
+  nativeBuildInputs = with pkgs; [ cmake python3 git pkg-config ];
+  buildInputs = with pkgs; [ llvmPackages.clang llvmPackages.llvm ];
 
   # run the fetch_sources.py script before CMake
   preConfigure = ''
     echo ">>> Running CLVK dependency fetch script..."
     cd external/clspv
-    ${python3}/bin/python3 utils/fetch_sources.py
+    ${pkgs.python3}/bin/python3 utils/fetch_sources.py
     cd ../../
   '';
 
