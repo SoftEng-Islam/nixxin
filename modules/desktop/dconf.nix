532636f680906e3f46a7834d46eb133a1e5b3493
Islam Ahmed <softeng.islam@gmail.com>
2025-10-24 03:42:00 +0300

    updates & Improvements

--- modules/desktop/dconf.nix
+++ modules/desktop/dconf.nix
@@ -1,13 +1,13 @@
 { settings, lib, pkgs, ... }:
+let
+  # you can try this command if you have any problem with gnome settings
+  # dconf reset -f /org/gnome/
 
-# you can try this command if you have any problem with gnome settings
-# dconf reset -f /org/gnome/
+  # Run the following command to disable the Gnome check-alive-timeout or "App Not Responding" dialog:
+  # dconf write /org/gnome/mutter/debug/enable-frame-timing false
+  # gsettings set org.gnome.mutter check-alive-timeout 0
 
-# Run the following command to disable the Gnome check-alive-timeout or "App Not Responding" dialog:
-# dconf write /org/gnome/mutter/debug/enable-frame-timing false
-# gsettings set org.gnome.mutter check-alive-timeout 0
-
-with lib.gvariant; {
+in with lib.gvariant; {
   programs.dconf.enable = true;
   environment.systemPackages = with pkgs; [ dconf ];
 
@@ -58,6 +58,94 @@ with lib.gvariant; {
         "type-format" = "category";
         "view-type" = "list";
       };
+
+      # To list your enabled GNOME Shell extensions, you can use the gnome-extensions command-line tool, which provides various options for managing extensions.
+      # gnome-extensions list --enabled
+      "org/gnome/shell" = { disable-user-extensions = true; };
+      # dconf read /org/freedesktop/Tracker3/Miner/Files/enable-monitors
+      "org/freedesktop/Tracker3/Miner/Files/enable-monitors" = {
+        enable-monitors = false;
+      };
+
+      # Run this Command In the terminal to get list of options:
+      # gsettings list-keys org.gnome.desktop.interface
+
+      # gsettings get org.gnome.desktop.interface font-antialiasing
+      # 'grayscale'
+      "org/gnome/desktop/interface" = {
+        # accent-color
+        # avatar-directories
+        can-change-accels = false;
+        clock-format = "12h";
+        clock-show-date = true;
+        clock-show-seconds = false;
+        clock-show-weekday = true;
+        color-scheme = settings.modules.dconf.colorScheme;
+        cursor-blink = true;
+        cursor-blink-time = 500;
+        cursor-blink-timeout = 10;
+        cursor-size = settings.common.cursor.size;
+        cursor-theme = settings.common.cursor.name;
+        enable-animations = true;
+        # enable-hot-corners
+
+        # ---- Fonts ---- #
+        font-name = lib.mkForce "${settings.modules.fonts.main.name} ${
+            toString settings.modules.fonts.main.size.main
+          }";
+        document-font-name = lib.mkForce settings.modules.fonts.main.name;
+        font-hinting = lib.mkForce settings.modules.fonts.main.hinting;
+        font-rendering = lib.mkForce settings.modules.fonts.main.rendering;
+        font-antialiasing =
+          lib.mkForce settings.modules.fonts.main.antialiasing;
+        font-rgba-order = lib.mkForce settings.modules.fonts.main.rgba_order;
+
+        # gtk-color-palette
+
+        # $ gsettings describe org.gnome.desktop.interface gtk-color-scheme
+        # $ gsettings get org.gnome.desktop.interface gtk-color-scheme
+        # gtk-color-scheme = lib.mkForce "${settings.modules.dconf.colorScheme}";
+
+        # gtk-enable-primary-paste
+        # gtk-im-module
+        # gtk-im-preedit-style
+        # gtk-im-status-style
+        # gtk-key-theme
+        gtk-theme = lib.mkForce "${settings.common.gtk.theme}";
+        # gtk-timeout-initial
+        # gtk-timeout-repeat
+        icon-theme = "${settings.modules.desktop.dconf.icons.nameInDark}";
+        # locate-pointer
+        # menubar-accel
+        # menubar-detachable
+        # menus-have-tearoff
+        # monospace-font-name
+        # overlay-scrolling
+        # scaling-factor
+        # show-battery-percentage
+        # text-scaling-factor
+        # toolbar-detachable
+        # toolbar-icons-size
+        # toolbar-style
+        # toolkit-accessibility
+      };
+      # gsettings set org.gnome.nautilus.preferences show-image-thumbnails 'always'
+      # gsettings set org.gnome.nautilus.preferences thumbnail-limit 104857600
+      "org/gnome/desktop/thumbnail-cache" = {
+        maximum-age = 200;
+        maximum-size = 2048;
+      };
+
+      # "org/gnome/mutter" = {
+      #   center-new-windows = true;
+      #   check-alive-timeout = 0;
+      #   dynamic-workspaces = false;
+      #   edge-tiling = true;
+      #   experimental-features = "['scale-monitor-framebuffer']";
+      #   num-workspaces = 6;
+      #   workspaces-only-on-primary = true;
+      # };
+
     };
   };
 }
--- modules/icons/default.nix
+++ modules/icons/default.nix
@@ -1,7 +1,7 @@
 { settings, lib, pkgs, ... }:
 let inherit (lib) mkIf;
 
-in mkIf (settings.modules.icons.enable or false) {
+in mkIf (settings.modules.icons.enable or true) {
   environment.systemPackages = with pkgs; [
     icon-library # Symbolic icons for your apps
     papirus-folders
