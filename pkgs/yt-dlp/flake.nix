529b9c5ca2508885d65e5818ac0200975449627d
Islam Ahmed <softeng.islam@gmail.com>
2025-11-05 04:04:54 +0200

    updates & Improvements

--- pkgs/yt-dlp/flake.nix
+++ pkgs/yt-dlp/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     yt-dlp-src = {
-      url = "github:yt-dlp/yt-dl/master";
+      url = "github:yt-dlp/yt-dl";
       flake = false;
     };
   };
