15f12120268c6ea72c3f6a6ff4adb4b1223ef88e
Islam Ahmed <softeng.islam@gmail.com>
2025-11-05 04:13:02 +0200

    updates & Improvements

--- modules/data_transferring/yt-dlp.nix
+++ modules/data_transferring/yt-dlp.nix
@@ -2,7 +2,7 @@
   home-manager.users.${settings.user.username} = {
     programs.yt-dlp = {
       enable = settings.modules.data_transferring.yt-dlp.enable or false;
-      package = inputs.yt-dlp.packages.${pkgs.system}.default;
+      package = inputs.yt-dlp-src.packages.${pkgs.system}.default;
       # package = pkgs.yt-dlp;
 
       settings = {
