669522aa7c694cb6040eeb2bc1532159e7b2b3eb
Islam Ahmed <softeng.islam@gmail.com>
2025-11-08 04:43:49 +0200

    updates & Improvements

--- modules/desktop/hyprland/configs/animations.nix
+++ modules/desktop/hyprland/configs/animations.nix
@@ -20,28 +20,6 @@ in {
     wayland.windowManager.hyprland = {
       settings.animations = {
         enabled = true;
-        bezier = [
-          # "linear, 0, 0, 1, 1"
-          # "md3_standard, 0.2, 0, 0, 1"
-          # "md3_decel, 0.05, 0.7, 0.1, 1"
-          # "md3_accel, 0.3, 0, 0.8, 0.15"
-          # "overshot, 0.05, 0.9, 0.1, 1.1"
-          # "crazyshot, 0.1, 1.5, 0.76, 0.92"
-          # "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
-          # "menu_decel, 0.1, 1, 0, 1"
-          # "menu_accel, 0.38, 0.04, 1, 0.07"
-          # "easeInOutCirc, 0.85, 0, 0.15, 1"
-          # "easeOutCirc, 0, 0.55, 0.45, 1"
-          # "easeOutExpo, 0.16, 1, 0.3, 1"
-          # "softAcDecel, 0.26, 0.26, 0.15, 1"
-          # "md2, 0.4, 0, 0.2, 1"
-
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-          "almostLinear,0.5,0.5,0.75,1.0"
-          "quick,0.15,0,0.1,1"
-        ];
         animation = [
           # "windows, 1, ${animationDuration}, md3_decel, popin 60%"
           # "windowsIn, 1, ${animationDuration}, md3_decel, popin 60%"
