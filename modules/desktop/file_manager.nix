fa4eddb9a08eee22ab5f1327703c10469c62a6b0
Islam Ahmed <softeng.islam@gmail.com>
2025-11-08 04:11:13 +0200

    updates & Improvements

--- modules/desktop/file_manager.nix
+++ modules/desktop/file_manager.nix
@@ -18,7 +18,7 @@
 
   nixpkgs.overlays = [
     (self: super: {
-      gnome = super.gnome.overrideScope' (gself: gsuper: {
+      gnome = super.gnome.overrideScope (gself: gsuper: {
         nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
           buildInputs = nsuper.buildInputs
             ++ (with pkgs.gst_all_1; [ gst-plugins-good gst-plugins-bad ]);
