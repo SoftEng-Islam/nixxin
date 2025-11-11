79079a95e53746ec2f23d055d49d7ae2ac94788a
Islam Ahmed <softeng.islam@gmail.com>
2025-11-11 03:19:27 +0200

    updates & Improvements

--- modules/desktop/hyprland/shaders/blue-light-filter.glsl
+++ modules/desktop/hyprland/shaders/blue-light-filter.glsl
@@ -8,7 +8,7 @@ uniform sampler2D tex;
 out vec4 fragColor;
 
 const float temperature = 2600.0;
-const float temperatureStrength = 0.75;
+const float temperatureStrength = 0.60;
 
 #define WithQuickAndDirtyLuminancePreservation
 const float LuminancePreservationFactor = 1.0;
