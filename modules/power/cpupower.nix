c14ec400c0014b2b8f9a6386d2c5d999879aca39
Islam Ahmed <softeng.islam@gmail.com>
2025-11-26 02:14:23 +0200

    Update

--- modules/power/cpupower.nix
+++ modules/power/cpupower.nix
@@ -1,9 +1,9 @@
 { settings, lib, pkgs, ... }:
 lib.mkIf (settings.modules.power.cpupower.enable or false) {
-  services.cpupower-gui.enable = true; # GUI frontend for CPU governor/freq
+  services.cpupower-gui.enable = false; # GUI frontend for CPU governor/freq
   environment.systemPackages = with pkgs; [
-    cpupower-gui
-    # cpufrequtils
+    # cpupower-gui
+    cpufrequtils
     perf-tools
     linuxKernel.packages.linux_zen.cpupower
   ];
