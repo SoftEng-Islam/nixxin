3824d8e46d32b8fa33ba2636efd38614c33c300f
Islam Ahmed <softeng.islam@gmail.com>
2025-11-27 05:13:19 +0200

    Update

--- modules/office/n8n.nix
+++ modules/office/n8n.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = { };
   };
-  systemd.services.n8n.serviceConfig.ProtectHome = "tmpfs";
+  # systemd.services.n8n.serviceConfig.ProtectHome = "tmpfs";
   systemd.services.n8n.serviceConfig.BindPaths = "/home/user/n8n";
   environment.variables.N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS = "true";
   environment.systemPackages = with pkgs; [ n8n ];
