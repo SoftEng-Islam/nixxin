52d3f796f7a1e5cae1a9c2a2b7fd81b447a5793f
Islam Ahmed <softeng.islam@gmail.com>
2025-12-11 02:21:20 +0200

    Update

--- modules/office/n8n.nix
+++ modules/office/n8n.nix
@@ -2,11 +2,11 @@
   # Free and source-available fair-code licensed workflow automation tool
 
   services.n8n = {
-    enable = false;
-    settings = { };
+    enable = true;
+    environment = { };
   };
   # systemd.services.n8n.serviceConfig.ProtectHome = "tmpfs";
-  systemd.services.n8n.serviceConfig.BindPaths = "/home/user/n8n";
-  environment.variables.N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS = "true";
+  # systemd.services.n8n.serviceConfig.BindPaths = "/home/user/n8n";
+  # environment.variables.N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS = "true";
   environment.systemPackages = with pkgs; [ n8n ];
 }
