c8f26cfed10d999706691fbe474be7b3ed080058
Islam Ahmed <softeng.islam@gmail.com>
2025-12-11 03:18:08 +0200

    Update

--- modules/desktop/hyprland/configs/exec.nix
+++ modules/desktop/hyprland/configs/exec.nix
@@ -4,7 +4,7 @@ let
       toString settings.modules.fonts.main.size.main
     }";
 
-  ashell = inputs.ashell.defaultPackage.${pkgs.system};
+  ashell = inputs.ashell.${pkgs.system};
 
   startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
     #!/usr/bin/env bash
