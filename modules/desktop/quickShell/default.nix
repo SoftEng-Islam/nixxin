f828a4b02333f73080d6107fb7eb607fa689817c
Islam Ahmed <softeng.islam@gmail.com>
2025-12-19 03:40:58 +0200

    update

--- modules/desktop/quickShell/default.nix
+++ modules/desktop/quickShell/default.nix
@@ -2,7 +2,7 @@
   # git clone https://github.com/Shanu-Kumawat/quickshell-overview ~/.config/quickshell/overview
 
   home-manager.users.${settings.user.username} = {
-    home.file."~/.config/quickshell/overview".source = ./quickshell/overview;
+    home.file."~/.config/quickshell/overview".source = ./overview;
   };
 
   environment.systemPackages = with pkgs; [ quickshell ];
