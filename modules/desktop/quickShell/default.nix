25a4d5abb87cd660d5331714b224e8ae93531ab8
Islam Ahmed <softeng.islam@gmail.com>
2025-12-19 03:58:52 +0200

    update

--- modules/desktop/quickShell/default.nix
+++ modules/desktop/quickShell/default.nix
@@ -5,5 +5,11 @@
     home.file."~/.config/quickshell/overview".source = ./overview;
   };
 
-  environment.systemPackages = with pkgs; [ quickshell ];
+  environment.systemPackages = with pkgs; [
+    quickshell
+    libsForQt5.qt5.qtsvg
+    libsForQt5.qt5.qtimageformats
+    libsForQt5.qt5.qtmultimedia
+    kdePackages.qt5compat
+  ];
 }
