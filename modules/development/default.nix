4f1d29b88bf84cfae381e406c9c5dae39a8f1b52
Islam Ahmed <softeng.islam@gmail.com>
2025-12-24 02:05:31 +0200

    update

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -10,10 +10,32 @@ let
     (optional development.eclipse ./eclipse.nix)
     (optional development.helix ./helix.nix)
   ];
-
+  HOME = config.home.homeDirectory;
 in {
   imports = optionals (development.enable or false) flatten _imports;
   config = mkIf (development.enable or false) {
+    environment.variables = {
+      # PNPM
+      state-dir = "${HOME}/.local/state/pnpm-state";
+      store-dir = "${HOME}/.local/share/pnpm-store";
+
+      # --- npm ---
+      # Prefer XDG paths
+      NPM_CONFIG_USERCONFIG = "$HOME/.config/npm/npmrc";
+      NPM_CONFIG_CACHE = "$HOME/.cache/npm";
+      NPM_CONFIG_PREFIX = "$HOME/.local/share/npm";
+
+      # --- pnpm ---
+      PNPM_HOME = "$HOME/.local/share/pnpm";
+      PNPM_STORE_PATH = "$HOME/.cache/pnpm-store";
+
+      # --- yarn ---
+      YARN_CACHE_FOLDER = "$HOME/.cache/yarn";
+      YARN_GLOBAL_FOLDER = "$HOME/.local/share/yarn";
+
+      # Node REPL history (keep it out of $HOME root)
+      NODE_REPL_HISTORY = "$HOME/.local/share/node/repl_history";
+    };
     services.mongodb.enable = true;
     # nixpkgs.config.permittedInsecurePackages = [ "beekeeper-studio-5.3.4" ];
     environment.systemPackages = with pkgs; [
