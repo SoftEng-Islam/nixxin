011c4f9c334ab203aa0ef90b59bf39460705bbf9
Islam Ahmed <softeng.islam@gmail.com>
2025-12-28 03:10:59 +0200

    update

--- flake.nix
+++ flake.nix
@@ -1,61 +1,51 @@
 {
   description = "NIXXIN Configuration.";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     systems.url = "github:nix-systems/default-linux";
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # nixGL.url = "github:nix-community/nixGL";
-    # nixGL.inputs.nixpkgs.follows = "nixpkgs";
-    # nixGL.inputs.flake-utils.follows = "flake-utils";
+    nixgl.url = "github:nix-community/nixGL";
 
     hyprpolkitagent.url = "github:hyprwm/hyprpolkitagent";
-
-    # nixpkgs-waydroid.url = "github:NixOS/nixpkgs/pull/455257/head";
-
-    # ashell.url = "github:MalpenZibo/ashell";
-    # ashell.flake = true;
 
     yt-dlp-src.url = "path:./pkgs/yt-dlp";
 
     quickshell = {
-      # add ?ref=<tag> to track a tag
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-
-      # THIS IS IMPORTANT
-      # Mismatched system dependencies will lead to crashes and other issues.
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # illogical-impulse.url = "github:xBLACKICEx/end-4-dots-hyprland-nixos";
-    # illogical-impulse.inputs.nixpkgs.follows = "nixpkgs";
-
   };
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      _SETTINGS = import (./. + "/_settings.nix") { inherit pkgs; };
-      settings = _SETTINGS.profile;
-      # pkgs = import nixpkgs { system = settings.system.architecture; };
-      pkgs = nixpkgs.legacyPackages.${settings.system.architecture};
 
+  outputs = { self, nixpkgs, nixgl, ... }@inputs:
+    let
+      settingsFile = import ./_settings.nix;
+      settings = settingsFile.profile;
+      system = settings.system.architecture;
     in {
-      # NixOS configuration entrypoint.
-      # sudo nixos-rebuild switch --flake .#YourHostname
       nixosConfigurations = {
         "${settings.system.hostName}" = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit self;
-            inherit inputs;
-            inherit _SETTINGS;
-            inherit settings;
-          };
+          inherit system;
+
+          specialArgs = { inherit self inputs settings; };
+
           modules = [
-            # inputs.stylix.nixosModules.stylix
+            # Apply nixpkgs config + overlays HERE (correct place)
+            ({ ... }: {
+              nixpkgs = {
+                overlays = [ nixgl.overlay ];
+                config.allowUnfree = true;
+              };
+            })
+
             inputs.home-manager.nixosModules.home-manager
-            (./. + _SETTINGS.path + "/configuration.nix")
+
+            (./. + settingsFile.path + "/configuration.nix")
           ];
         };
       };
