f345676248ee0fce94fd477af524b3965e19d64b
Islam Ahmed <softeng.islam@gmail.com>
2025-12-28 03:15:31 +0200

    update

--- flake.nix
+++ flake.nix
@@ -4,54 +4,28 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
     flake-utils.url = "github:numtide/flake-utils";
-
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     nixgl.url = "github:nix-community/nixGL";
     # nixGL.inputs.nixpkgs.follows = "nixpkgs";
-    # nixGL.inputs.flake-utils.follows = "flake-utils";
-
-    hyprpolkitagent.url = "github:hyprwm/hyprpolkitagent";
-
+    # nixGL.inputs.flake-utils.follows = "flake-utils"; hyprpolkitagent.url = "github:hyprwm/hyprpolkitagent";
     # nixpkgs-waydroid.url = "github:NixOS/nixpkgs/pull/455257/head";
-
     # ashell.url = "github:MalpenZibo/ashell";
-    # ashell.flake = true;
-
-    yt-dlp-src.url = "path:./pkgs/yt-dlp";
-
+    # ashell.flake = true; yt-dlp-src.url = "path:./pkgs/yt-dlp";
     quickshell = {
-      # add ?ref=<tag> to track a tag
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-
+      # add ?ref=<tag> to track a tag url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       # THIS IS IMPORTANT
       # Mismatched system dependencies will lead to crashes and other issues.
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     # illogical-impulse.url = "github:xBLACKICEx/end-4-dots-hyprland-nixos";
     # illogical-impulse.inputs.nixpkgs.follows = "nixpkgs";
-
   };
   outputs = { self, nixgl, nixpkgs, ... }@inputs:
     let
-      # _SETTINGS = import (./. + "/_settings.nix") { inherit pkgs; };
-      # settings = _SETTINGS.profile;
-      # pkgs = nixpkgs.legacyPackages.${settings.system.architecture};
-
-      settingsFile = import ./_settings.nix;
-
-      settings = settingsFile.profile;
-      system = settings.system.architecture;
-
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ nixgl.overlay ];
-        config.allowUnfree = true;
-      };
-
-      _SETTINGS = settingsFile { inherit pkgs; };
+      _SETTINGS = import (./. + "/_settings.nix") { inherit pkgs; };
+      settings = _SETTINGS.profile;
+      pkgs = nixpkgs.legacyPackages.${settings.system.architecture};
     in {
       # NixOS configuration entrypoint.
       # sudo nixos-rebuild switch --flake .#YourHostname
@@ -64,7 +38,7 @@
             inherit settings;
           };
           modules = [
-            # inputs.stylix.nixosModules.stylix
+
             inputs.home-manager.nixosModules.home-manager
             (./. + _SETTINGS.path + "/configuration.nix")
           ];
