15a7294678bd3ea9404c9b910fd5eb4f035587bf
Islam Ahmed <softeng.islam@gmail.com>
2025-12-28 03:18:05 +0200

    update

--- flake.nix
+++ flake.nix
@@ -4,23 +4,43 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
     flake-utils.url = "github:numtide/flake-utils";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixgl.url = "github:nix-community/nixGL";
+
+    # nixGL.url = "github:nix-community/nixGL";
     # nixGL.inputs.nixpkgs.follows = "nixpkgs";
-    # nixGL.inputs.flake-utils.follows = "flake-utils"; hyprpolkitagent.url = "github:hyprwm/hyprpolkitagent";
+    # nixGL.inputs.flake-utils.follows = "flake-utils";
+
+    hyprpolkitagent.url = "github:hyprwm/hyprpolkitagent";
+
     # nixpkgs-waydroid.url = "github:NixOS/nixpkgs/pull/455257/head";
+
     # ashell.url = "github:MalpenZibo/ashell";
-    # ashell.flake = true; yt-dlp-src.url = "path:./pkgs/yt-dlp";
+    # ashell.flake = true;
+
+    yt-dlp-src.url = "path:./pkgs/yt-dlp";
+
+    quickshell = {
+      # add ?ref=<tag> to track a tag
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+
+      # THIS IS IMPORTANT
+      # Mismatched system dependencies will lead to crashes and other issues.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # illogical-impulse.url = "github:xBLACKICEx/end-4-dots-hyprland-nixos";
     # illogical-impulse.inputs.nixpkgs.follows = "nixpkgs";
+
   };
-  outputs = { self, nixgl, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       _SETTINGS = import (./. + "/_settings.nix") { inherit pkgs; };
       settings = _SETTINGS.profile;
+      # pkgs = import nixpkgs { system = settings.system.architecture; };
       pkgs = nixpkgs.legacyPackages.${settings.system.architecture};
+
     in {
       # NixOS configuration entrypoint.
       # sudo nixos-rebuild switch --flake .#YourHostname
@@ -33,7 +53,7 @@
             inherit settings;
           };
           modules = [
-
+            # inputs.stylix.nixosModules.stylix
             inputs.home-manager.nixosModules.home-manager
             (./. + _SETTINGS.path + "/configuration.nix")
           ];
