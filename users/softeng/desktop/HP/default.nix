fd7c3f8587cbfd916657a1e1d8cec27614c646ee
Islam Ahmed <softeng.islam@gmail.com>
2026-01-10 04:50:08 +0200

    update

--- users/softeng/desktop/HP/default.nix
+++ users/softeng/desktop/HP/default.nix
@@ -362,7 +362,7 @@
 
   # [Networking]
   modules.networking.dnsResolver = "resolved"; # "dnsmasq" or "resolved"
-  modules.networking.nameservers = [ "1.1.1.1" "8.8.8.8" ]; # DNS
+  modules.networking.nameservers = [ "8.8.8.8" ]; # DNS
   modules.networking.dnsmasq.settings.server = modules.networks.nameservers;
   modules.networking.interfaces = {
     # eno1 = {
