dceecf677dd90aa27e3b7994611ee88703270847
Islam Ahmed <softeng.islam@gmail.com>
2026-01-12 01:42:07 +0200

    update

--- pkgs/clvk/flake.nix
+++ pkgs/clvk/flake.nix
@@ -14,7 +14,7 @@
   outputs = { self, nixpkgs, flake-utils, clvk-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 
         # Fetch LLVM manually because clspv needs it and we can't run the python script
         llvmProject = pkgs.fetchFromGitHub {
