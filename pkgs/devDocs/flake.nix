c0d54522f4c3cc53ebd57c5eecf84dd3bc94c8b9
Islam Ahmed <softeng.islam@gmail.com>
2026-01-18 01:25:09 +0200

    update

--- pkgs/devDocs/flake.nix
+++ pkgs/devDocs/flake.nix
@@ -3,24 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
 
+        ruby = pkgs.ruby_3_3;
+
         gems = pkgs.bundlerEnv {
           name = "devdocs-env";
-          ruby = pkgs.ruby;
+          inherit ruby;
           gemdir = ./.;
         };
       in {
         packages.default = gems;
 
-        devShells.default = pkgs.mkShell { buildInputs = [ pkgs.nodejs ]; }
-          // gems.env;
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ gems ];
+          packages = [ pkgs.nodejs ruby ];
+        };
       });
-
 }
