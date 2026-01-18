a1384d92571602eb6910e25c71cc4aae5034e658
Islam Ahmed <softeng.islam@gmail.com>
2026-01-18 04:54:05 +0200

    update

--- modules/graphics/clvk.nix
+++ modules/graphics/clvk.nix
@@ -3,18 +3,22 @@ let
   system = pkgs.stdenv.hostPlatform.system;
   nixos-opencl = inputs.nixos-opencl;
 in {
-  environment.systemPackages = with pkgs; [ clinfo opencl-headers ];
+  environment.systemPackages = with pkgs; [
+    clinfo
+    opencl-headers
+    nixos-opencl.packages.${system}.clvk
+  ];
   # hardware.graphics.extraPackages = [ nixos-opencl.packages.${system}.clvk ];
   environment.sessionVariables = {
-    OCL_ICD_VENDORS = let
-      drivers = [
-        # nixos-opencl.packages.${system}.pocl
-        nixos-opencl.packages.${system}.clvk
-      ];
-    in pkgs.symlinkJoin {
-      name = "opencl-vendors";
-      paths = drivers;
-    };
+    # OCL_ICD_VENDORS = let
+    #   drivers = [
+    #     # nixos-opencl.packages.${system}.pocl
+    #     nixos-opencl.packages.${system}.clvk
+    #   ];
+    # in pkgs.symlinkJoin {
+    #   name = "opencl-vendors";
+    #   paths = drivers;
+    # };
   };
 
 }
