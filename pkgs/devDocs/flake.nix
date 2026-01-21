ea4b047a47535045cd32cff16bda267310d64f23
Islam Ahmed <softeng.islam@gmail.com>
2026-01-22 01:32:08 +0200

    update

--- pkgs/devDocs/flake.nix
+++ pkgs/devDocs/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, ... }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
       packages.default = pkgs.bundlerEnv {
