65894fbd94784f30b90ac536a1003e8a440ab70a
Islam Ahmed <softeng.islam@gmail.com>
2026-01-21 03:36:32 +0200

    update

--- pkgs/devDocs/flake.nix
+++ pkgs/devDocs/flake.nix
@@ -1,35 +1,23 @@
 {
-  description = "DevDocs flake";
+  description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, utils }:
+    { } // utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        gems = pkgs.bundlerEnv {
+          name = "devdocs-env";
+          ruby = pkgs.ruby;
+          gemdir = ./.;
+        };
+      in {
+        packages.${system}.default = gems;
 
-      ruby = pkgs.ruby_3_4;
-
-      gems = pkgs.bundlerEnv {
-        name = "devdocs-env";
-        inherit ruby;
-        gemdir = ./.;
-        groups = [ "default" ];
-      };
-    in {
-      packages.${system}.default = gems;
-
-      devShells.${system}.default = pkgs.mkShell {
-        inputsFrom = [ gems ];
-        packages = [
-          ruby
-          pkgs.nodejs
-          pkgs.pngquant
-          pkgs.jpegoptim
-          pkgs.optipng
-          pkgs.gifsicle
-          pkgs.svgo
-        ];
-      };
-    };
+        devShell = with pkgs; mkShell { buildInputs = [ nodejs ]; } // gems.env;
+      });
 }
