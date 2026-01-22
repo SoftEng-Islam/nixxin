a50b81048f1a160436a83d215f325d49a53be312
Islam Ahmed <softeng.islam@gmail.com>
2026-01-22 02:03:59 +0200

    update

--- pkgs/devDocs/flake.nix
+++ pkgs/devDocs/flake.nix
@@ -1,26 +1,32 @@
-{
-  description = "A very basic flake";
+{ pkgs }:
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+let
+  ruby = pkgs.ruby_3_4;
+  targetRuby = pkgs.ruby_3_4;
+  myBundler = pkgs.bundler.override { ruby = targetRuby; };
+  gems = pkgs.bundlerEnv {
+    name = "devdocs-gems";
+    # inherit ruby;
+    ruby = targetRuby;
+    bundler = myBundler;
+    gemdir = ./.;
+    extraConfigPaths = [ "${./.}/.ruby-version" ];
   };
+in pkgs.stdenv.mkDerivation {
+  pname = "devdocs";
+  version = "unstable";
 
-  outputs = { self, nixpkgs, utils, ... }:
-    { } // utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        targetRuby = pkgs.ruby_3_4;
-        myBundler = pkgs.bundler.override { ruby = targetRuby; };
-        gems = pkgs.bundlerEnv {
-          name = "devdocs-env";
-          ruby = targetRuby;
-          bundler = myBundler;
-          gemdir = ./.;
-          extraConfigPaths = [ "${./.}/.ruby-version" ];
-        };
-      in {
-        packages.default = gems;
-        # devShell = with pkgs; mkShell { buildInputs = [ nodejs ]; } // gems.env;
-      });
+  src = ./.;
+
+  nativeBuildInputs = [ gems gems.wrappedRuby ];
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r . $out/
+  '';
+
+  meta = {
+    description = "DevDocs packaged with bundlerEnv";
+    platforms = ruby.meta.platforms;
+  };
 }
