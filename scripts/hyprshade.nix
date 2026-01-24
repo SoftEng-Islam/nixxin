885b4cbce085ed6e4d1f0939dd8d1ecfdd0b390b
Islam Ahmed <softeng.islam@gmail.com>
2026-01-24 03:21:35 +0200

    update

--- scripts/hyprshade.nix
+++ scripts/hyprshade.nix
@@ -1,14 +1,14 @@
 { settings, pkgs, ... }:
 let
   blue-filter = pkgs.writeShellScriptBin "run-blue-filter" ''
-    # ${pkgs.hyprshade}/bin/hyprshade toggle ~/.config/hypr/shaders/blue-light-filter.glsl
     hyprshadeCurrent=$(hyprshade current)
     if ["$hyprshadeCurrent" = "blue-light-filter"] ; then
       ${pkgs.hyprshade}/bin/hyprshade off
-      ${pkgs.libnotify}/bin/notify-send 'blue-light-filter ended';
+      ${pkgs.libnotify}/bin/notify-send "blue-light-filter ended";
       exit
+      else
+        ${pkgs.hyprshade}/bin/hyprshade on ~/.config/hypr/shaders/blue-light-filter.glsl
+        ${pkgs.libnotify}/bin/notify-send "blue-light-filter started";
     fi
-    ${pkgs.hyprshade}/bin/hyprshade on ~/.config/hypr/shaders/blue-light-filter.glsl
-    start = "${pkgs.libnotify}/bin/notify-send 'blue-light-filter started';
   '';
 in { environment.systemPackages = [ blue-filter ]; }
