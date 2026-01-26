3dc0e4c2ce0ebe717ed377b4eea0f4d9ebcbe136
Islam Ahmed <softeng.islam@gmail.com>
2026-01-26 03:28:21 +0200

    update

--- modules/graphics/default.nix
+++ modules/graphics/default.nix
@@ -143,20 +143,23 @@ in {
       # =================================
 
       # Vulkan ICD files — this should point to the system-wide location from Mesa
-      VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json";
+      VK_ICD_FILENAMES =
+        "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json";
 
       # Vulkan ICD (Installable Client Driver) configuration
       # /run/opengl-driver/share/vulkan/icd.d/
       # VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
-      VK_DRIVER_FILES = "${mesa_icd_dir}/radeon_icd.x86_64.json:${mesa_icd_dir}/lvp_icd.x86_64.json:${mesa_icd_dir}/gfxstream_vk_icd.x86_64.json";
+      VK_DRIVER_FILES =
+        "${mesa_icd_dir}/radeon_icd.x86_64.json:${mesa_icd_dir}/lvp_icd.x86_64.json:${mesa_icd_dir}/gfxstream_vk_icd.x86_64.json";
 
       # Set Vulkan environment variables
       Vulkan_INCLUDE_DIR = "${pkgs.vulkan-headers}/include";
       Vulkan_LIBRARY = "${pkgs.vulkan-loader}/lib/libvulkan.so.1";
-      VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
-      VULKAN_SDK = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+      VK_LAYER_PATH =
+        "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+      VULKAN_SDK =
+        "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
       # =================================
-
 
       # Enable present_wait extension (helps with frame timing on Wayland)
       VK_KHR_PRESENT_WAIT_ENABLED = "1";
@@ -165,7 +168,8 @@ in {
       VK_PRESENT_MODE = "mailbox";
 
       # Disable problematic/unused Vulkan layers
-      VK_LOADER_LAYERS_DISABLE = "VK_LAYER_LUNARG_api_dump:VK_LAYER_LUNARG_monitor";
+      VK_LOADER_LAYERS_DISABLE =
+        "VK_LAYER_LUNARG_api_dump:VK_LAYER_LUNARG_monitor";
 
       # Tell Mesa to prefer Wayland
       VK_WSI_MODE = "wayland";
@@ -218,11 +222,14 @@ in {
       DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1 = "1";
 
       # Some apps dont like integrated + discreet and default to integrated so this should fix that
-      __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
+      __EGL_VENDOR_LIBRARY_FILENAMES =
+        "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
       # __EGL_VENDOR_LIBRARY_FILENAMES = "${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json";
 
-      LIBGL_DRIVERS_PATH = lib.makeSearchPathOutput "lib" "lib/dri" [ pkgs.mesa ];
-      LIBVA_DRIVERS_PATH = lib.makeSearchPathOutput "out" "lib/dri" [ pkgs.mesa ];
+      LIBGL_DRIVERS_PATH =
+        lib.makeSearchPathOutput "lib" "lib/dri" [ pkgs.mesa ];
+      LIBVA_DRIVERS_PATH =
+        lib.makeSearchPathOutput "out" "lib/dri" [ pkgs.mesa ];
       __EGL_VENDOR_LIBRARY_DIRS = "${pkgs.mesa}/share/glvnd/egl_vendor.d/";
 
       # Disable Mesa’s experimental device select layer if needed
@@ -234,19 +241,19 @@ in {
       MESA_GLTHREAD = "true";
 
       # Mesa OpenGL (somewhat useful)
-      # MESA_NO_ERROR = "1";
-      # MESA_GL_VERSION_OVERRIDE = "4.6";
-      # MESA_GLSL_VERSION_OVERRIDE = "460";
-      # MESA_LOADER_DRIVER_OVERRIDE = "radeonsi";
-      # MESA_VK_WSI_PRESENT_MODE = "fifo"; # Performance optimization
-      # MESA_DISK_CACHE_DATABASE = "1";
-      # MESA_DISK_CACHE_SINGLE_FILE = "0";
-      # MESA_GLSL_CACHE_MAX_SIZE = "32G";
-      # MESA_SHADER_CACHE_MAX_SIZE = "32G";
-      # MESA_GLSL_CACHE_ENABLE = "true";
-      # MESA_DISK_CACHE_MULTI_FILE = "1";
-      # MESA_VK_WSI_DISPLAY = "wayland";
-      # MESA_VK_WSI_LIST = "wayland";
+      MESA_NO_ERROR = "1";
+      MESA_GL_VERSION_OVERRIDE = "4.6";
+      MESA_GLSL_VERSION_OVERRIDE = "460";
+      MESA_LOADER_DRIVER_OVERRIDE = "radeonsi";
+      MESA_VK_WSI_PRESENT_MODE = "fifo"; # Performance optimization
+      MESA_DISK_CACHE_DATABASE = "1";
+      MESA_DISK_CACHE_SINGLE_FILE = "0";
+      MESA_GLSL_CACHE_MAX_SIZE = "32G";
+      MESA_SHADER_CACHE_MAX_SIZE = "32G";
+      MESA_GLSL_CACHE_ENABLE = "true";
+      MESA_DISK_CACHE_MULTI_FILE = "1";
+      MESA_VK_WSI_DISPLAY = "wayland";
+      MESA_VK_WSI_LIST = "wayland";
     };
 
     # ========== Hardware Graphics Configuration ==========
