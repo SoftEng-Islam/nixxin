6a780c5cbf6e4025a8f18d91d782e7744f7616e2
Islam Ahmed <softeng.islam@gmail.com>
2026-02-25 04:17:15 +0200

    update

--- modules/system/display_manager.nix
+++ modules/system/display_manager.nix
@@ -5,17 +5,29 @@
   pkgs,
   ...
 }:
+let
+  sugarTheme =
+    if pkgs ? sddm-sugar-candy then
+      {
+        name = "sugar-candy";
+        package = pkgs.sddm-sugar-candy;
+      }
+    else
+      {
+        name = "sugar-dark";
+        package = pkgs.sddm-sugar-dark;
+      };
+in
 {
   # Desktop Manager & Display Manager
   services.displayManager.enable = true;
   services.displayManager.sddm = {
     enable = true;
-    theme = "sugar-candy";
-    extraPackages = with pkgs; [
-      sddm-sugar-dark
-      qt5.qtgraphicaleffects
-      qt5.qtquickcontrols2
-      qt5.qtsvg
+    theme = sugarTheme.name;
+    extraPackages = [
+      sugarTheme.package
+      pkgs.qt6.qt5compat
+      pkgs.qt6.qtsvg
     ];
     settings = {
       Theme = {
