2a1752f44d57884c20125e98fb10347a73fa275b
Islam Ahmed <softeng.islam@gmail.com>
2026-02-25 04:31:36 +0200

    update

--- modules/desktop/keyring.nix
+++ modules/desktop/keyring.nix
@@ -2,9 +2,10 @@
 # Handles credential/key storage for apps (GNOME Keyring for now, but could be extended later for KeePassXC or pass).
 { settings, lib, inputs, pkgs, ... }:
 lib.mkIf (settings.modules.desktop.keyring.enable or false) {
-  # Make sure user and greetd PAM can unlock the keyring
+  # Make sure user and display manager PAM can unlock the keyring
   security.pam.services.${settings.user.username} = { enable = true; };
   security.pam.services.greetd.enableGnomeKeyring = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
 
   # GNOME Keyring (default secret service provider)
   services.gnome.gnome-keyring.enable = true; # User's credentials manager
--- modules/system/display_manager.nix
+++ modules/system/display_manager.nix
@@ -6,29 +6,73 @@
   ...
 }:
 let
-  sugarTheme =
-    if pkgs ? sddm-sugar-candy then
-      {
-        name = "sugar-candy";
-        package = pkgs.sddm-sugar-candy;
-      }
+  sddmAstronautBase =
+    if pkgs ? sddm-astronaut then
+      pkgs.sddm-astronaut
     else
-      {
-        name = "sugar-dark";
-        package = pkgs.sddm-sugar-dark;
+      pkgs.stdenvNoCC.mkDerivation {
+        pname = "sddm-astronaut-theme";
+        version = "unstable";
+        src = pkgs.fetchFromGitHub {
+          owner = "Keyitdev";
+          repo = "sddm-astronaut-theme";
+          rev = "master";
+          hash = lib.fakeHash;
+        };
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/share/sddm/themes
+          cp -R . $out/share/sddm/themes/sddm-astronaut-theme
+          runHook postInstall
+        '';
       };
+
+  sddmAstronautBlackHole = sddmAstronautBase.overrideAttrs (old: {
+    postInstall = (old.postInstall or "") + ''
+      substituteInPlace "$out/share/sddm/themes/sddm-astronaut-theme/metadata.desktop" \
+        --replace-fail "ConfigFile=Themes/astronaut.conf" "ConfigFile=Themes/black_hole.conf"
+    '';
+  });
+
+  qtMultimedia =
+    if pkgs ? kdePackages && pkgs.kdePackages ? qtmultimedia then
+      pkgs.kdePackages.qtmultimedia
+    else if pkgs ? qt6 && pkgs.qt6 ? qtmultimedia then
+      pkgs.qt6.qtmultimedia
+    else
+      null;
+
+  qtVirtualKeyboard =
+    if pkgs ? kdePackages && pkgs.kdePackages ? qtvirtualkeyboard then
+      pkgs.kdePackages.qtvirtualkeyboard
+    else if pkgs ? qt6 && pkgs.qt6 ? qtvirtualkeyboard then
+      pkgs.qt6.qtvirtualkeyboard
+    else
+      null;
+
+  qtSvg =
+    if pkgs ? kdePackages && pkgs.kdePackages ? qtsvg then
+      pkgs.kdePackages.qtsvg
+    else if pkgs ? qt6 && pkgs.qt6 ? qtsvg then
+      pkgs.qt6.qtsvg
+    else
+      null;
+
+  sddmAstronautDeps =
+    lib.optionals (qtMultimedia != null) [ qtMultimedia ]
+    ++ lib.optionals (qtVirtualKeyboard != null) [ qtVirtualKeyboard ]
+    ++ lib.optionals (qtSvg != null) [ qtSvg ];
 in
 {
   # Desktop Manager & Display Manager
   services.displayManager.enable = true;
+
+  environment.systemPackages = [ sddmAstronautBlackHole ] ++ sddmAstronautDeps;
+
   services.displayManager.sddm = {
     enable = true;
-    theme = sugarTheme.name;
-    extraPackages = [
-      sugarTheme.package
-      pkgs.qt6.qt5compat
-      pkgs.qt6.qtsvg
-    ];
+    theme = "sddm-astronaut-theme";
+    extraPackages = [ sddmAstronautBlackHole ] ++ sddmAstronautDeps;
     settings = {
       Theme = {
         CursorTheme = settings.common.cursor.name;
