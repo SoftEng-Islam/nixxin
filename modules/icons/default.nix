b42a4a9b04b7408cf3a646e5e28652ebcc0072d7
Islam Ahmed <softeng.islam@gmail.com>
2026-02-25 04:44:11 +0200

    update

--- modules/icons/default.nix
+++ modules/icons/default.nix
@@ -12,24 +12,31 @@ in mkIf (settings.modules.icons.enable or false) {
           # settings
           # black, blue, brown, cyan, green, grey, indigo, magenta, orange, pink, purple, red, teal, white, yellow
           color=${settings.common.icons.folder-color}
-          THEME_DIR=$out/share/icons/Papirus
+          THEME_DIRS=(
+            "$out/share/icons/Papirus"
+            "$out/share/icons/Papirus-Dark"
+            "$out/share/icons/Papirus-Light"
+          )
 
           # see change_color
           sizes=(22x22 24x24 32x32 48x48 64x64)
           prefixes=("folder-$color" "user-$color")
 
-          for size in "''${sizes[@]}"; do
-            for prefix in "''${prefixes[@]}"; do
-              for file_path in "$THEME_DIR/$size/places/$prefix"{-*,}.svg; do
-                [ -f "$file_path" ] || continue  # skip if not a file
-                [ -L "$file_path" ] && continue  # skip if symlink
+          for THEME_DIR in "''${THEME_DIRS[@]}"; do
+            [ -d "$THEME_DIR" ] || continue
+            for size in "''${sizes[@]}"; do
+              for prefix in "''${prefixes[@]}"; do
+                for file_path in "$THEME_DIR/$size/places/$prefix"{-*,}.svg; do
+                  [ -f "$file_path" ] || continue  # skip if not a file
+                  [ -L "$file_path" ] && continue  # skip if symlink
 
-                file_name="''${file_path##*/}"
-                symlink_path="''${file_path/-$color/}"  # remove color suffix
+                  file_name="''${file_path##*/}"
+                  symlink_path="''${file_path/-$color/}"  # remove color suffix
 
-                ln -sf "$file_name" "$symlink_path" || {
-                  echo "Failed to create '$symlink_path'"
-                }
+                  ln -sf "$file_name" "$symlink_path" || {
+                    echo "Failed to create '$symlink_path'"
+                  }
+                done
               done
             done
           done
