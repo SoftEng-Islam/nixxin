6de5f4b03b6507779b99fb5215052dacea0a07bf
Islam Ahmed <softeng.islam@gmail.com>
2026-02-25 03:49:11 +0200

    update

--- modules/system/display_manager.nix
+++ modules/system/display_manager.nix
@@ -1,36 +1,34 @@
 # Display/Login manager
-{ settings, lib, pkgs, ... }: {
+{
+  settings,
+  lib,
+  pkgs,
+  ...
+}:
+{
   # Desktop Manager & Display Manager
   services.displayManager.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "sugar-candy";
+    extraPackages = with pkgs; [
+      sddm-sugar-candy
+      qt5.qtgraphicaleffects
+      qt5.qtquickcontrols2
+      qt5.qtsvg
+    ];
+    settings = {
+      Theme = {
+        CursorTheme = settings.common.cursor.name;
+        CursorSize = settings.common.cursor.size;
+      };
+    };
+  };
 
   # ---- Set Default Session ---- #
   services.displayManager.defaultSession = "hyprland";
 
   # ---- XSERVER ---- #
   services.xserver.enable = true;
-  services.xserver.autorun = false;
-
-  # Init session with hyprland
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = let
-        tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
-        tuigreetOptions = [
-          "--debug /home/${settings.user.username}/.config/tuigreet/debug.log"
-          "--remember"
-          "--time"
-          "--asterisks"
-          "--time-format '%I:%M %p | %a - %h | %F'"
-          "--greeting 'Hello There!'"
-          "--theme 'border=magenta;text=white;prompt=magenta;time=white;action=magenta;button=white;container=black;input=white'"
-          "--cmd 'Hyprland > /dev/null 2>&1'"
-        ];
-        flags = lib.concatStringsSep " " tuigreetOptions;
-      in {
-        command = "${tuigreet} ${flags}";
-        user = "greeter";
-      };
-    };
-  };
+  services.xserver.autorun = true;
 }
