eef37d26d93151f7e8e56c813e957ea15c7a4106
Islam Ahmed <softeng.islam@gmail.com>
2026-02-26 05:37:24 +0200

    update

--- modules/desktop/file_manager.nix
+++ modules/desktop/file_manager.nix
@@ -94,7 +94,7 @@
             <property name="default-view" type="string" value="ThunarIconView"/>
             <property name="last-details-view-column-widths" type="string" value="50,50,118,111,50,50,50,50,993,50,50,84,50,151"/>
             <property name="last-details-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_38_PERCENT"/>
-            <property name="last-icon-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_100_PERCENT"/>
+            <property name="last-icon-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_500_PERCENT"/>
             <property name="last-image-preview-visible" type="bool" value="false"/>
             <property name="last-location-bar" type="string" value="ThunarLocationEntry"/>
             <property name="last-menubar-visible" type="bool" value="false"/>
