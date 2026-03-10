23ba3ba229e790a214c5ff5ce54d04ede4d3d5b9
Islam Ahmed <softeng.islam@gmail.com>
2026-03-10 03:37:35 +0200

    update

--- modules/desktop/fileManager/yazi.nix
+++ modules/desktop/fileManager/yazi.nix
@@ -1,36 +1,37 @@
 {
+  settings,
   pkgs,
   ...
 }:
 {
-
-  programs.yazi = {
-    enable = true;
-    flavors = {
-      inherit (pkgs.yaziFlavors)
-        vscode-dark-modern
-        vscode-light-modern
-        ;
-    };
-    theme.flavor = {
-      dark = "vscode-dark-modern";
-      light = "vscode-light-modern";
-    };
-    settings = {
-      mgr = {
-        ratio = [
-          1
-          3
-          4
-        ];
-        show_hidden = true;
-        show_symlink = true;
-        linemode = "size";
+  home-manager.users.${settings.user.username} = {
+    programs.yazi = {
+      enable = true;
+      flavors = {
+        inherit (pkgs.yaziFlavors)
+          vscode-dark-modern
+          vscode-light-modern
+          ;
       };
-      preview.wrap = "yes";
+      theme.flavor = {
+        dark = "vscode-dark-modern";
+        light = "vscode-light-modern";
+      };
+      settings = {
+        mgr = {
+          ratio = [
+            1
+            3
+            4
+          ];
+          show_hidden = true;
+          show_symlink = true;
+          linemode = "size";
+        };
+        preview.wrap = "yes";
+      };
     };
   };
-
   environment.systemPackages = with pkgs; [
     yazi
   ];
