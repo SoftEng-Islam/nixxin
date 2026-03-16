bfcb7b264aef6400da4c08a6b074df56786775a6
Islam Ahmed <softeng.islam@gmail.com>
2026-03-16 02:52:13 +0200

    update

--- modules/development/vscode.nix
+++ modules/development/vscode.nix
@@ -7,25 +7,6 @@
 {
   programs.vscode = {
     enable = true;
-    package =
-      (pkgs.vscode.overrideAttrs (old: {
-        postInstall = (old.postInstall or "") + ''
-          # wrapProgram $out/bin/code \
-          #  --run "exec 2>/dev/null"
-        '';
-      })).override
-        {
-          isInsiders = false;
-          # Configure VSCode to run without requiring --no-sandbox
-          useVSCodeRipgrep = true;
-          commandLineArgs = builtins.concatStringsSep " " [
-            "--ozone-platform=wayland"
-            "--enable-wayland-ime"
-            # Keep rendering at 1:1 to avoid fractional scaling performance issues.
-            "--force-device-scale-factor=1"
-            "--password-store=gnome" # use gnome-keyring as password store
-          ];
-        };
   };
   home-manager.users.${settings.user.username} = {
     xdg.mimeApps.associations.removed = {
