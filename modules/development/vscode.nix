bd7a48bd861159505345ed0a99ae12c7831a8327
Islam Ahmed <softeng.islam@gmail.com>
2026-03-16 06:03:31 +0200

    update

--- modules/development/vscode.nix
+++ modules/development/vscode.nix
@@ -5,12 +5,13 @@
   ...
 }:
 {
-  programs.vscode = {
-    enable = true;
-    package = pkgs.update.vscode;
-  };
   programs.vscode.defaultEditor = true;
   home-manager.users.${settings.user.username} = {
+    programs.vscode = {
+      enable = true;
+      package = pkgs.update.vscode;
+      mutableExtensionsDir = true;
+    };
     xdg.mimeApps.associations.removed = {
       "inode/directory" = "code.desktop";
     };
