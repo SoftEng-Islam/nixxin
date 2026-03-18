17bb0e7dbba4bb6a1b66663c725688839b5122dc
Islam Ahmed <softeng.islam@gmail.com>
2026-03-18 02:22:03 +0200

    update

--- modules/cli/yazi/default.nix
+++ modules/cli/yazi/default.nix
@@ -1,7 +1,7 @@
 {
   settings,
   pkgs,
-  config,
+  inputs,
   ...
 }:
 {
@@ -16,7 +16,17 @@
       keymap = import ./keymap.nix;
       theme = import ./theme.nix;
       flavors = import ./flavors.nix { inherit pkgs; };
-      plugins = import ./plugins.nix { inherit pkgs; };
+      plugins = {
+        augment-command = "${inputs.yazi-augment-command}";
+        chmod = "${inputs.yazi-plugins}/chmod.yazi";
+        compress = "${inputs.yazi-compress}";
+        full-border = "${inputs.yazi-plugins}/full-border.yazi";
+        git = "${inputs.yazi-plugins}/git.yazi";
+        hexyl = "${inputs.yazi-hexyl}";
+        mount = "${inputs.yazi-plugins}/mount.yazi";
+        toggle-pane = "${inputs.yazi-plugins}/toggle-pane.yazi";
+        what-size = "${inputs.yazi-what-size}";
+      };
       shellWrapperName = "y";
       initLua = ''
         -- require("full-border"):setup()
