4751e6c0f36f3da7f21c4e748a095da8d353c0dd
Islam Ahmed <softeng.islam@gmail.com>
2026-03-22 05:53:32 +0200

    update

--- modules/cli/postgre.nix
+++ modules/cli/postgre.nix
@@ -2,6 +2,7 @@
 {
   services.postgresql.enable = true;
   # services.pgadmin.enable = true;
+  # services.pgadmin.initialEmail =
   environment.systemPackages = with pkgs; [
     postgresql
     # pgadmin4
