639099d760bbc4c49b0701525311d5598f1aca1b
Islam Ahmed <softeng.islam@gmail.com>
2026-03-29 02:10:48 +0200

    update automation

--- modules/automation/default.nix
+++ modules/automation/default.nix
@@ -3,5 +3,10 @@
   imports = [
     ./n8n.nix
   ];
-
+  environment.systemPackages = with pkgs; [
+    uv
+    (pkgs.writeShellScriptBin "browser-use" ''
+      exec ${pkgs.uv}/bin/uvx browser-use[cli] "$@"
+    '')
+  ];
 }
