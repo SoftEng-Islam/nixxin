284fba5c4545ce06639019e2aff23e98c8aec2ee
Islam Ahmed <softeng.islam@gmail.com>
2026-04-03 02:16:23 +0200

    Update

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -37,23 +37,23 @@
       "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
       "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
     ];
-  };
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      # "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
-      # "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
-      # "text/html" = "app.zen_browser.zen.desktop";
-      # "application/pdf" = "org.gnome.Evince.desktop";
-      # "audio/mpeg" = "io.mpv.Mpv.desktop";
-      # "video/mp4" = "io.mpv.Mpv.desktop";
-      # "x-scheme-handler/video" = "io.mpv.Mpv.desktop";
-      # "image/jpeg" = "org.gnome.Loupe.desktop";
-      # "image/png" = "org.gnome.Loupe.desktop";
-      # "image/*" = "org.gnome.Loupe.desktop";
-      # "x-scheme-handler/terminal" = "kitty.desktop";
-      # "x-scheme-handler/mailto" = "eu.betterbird.Betterbird.desktop";
-      # "message/rfc822" = "eu.betterbird.Betterbird.desktop";
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        # "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+        # "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+        # "text/html" = "app.zen_browser.zen.desktop";
+        # "application/pdf" = "org.gnome.Evince.desktop";
+        # "audio/mpeg" = "io.mpv.Mpv.desktop";
+        # "video/mp4" = "io.mpv.Mpv.desktop";
+        # "x-scheme-handler/video" = "io.mpv.Mpv.desktop";
+        # "image/jpeg" = "org.gnome.Loupe.desktop";
+        # "image/png" = "org.gnome.Loupe.desktop";
+        # "image/*" = "org.gnome.Loupe.desktop";
+        # "x-scheme-handler/terminal" = "kitty.desktop";
+        # "x-scheme-handler/mailto" = "eu.betterbird.Betterbird.desktop";
+        # "message/rfc822" = "eu.betterbird.Betterbird.desktop";
+      };
     };
   };
 }
