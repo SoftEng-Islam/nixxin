a3c7841d5cac280d9fd954525fad79a84747b630
Islam Ahmed <softeng.islam@gmail.com>
2026-04-04 04:57:17 +0200

    update/fix for antigravity

--- modules/development/ai-tools.nix
+++ modules/development/ai-tools.nix
@@ -1,10 +1,19 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
     # AI Tools for developers
     pkgs.update.code-cursor
-    pkgs.update.antigravity
+    (pkgs.symlinkJoin {
+      name = "antigravity";
+      paths = [ pkgs.update.antigravity ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/antigravity \
+          --unset NIXOS_OZONE_WL \
+          --add-flags "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations"
+      '';
+    })
     pkgs.update.windsurf
   ];
 }
