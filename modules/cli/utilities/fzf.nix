d8da38096ba071832d60213cc39e4746a0fe1ec8
Islam Ahmed <softeng.islam@gmail.com>
2026-08-22 03:29:20 +0300

    update

--- modules/cli/utilities/fzf.nix
+++ modules/cli/utilities/fzf.nix
@@ -12,10 +12,11 @@
       "--border"
     ];
 
-    fileWidgetCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
-    fileWidgetOptions = [
+    fileWidget.command = "fd --hidden --strip-cwd-prefix --exclude .git";
+    fileWidget.options = [
       "--preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'"
     ];
+    historyWidget.command = ""; # <-- add this, hands Ctrl-R to atuin
     changeDirWidgetCommand = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
     changeDirWidgetOptions = [
       "--preview 'eza --tree --color=always {} | head -200'"
