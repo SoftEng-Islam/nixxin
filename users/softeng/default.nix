fc603579f798d6fe4278c0615386a03e9df7b6bf
Islam Ahmed <softeng.islam@gmail.com>
2026-08-29 16:18:51 +0300

    update

--- users/softeng/default.nix
+++ users/softeng/default.nix
@@ -28,6 +28,7 @@ self: {
 
   modules.development.editors.kiro.enable = false;
   modules.development.editors.windsurf.enable = true;
+  modules.development.editors.antigravity.enable = true;
   modules.development.languages.rust.enable = false;
 
   # Not perfectly working
