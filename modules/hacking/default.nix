27b3aa7f76c9366c61a58fa2498bcdf5902cc3f3
Islam Ahmed <softeng.islam@gmail.com>
2026-09-03 03:17:07 +0300

    try to install ROCM and enable HIP

--- modules/hacking/default.nix
+++ modules/hacking/default.nix
@@ -1,4 +1,10 @@
-{ settings, lib, pkgs, pkgs-older, ... }:
+{
+  settings,
+  lib,
+  pkgs,
+  pkgs-older,
+  ...
+}:
 let
   inherit (lib) mkIf;
 
@@ -32,28 +38,30 @@ let
 in
 {
   config = mkIf (settings.modules.hacking.enable) {
-    environment.systemPackages = with pkgs; [
-      hashcatWrapper
-      hashcat-utils
-      john
-      johnny
-      aircrack-ng
-      wireshark
-      airgorah
-      xterm
-      macchanger
-      hcxdumptool
-      hcxtools
-      netcat
-      netop
-      cowpatty
-      wavemon
-      linssid
-      wifite2
-      thc-hydra
-      vault-medusa
-      medusa
-    ]
-    ++ lib.optional (hashcatLegacy != null) hashcatLegacy;
+    environment.systemPackages =
+      with pkgs;
+      [
+        hashcat
+        hashcat-utils
+        john
+        johnny
+        aircrack-ng
+        wireshark
+        airgorah
+        xterm
+        macchanger
+        hcxdumptool
+        hcxtools
+        netcat
+        netop
+        cowpatty
+        wavemon
+        linssid
+        wifite2
+        thc-hydra
+        vault-medusa
+        medusa
+      ]
+      ++ lib.optional (hashcatLegacy != null) hashcatLegacy;
   };
 }
