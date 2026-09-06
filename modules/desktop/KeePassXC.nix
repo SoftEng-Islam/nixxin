6529c543311fba5e9181fb854558a38cf0db09ff
Islam Ahmed <softeng.islam@gmail.com>
2026-09-06 04:44:09 +0300

    update

--- modules/desktop/KeePassXC.nix
+++ modules/desktop/KeePassXC.nix
@@ -1,26 +1,36 @@
 {
   settings,
   lib,
-  pkgs,
   ...
 }:
 {
   home-manager.users."${settings.user.username}" = {
-    xdg.configFile."keepassxc/keepassxc.ini".text = lib.generators.toINI { } {
-      General = {
-        ConfigVersion = 2;
-        LastActiveDatabase = "/home/${settings.user.username}/Documents/Passwords.kdbx";
+    programs.keepassxc = {
+      enable = true;
+      autostart = true;
+      settings = {
+        General = {
+          ConfigVersion = 2;
+          LastActiveDatabase = "/home/${settings.user.username}/Documents/Passwords.kdbx";
+        };
+        Security = {
+          LockDatabaseIdle = true;
+          LockDatabaseIdleSeconds = 60;
+        };
+        Browser = {
+          Enabled = true;
+          SearchInAllDatabases = true;
+          UpdateBinaryPath = false;
+        };
+        GUI = {
+          AdvancedSettings = true;
+          ApplicationTheme = "dark";
+          MinimizeOnClose = true;
+          MinimizeToTray = true;
+          ShowTrayIcon = true;
+        };
+        SSHAgent.Enabled = true;
       };
-      Security = {
-        LockDatabaseIdle = true;
-        LockDatabaseIdleSeconds = 60;
-      };
-      Browser = {
-        Enabled = true;
-        SearchInAllDatabases = true;
-      };
-      GUI.TrayIconAppearance = "monochrome-light";
     };
-    home.packages = with pkgs; [ keepassxc ];
   };
 }
