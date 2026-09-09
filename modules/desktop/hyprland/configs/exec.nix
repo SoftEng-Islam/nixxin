ed24ff42d1e9d6473abfb635ffe082da928ca3a3
Islam Ahmed <softeng.islam@gmail.com>
2026-09-10 02:27:36 +0300

    update

--- modules/desktop/hyprland/configs/exec.nix
+++ modules/desktop/hyprland/configs/exec.nix
@@ -12,18 +12,18 @@
           "hyprland.start"
           (lib.generators.mkLuaInline ''
             function()
-              # ---- Start Noctalia V5 Shell ---- #
+         
               hl.exec_cmd("noctalia --daemon")
               hl.exec_cmd("qs -c overview & disown")
-              # ---- Blue Color Filter ---- #
+             
               hl.exec_cmd("${pkgs.hyprshade}/bin/hyprshade toggle ~/.config/hypr/shaders/blue-light-filter.glsl & disown")
-              # ---- Clipboard ---- #
+              
               hl.exec_cmd("${pkgs.wl-clipboard-rs}/bin/wl-paste --type text --watch ${pkgs.cliphist}/bin/cliphist store & disown")
               hl.exec_cmd("${pkgs.wl-clipboard-rs}/bin/wl-paste --type image --watch ${pkgs.cliphist}/bin/cliphist store & disown")
-              # ---- Set Cursor ---- #
+             
               hl.exec_cmd("${pkgs.hyprland}/bin/hyprctl setcursor ${settings.common.cursor.name} ${toString settings.common.cursor.size} & disown")
 
-              # ---- Apps To Start ---- #
+              
               hl.exec_cmd("${pkgs.telegram-desktop}/bin/telegram-desktop -startintray & disown")
             end
           '')
