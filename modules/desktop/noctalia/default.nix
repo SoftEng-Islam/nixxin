63d23962125f03449528fce381d789f6e952a2c4
Islam Ahmed <softeng.islam@gmail.com>
2026-09-09 21:03:35 +0300

    update

--- modules/desktop/noctalia/default.nix
+++ modules/desktop/noctalia/default.nix
@@ -163,8 +163,8 @@ in
       [theme.templates]
       enable_builtin_templates   = true
       enable_community_templates = true
-      builtin_ids                = [ "gtk3", "gtk4", "qt", "hyprland", "wezterm" ] # opt-in; list ids with: noctalia theme --list-templates
-      community_ids              = [ "zed", "yazi", "papirus-icons" ]     # fetched from api.noctalia.dev/templates, cached locally
+      builtin_ids                = [ "gtk3", "gtk4", "qt", "hyprland", "wezterm", "btop" ] # opt-in; list ids with: noctalia theme --list-templates
+      community_ids              = [ "zed", "yazi", "papirus-icons", "antigravity", "claude-code", "codex", "brave", "discord", "telegram", "blender", "inkscape", "libreoffice", "micro", "obsidian", "vscode", "zed", "steam", "obs", "siyuan", "bat", "fzf", "glow" ]     # fetched from api.noctalia.dev/templates, cached locally
 
       # User-defined templates are declared directly in config.
       # [theme.templates.user.my_app]
