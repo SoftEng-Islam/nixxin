97d100ecb38de023cb7a1177bc9aa492167d5aa7
Islam Ahmed <softeng.islam@gmail.com>
2026-09-10 22:49:32 +0300

    update

--- modules/desktop/hyprland/configs/monitor.nix
+++ modules/desktop/hyprland/configs/monitor.nix
@@ -11,12 +11,12 @@
           mode = "2560x1440@144";
           position = "0x0";
           scale = 1;
-          vrr = 1;
+          # vrr = 1;
           bitdepth = 8;
           # cm = "srgb"; # Desktop stays in sRGB; cm_auto_hdr handles HDR games
           sdr_eotf = "gamma22";
           # supports_wide_color = 0; # 0 = auto (allows DCI-P3 wide color in HDR)
-          supports_hdr = 1; # 1 = force HDR capability enabled
+          # supports_hdr = 1; # 1 = force HDR capability enabled
         }
         {
           # Secondary: Samsung S22C450 (1080p @ 60Hz)
@@ -24,7 +24,7 @@
           mode = "1920x1080@60";
           position = "2560x0";
           scale = 1;
-          vrr = 0;
+          # vrr = 0;
           bitdepth = 8;
           # cm = "auto";
           sdr_eotf = "gamma22";
