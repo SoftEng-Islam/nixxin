aa090f8804e7d5fab65becbc4bc78731128be225
Islam Ahmed <softeng.islam@gmail.com>
2026-09-20 22:51:23 +0300

    Update & fix

--- modules/gaming/zeroad.nix
+++ modules/gaming/zeroad.nix
@@ -6,26 +6,42 @@
 }:
 lib.mkIf (settings.modules.gaming.zeroad.enable or false) {
   environment.systemPackages = with pkgs; [
-    # 0 A.D. with Vulkan support
-    (zeroad.overrideAttrs {
-      postFixup = ''
-        wrapProgram $out/bin/haruna \
-        --prefix LD_LIBRARY_PATH : ${
-          lib.makeLibraryPath [
-            vulkan-loader # libvulkan.so
-            vulkan-validation-layers # validation layer runtime
-            pipewire
-            sqlite
-            mesa
-            mesa_i686
-            libGL
-            libGLU
-            libglvnd
-          ]
-        } \
-        --set LD_PRELOAD "${pkgs.vulkan-loader}/lib/libvulkan.so.1"
+    (zeroad.overrideAttrs (oldAttrs: {
+      nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+      postFixup = (oldAttrs.postFixup or "") + ''
+        wrapProgram $out/bin/pyrogenesis \
+          --set SDL_VIDEODRIVER x11 \
+          --prefix LD_LIBRARY_PATH : ${
+            lib.makeLibraryPath [
+              vulkan-loader # libvulkan.so
+              vulkan-validation-layers # validation layer runtime
+              pipewire
+              sqlite
+              mesa
+              mesa_i686
+              libGL
+              libGLU
+              libglvnd
+            ]
+          } \
+          --set LD_PRELOAD "${pkgs.vulkan-loader}/lib/libvulkan.so.1"
       '';
-    })
+    }))
     pkgs.zeroad-data
   ];
+  home-manager.users.${settings.user.username} = {
+    xdg.desktopEntries.zeroad = {
+      name = "0 A.D.";
+      genericName = "Real-Time Strategy Game";
+      comment = "A free, open-source real-time strategy game";
+      exec = "env SDL_VIDEODRIVER=x11 ${pkgs.zeroad}/bin/0ad %U";
+      icon = "0ad";
+      terminal = false;
+      type = "Application";
+      categories = [
+        "Game"
+        "StrategyGame"
+      ];
+    };
+  };
 }
